eef7f40806db36a913ec88adbfd05124c557cfbc
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-21 14:15:00 +0300

    conditionals, all labs

---
+++ JS-Programming-Basics/Homeworks_and_labs/02-Conditional-Statements-LAB/01.js
@@ -0,0 +1,7 @@
+function score(input) {
+  let test = Number(input[0]);
+
+  if (test >= 5.5) {
+    console.log('Excellent!');
+  }
+}
---
+++ JS-Programming-Basics/Homeworks_and_labs/02-Conditional-Statements-LAB/04.js
@@ -0,0 +1,10 @@
+function pw(input) {
+  let a = input[0];
+  let b = 's3cr3t!P@ssw0rd';
+
+  if (a === b) {
+    console.log('Welcome');
+  } else {
+    console.log('Wrong password!');
+  }
+}
