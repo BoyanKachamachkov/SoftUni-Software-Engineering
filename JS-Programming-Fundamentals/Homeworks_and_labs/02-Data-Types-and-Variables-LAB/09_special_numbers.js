6ee918c9bbb8ebe107945fd62624b2d85147f659
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-22 18:08:03 +0300

    Solved 9

--- JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-LAB/09_special_numbers.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-LAB/09_special_numbers.js
@@ -1,6 +1,18 @@
-function specialNumbers(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {}
-}
+function special(num) {
+  for (let i = 1; i <= num; i++) {
+    let currNum = String(i);
+    let sum = 0;
 
-specialNumbers(15);
+    for (let b = 0; b < currNum.length; b++) {
+      let digit = Number(currNum[b]);
+      sum += digit;
+    }
+
+    if (sum == 5 || sum == 7 || sum == 11) {
+      console.log(`${currNum} -> True`);
+    } else {
+      console.log(`${currNum} -> False`);
+    }
+  }
+}
+special(20);
