fc5175df77721a2f80df3ecd92243fb52ddf9b72
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-23 18:13:14 +0300

    4-5 done

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/04_convert_metres_to_kilometres.js
@@ -0,0 +1,4 @@
+function convert(distanceInMetres) {
+  console.log(`${(distanceInMetres / 1000).toFixed(2)}`);
+}
+convert(1852);
---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/05_pounds_to_dollars.js
@@ -0,0 +1,7 @@
+function convert(britishPounds) {
+  let dollars = britishPounds * 1.31;
+
+  console.log(dollars.toFixed(3));
+}
+convert(80);
+convert(39);
