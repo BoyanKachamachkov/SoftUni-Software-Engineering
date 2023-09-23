34f909aeb29f01ae0295e992628e543410a2f118
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-23 20:03:31 +0300

    6,7 done

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/06_reversed_chars.js
@@ -0,0 +1,5 @@
+function reversed(a, b, c) {
+  let output = `${c} ${b} ${a}`;
+  console.log(output);
+}
+reversed('A', 'B', 'C');
---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/07_lower_to_upper.js
@@ -0,0 +1,6 @@
+function check(charAsString) {
+  let transformToUpper = charAsString.toUpperCase();
+  console.log(transformToUpper);
+}
+
+check('L'); //upper-case
