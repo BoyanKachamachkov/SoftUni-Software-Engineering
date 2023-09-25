f15c21cc401a61d2f34c49d7541687215a455300
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-25 21:45:59 +0300

    10 solved

--- JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/10_spice_must_flow*.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/02-Data-Types-and-Variables-EXERCISES/10_spice_must_flow*.js
@@ -1,1 +1,20 @@
+function solve(currentYield) {
+  //initialize result variable
+  let spice = 0;
+  let days = 0;
 
+  while (currentYield >= 100) {
+    days++;
+    spice += currentYield - 26;
+    currentYield -= 10;
+  }
+
+  if (spice >= 26) {
+    spice -= 26;
+  }
+  console.log(days);
+  console.log(spice);
+}
+solve(111); // 2 -136
+solve(450); //36 -8938
+solve(80); //36 -8938
