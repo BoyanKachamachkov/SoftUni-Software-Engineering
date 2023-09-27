22aef65a8512faf62a805944cddaef2e590e1145
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-27 21:12:16 +0300

    LAB tests

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/01.js
@@ -0,0 +1,7 @@
+function demo() {
+  let nums = [10, 20, 30];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(i, nums[i]);
+  }
+}
+demo();
---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/04.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+  for (let i = 0; i <= 2; i++) {
+    let j = arr.length - 1 - i;
+    console.log(i, j);
+  }
+}
+solve(['a', 'b', 'c', 'd', 'e']);
+// solve(['abc', 'def', 'hig', 'klm', 'nop']);
