54b612f73facfe8196351c71ae097e6852821a18
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-09-30 14:57:03 +0300

    1 lab, solved

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/01-sum-of-first-and-last-array-elements.js
@@ -0,0 +1,7 @@
+function solve(arr) {
+  let first = arr[0];
+  let last = arr[arr.length - 1]; //whatever the lengTH is, .length -1 always gives us the last element.
+
+  console.log(first + last);
+}
+solve([20, 30, 40]);
