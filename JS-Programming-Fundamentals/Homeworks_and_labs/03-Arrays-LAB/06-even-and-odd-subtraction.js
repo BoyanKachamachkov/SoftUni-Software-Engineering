58dd448ba34a202a40b7a6f191ef054c1d6ac919
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-01 11:11:09 +0300

    solved 6, added Demo file

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/06-even-and-odd-subtraction.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/06-even-and-odd-subtraction.js
@@ -1,15 +1,25 @@
-//Arrays are special objects
+function solve(arr) {
+  //Write a program that calculates the difference between the sum of the even and the sum of the odd numbers in an array.
 
-//they have built-in properties and methods like length
-//methods are written with a DOT after the variable name
+  //буферни променливи за четни и нечетни ; и краен резултат
+  let sumEven = 0;
+  let sumOdd = 0;
+  let result = 0;
 
-//example of property
-//.length
+  //обхождам масива;
+  for (let i = 0; i < arr.length; i++) {
+    //буферна променлива за конкретния индекс
+    let curNum = arr[i]; // взимам числото на индекса
 
-//-------------------
-
-//example of methods
-//.push()
-//.includes()
-//.toString()
-//.join() - един стринг от всички елементи
+    if (curNum % 2 == 0) {
+      //на всяка итерация с иф/елсе проверка добавям числото от конкретния индекс или към едната или към другата променлива
+      sumEven += curNum;
+    } else {
+      sumOdd += curNum;
+    }
+  }
+  result = sumEven - sumOdd;
+  console.log(result);
+  //накрая изваждам четните от нечетните и принтирам резултата
+}
+solve([1, 2, 3, 4, 5, 6]); //3 ---2 + 4 + 6 = 12, 1 + 3 + 5 = 9, 12 - 9 = 3
