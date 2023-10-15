2f107a608b1caf46691712863d701b0f0cb1ed1e
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-15 17:12:49 +0300

    arrays,lab,7 solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/07-equal-arrays.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/07-equal-arrays.js
@@ -1,15 +1,10 @@
-function solve(arr1, arr2) {
-  //обхождам масив1 индекс по индекс и проверявам с инклудс дали всеки индекс е равен на същия в масив2
-  //ако са идентични, събирам стойностите на първия масив и ги принтирам като сума
-
+function equalArrs(arr1, arr2) {
   let sum = 0;
-
   for (let i = 0; i < arr1.length; i++) {
-    //буферна променлива за конкретния индекс
-    let curNum = arr1[i];
+    let el = arr1[i];
 
-    if (arr2.includes(curNum)) {
-      sum += Number(curNum);
+    if (arr2.includes(el)) {
+      sum = sum + Number(el);
     } else {
       console.log(`Arrays are not identical. Found difference at ${i} index`);
       return;
@@ -18,5 +13,4 @@ function solve(arr1, arr2) {
 
   console.log(`Arrays are identical. Sum: ${sum}`);
 }
-solve(['10', '20', '30'], ['10', '20', '30']); //Arrays are identical. Sum: 60
-solve(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5']); //Arrays are not identical. Found difference at 2 index
+equalArrs(['10', '20', '30'], ['10', '20', '30']);
