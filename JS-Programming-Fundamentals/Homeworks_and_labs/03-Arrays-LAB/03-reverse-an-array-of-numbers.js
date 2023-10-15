11a35065aded33585e2923fd45433f0215da1657
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-15 16:16:18 +0300

    3,lab,array, re-resolved

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/03-reverse-an-array-of-numbers.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-LAB/03-reverse-an-array-of-numbers.js
@@ -1,13 +1,10 @@
-function solve(n, nums) {
-  //new array
+function reversed(n, nums) {
   let reversed = [];
 
-  //use nums array in reverse and push to new
   for (let i = n - 1; i >= 0; i--) {
     reversed.push(nums[i]);
   }
 
-  //print new array with join
   console.log(reversed.join(' '));
 }
-solve(3, [10, 20, 30, 40, 50]);
+reversed(2, [66, 43, 75, 89, 47]);
