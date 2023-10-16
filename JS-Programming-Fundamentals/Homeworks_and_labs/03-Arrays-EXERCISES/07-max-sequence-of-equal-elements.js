2a59e50d820a55564ebd4b031f0b2e8b9122d55e
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-16 20:18:30 +0300

    arrays,7,exe,solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/07-max-sequence-of-equal-elements.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/07-max-sequence-of-equal-elements.js
@@ -22,5 +22,6 @@ function solve(arr) {
   }
 
   console.log(`${(maxSequenceNum + ' ').repeat(maxSequence)}`);
+  console.log(`${(maxSequenceNum + ' ').repeat(maxSequence)}`);
 }
-solve([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+solve([0, 1, 1, 5, 2, 2, 6, 3, 3]);
