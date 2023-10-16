fd172705e7d88bb579c9d4e67e19e516893af76e
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-16 12:05:02 +0300

    arrays,exe,1,solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/01-add-and-subtract.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/01-add-and-subtract.js
@@ -1,41 +1,26 @@
-function solve(arr) {
-  //приготвяме нов празен масив
+function addAndSub(arr) {
   let newArr = [];
-  let originalArrSum = 0;
-  let newArrSum = 0;
+  let originalSum = 0;
+  let newSum = 0;
 
-  //обхождам цикъла
   for (let i = 0; i < arr.length; i++) {
-    //буферна променлива за моментното число
     let curNum = arr[i];
+    let index = i;
+    originalSum += curNum;
 
     if (curNum % 2 == 0) {
-      originalArrSum += curNum;
-      //ако числото е четно + индекса на който се намираме
-      curNum += i;
+      curNum += index;
+      newSum += curNum;
       newArr.push(curNum);
-      newArrSum += curNum;
     } else {
-      originalArrSum += curNum;
-      //ако числото е нечетно - индекса на който се намираме
-      curNum -= i;
+      curNum -= index;
+      newSum += curNum;
       newArr.push(curNum);
-      newArrSum += curNum;
     }
-    //на всяка итерация ПУШВАМЕ числото към новия ни масив
   }
 
-  //принтираме новия масив
   console.log(newArr);
-  //принтираме сумата на оригиналния масив;
-  console.log(originalArrSum);
-  //принтираме сумата на модифицирания масив
-  console.log(newArrSum);
+  console.log(originalSum);
+  console.log(newSum);
 }
-
-// solve([5, 15, 23, 56, 35]);
-// //[ 5, 14, 21, 59, 31 ]
-// // 134
-// // 130
-
-solve([-5, 11, 3, 0, 2]);
+addAndSub([5, 15, 23, 56, 35]);
