c732a90e8f25fa6f840d408e67af369bd37d7b4e
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-16 12:09:27 +0300

    arrays,exe,2,solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/02-common-elements.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/03-Arrays-EXERCISES/02-common-elements.js
@@ -1,26 +1,19 @@
-// Write a function, which prints common elements in two string arrays. Print all matches on separate lines. Compare the first array with the second array.
-
-function solve(arr1, arr2) {
-  //обхождаме масив1
+function commonEls(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
-    // буферна променлива
-    let el = arr1[i];
+    let curEl = arr1[i];
 
-    // сравняваме дали всеки индекс е същия с масив 2
-    if (arr2.includes(el)) {
-      // ако е същия, това е комън елемент;
-
-      // принтираме всички съвпадения на отделен ред
-      console.log(el);
+    if (arr2.includes(curEl)) {
+      console.log(curEl);
     }
   }
 }
 
-solve(
+commonEls(
   ['Hey', 'hello', 2, 4, 'Peter', 'e'],
   ['Petar', 10, 'hey', 4, 'hello', '2']
 );
 
-solve(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '], ['s', 'o', 'c', 'i', 'a', 'l']);
-
-//hello 4
+commonEls(
+  ['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
+  ['s', 'o', 'c', 'i', 'a', 'l']
+);
