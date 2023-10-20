8ac86a17e63344b52a5b026b656881a783a56c29
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-20 15:55:27 +0300

    theory file uploaded

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/6-pre-exam/theory.js
@@ -0,0 +1,42 @@
+// --END OF AN ARRAY METHODS--
+// push - adds at the end of an array (can add multiple)
+// pop - removes from the end (and returns)
+
+// --BEGINNING OF AN ARRAY METHODS--
+// unshift - add to the beginning
+// shift - remove from the beginning
+
+// --LOOK FOR VALUES ARRAY METHODS--
+// includes - look for value (returns true or false)
+// indexOf - find index of value (returns the index or -1 if not found)
+
+// -------------------------------
+// Slicing arrays (divide arrays)
+// slice() - new array from PART of another
+// -original array wont be modified
+// -you can start/end it or just put 1 argument to take the rest
+
+// Splice: Cut and Insert Array Elements
+// splice() adds/removes items to/from an array, and returns the removed item(s)
+
+// Transform Elements
+// map() – creates a new array by applying a function to every element
+
+// filter() – creates a new array from elements matching predicate
+
+//sorting
+
+// ▪ The sort() function sorts the items of an array
+// ▪ Depending on the provided compare function,
+// sorting can be alphabetic or numeric,
+// and either ascending (up) or descending (down)
+// ▪ By default, the sort() function sorts the values as
+// strings in alphabetical and ascending order
+// ▪ If you want to sort numbers or other values, you
+// need to provide the correct compare function!
+
+//Sorting String Arrays
+// The localeCompare() method is used to compare any two
+// characters without regard for the case used
+// ▪ It's a string method so it can't be used directly on an array
+// ▪ Pass localeCompare() as the comparison function:
