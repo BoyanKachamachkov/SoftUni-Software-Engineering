cd8e0bf75297f8752d78cca4c3ea2d6fc36462b5
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-20 11:33:27 +0300

    functions,exe,4,solved

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/04-Functions-EXERCISE/04-odd-and-even-sum.js
@@ -0,0 +1,20 @@
+function oddAndEeven(num) {
+  let evenSum = 0;
+  let oddSum = 0;
+
+  let numAsStr = num.toString();
+
+  for (let char of numAsStr) {
+    let digit = Number(char);
+
+    if (digit % 2 == 0) {
+      evenSum += digit;
+    } else {
+      oddSum += digit;
+    }
+  }
+
+  console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
+oddAndEeven(1000435);
+oddAndEeven(3495892137259234);
