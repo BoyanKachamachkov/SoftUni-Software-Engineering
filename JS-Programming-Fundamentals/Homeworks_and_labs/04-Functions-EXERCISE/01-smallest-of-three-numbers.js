b257895ea5f74d48cae65db02eadc276190a5c7c
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-20 10:57:12 +0300

    functions,exe,1,solved

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/04-Functions-EXERCISE/01-smallest-of-three-numbers.js
@@ -0,0 +1,6 @@
+function smallest(a, b, c) {
+  let minNum = Math.min(a, b, c);
+  console.log(minNum);
+}
+smallest(600, 342, 123);
+smallest(2, 5, 3);
--- JS-Programming-Fundamentals/Homeworks_and_labs/6-pre-exam/3.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/6-pre-exam/3.js
@@ -1,76 +1,50 @@
-function manoWar(input) {
-  // parse input, removing elements
-  // - split first two elements and create pirate ship and man-o-war status arrays
-  // - parse max health from third element
-  let pirate = input.shift().split('>').map(Number); //взимаме елемент 1 и го трием от масива
+function ships(input) {
+  let pirate = input.shift().split('>').map(Number);
   let warship = input.shift().split('>').map(Number);
   let maxHp = Number(input.shift());
   let critical = maxHp * 0.2;
 
   for (let current of input) {
-    // for each remaining element:
-    // - parse current element
-    // -- split element by ' '
-    // -- first token is command, other tokens (if any) are numbers
-    // - depending on element type, perform action:
-
     let tokens = current.split(' ');
     let command = tokens.shift();
 
     if (command == 'Retire') {
-      // # Retire - stop processing
-
       break;
     } else if (command == 'Fire') {
-      // # Fire -
-      // - validate index
-      // - damage man-o-war section
-      // - if section is broken, pring message and stop execution
       let index = Number(tokens[0]);
       let damage = Number(tokens[1]);
 
       if (index < 0 || index >= warship.length) {
         continue;
       }
-
       warship[index] -= damage;
       if (warship[index] <= 0) {
-        console.log(`You won! The enemy ship has sunken.`);
+        console.log('You won! The enemy ship has sunken.');
         return;
       }
     } else if (command == 'Defend') {
-      // # Defend - damage pirate ship sections
-      // - validate index
       let start = Number(tokens[0]);
       let end = Number(tokens[1]);
       let damage = Number(tokens[2]);
 
       if (
-        start > 0 ||
+        start < 0 ||
         start >= pirate.length ||
         end < 0 ||
         end >= pirate.length
       ) {
         continue;
-        // - damage pirate ship section
       }
 
       for (let i = start; i <= end; i++) {
         pirate[i] -= damage;
 
         if (pirate[i] <= 0) {
-          console.log(`You lost! The pirate ship has sunken.`);
+          console.log('You lost! The pirate ship has sunken.');
           return;
         }
       }
-
-      // - if section is broken, pring message and stop execution
-    }
-    // # Status - print heavily damaged (hp < 20% max) sections
-    else if (command == 'Repair') {
-      // # Repair -
-      // - validate index
-
+    } else if ((command = 'Repair')) {
       let index = Number(tokens[0]);
       let hp = Number(tokens[1]);
 
@@ -82,15 +56,11 @@ function manoWar(input) {
       if (pirate[index] > maxHp) {
         pirate[index] = maxHp;
       }
-
-      // - increase section hp (without exceeding max)
     } else if (command == 'Status') {
       let damaged = pirate.filter((section) => section < critical);
       console.log(`${damaged.length} sections need repair.`);
     }
   }
-
-  // print
 
   let pirateStatus = 0;
   let warshipStatus = 0;
@@ -102,11 +72,12 @@ function manoWar(input) {
   for (let section of warship) {
     warshipStatus += section;
   }
+
   console.log(`Pirate ship status: ${pirateStatus}`);
   console.log(`Warship status: ${warshipStatus}`);
 }
 
-manoWar([
+ships([
   '12>13>11>20>66',
   '12>22>33>44>55>32>18',
   '70',
@@ -119,7 +90,8 @@ manoWar([
   'Retire',
 ]);
 
-manoWar([
+console.log(`-----`);
+ships([
   '2>3>4>5>2',
   '6>7>8>9>10>11',
   '20',
