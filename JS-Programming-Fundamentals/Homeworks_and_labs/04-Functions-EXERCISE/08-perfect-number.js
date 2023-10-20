5f88f90a78d5fd3b8fa1b166d8b553cd0b211382
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-20 14:59:32 +0300

    functions,exe,8,solved

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/04-Functions-EXERCISE/08-perfect-number.js
@@ -0,0 +1,18 @@
+function perfectNum(num) {
+  let sumProperDivisors = 0;
+
+  for (let divisor = 1; divisor < num; divisor++) {
+    if (num % divisor == 0) {
+      sumProperDivisors += divisor;
+    }
+  }
+
+  if (sumProperDivisors == num) {
+    console.log(`We have a perfect number!`);
+  } else {
+    console.log(`It\'s not so perfect.`);
+  }
+}
+perfectNum(6);
+perfectNum(6);
+perfectNum(1236498);
