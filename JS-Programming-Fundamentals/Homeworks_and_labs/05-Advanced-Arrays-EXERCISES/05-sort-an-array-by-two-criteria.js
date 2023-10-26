7419ab3ecb0fd67f6656a2c657d015f0b039647f
Boyan Kachamachkov <131705950+BoyanKachamachkov@users.noreply.github.com>
2023-10-26 19:24:15 +0300

    updated 5

--- JS-Programming-Fundamentals/Homeworks_and_labs/05-Advanced-Arrays-EXERCISES/05-sort-an-array-by-two-criteria.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/05-Advanced-Arrays-EXERCISES/05-sort-an-array-by-two-criteria.js
@@ -1,6 +1,11 @@
 function sorting(arr) {
   let sortedArr = arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
 
+  // if a.length - b.length gives 0 -> falsy value, then we evaluate the 2nd condition, and if its true, it will sort by it. In this case this is the localeCompare condition
+  // if we have left 0 and right 0 then we move to 2nd criteria (localeCompare)
+  // take a and compare it to b in alphabetical order(localeCompare)
+  // "щом дължината е еднаква, ще гледам азбучния ред" с 1 изречение
+
   console.log(sortedArr.join('\n'));
 }
 sorting(['alpha', 'beta', 'gamma']);
