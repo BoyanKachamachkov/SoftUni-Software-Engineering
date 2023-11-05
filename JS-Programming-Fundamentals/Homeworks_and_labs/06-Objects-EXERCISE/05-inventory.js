e96a7ea037e03f1fe39f70f9ecf16b8632a357af
Boyan Kachamachkov <kachamachkovboyan@gmail.com>
2023-11-05 15:14:04 +0200

    06-objects-exe-5-solved

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/06-Objects-EXERCISE/05-inventory.js
@@ -0,0 +1,40 @@
+function inventory(input) {
+  let result = [];
+
+  // work with input
+  // iterate
+  for (const line of input) {
+    let [name, level, items] = line.split(" / ");
+
+    // store data name->string, level->number, items->strings
+    let hero = {
+      name: name,
+      level: Number(level),
+      items: items,
+    };
+
+    result.push(hero);
+  }
+
+  // sort output by LEVEL(num) in ASC
+  let sortedResult = result.sort((a,b) => a.level - b.level);
+
+  for (const heroObject of sortedResult) {
+    
+console.log(`Hero: ${heroObject.name}`);
+console.log(`level => ${heroObject.level}`);
+console.log(`items => ${heroObject.items}`);
+  }
+
+}
+inventory([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
+
+// inventory([
+//     'Batman / 2 / Banana, Gun',
+//     'Superman / 18 / Sword',
+//     'Poppy / 28 / Sentinel, Antara'
+//     ])
