536942e99950dd25bd84decde6cb61f15ab3fb2f
Boyan Kachamachkov <kachamachkovboyan@gmail.com>
2023-11-05 15:23:43 +0200

    Objects-exe-7-solved

--- JS-Programming-Fundamentals/Homeworks_and_labs/06-Objects-EXERCISE/05-inventory.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/06-Objects-EXERCISE/05-inventory.js
@@ -17,15 +17,13 @@ function inventory(input) {
   }
 
   // sort output by LEVEL(num) in ASC
-  let sortedResult = result.sort((a,b) => a.level - b.level);
+  let sortedResult = result.sort((a, b) => a.level - b.level);
 
   for (const heroObject of sortedResult) {
-    
-console.log(`Hero: ${heroObject.name}`);
-console.log(`level => ${heroObject.level}`);
-console.log(`items => ${heroObject.items}`);
+    console.log(`Hero: ${heroObject.name}`);
+    console.log(`level => ${heroObject.level}`);
+    console.log(`items => ${heroObject.items}`);
   }
-
 }
 inventory([
   "Isacc / 25 / Apple, GravityGun",
