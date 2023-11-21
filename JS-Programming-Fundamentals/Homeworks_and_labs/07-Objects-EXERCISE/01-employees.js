3ffa74dfb63e4e1eb8ea4695a02419d62a8d3428
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-21 20:02:25 +0200

    solved 2

--- JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/01-employees.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/01-employees.js
@@ -1,20 +1,17 @@
 function employees(arr) {
+    let object = {};
 
+    for (let name of arr) {
+        object[name] = name.length;
+    }
 
-  let object = {};
+    // for in cycle for obj
 
-  for(let name of arr){
-
-    object[name] = name.length;
-  }
-
-  // for in cycle for obj
-
-  // kvp = key
-  // object[kvp] = value
-  for(kvp in object){
-    console.log(`Name: ${kvp} -- Personal Number: ${object[kvp]}`);
-  }
+    // kvp = key
+    // object[kvp] = value
+    for (kvp in object) {
+        console.log(`Name: ${kvp} -- Personal Number: ${object[kvp]}`);
+    }
 }
 employees([
     'Silas Butler',
--- JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/02-town.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/02-town.js
@@ -1,3 +1,15 @@
-function towns(arr) {}
+function towns(arr) {
+
+    let townInfo = {};
+
+    for(let string of arr){
+
+        let [name, latitude, longitude] = string.split(' | ');
+        townInfo.town = name;
+        townInfo.latitude = Number(latitude).toFixed(2);
+        townInfo.longitude = Number(longitude).toFixed(2);
+        console.log(townInfo);
+    }
+}
 
 towns(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']);
