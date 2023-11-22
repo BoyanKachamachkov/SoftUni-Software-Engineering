5d2420ee08e2a9ac82566e452cf69392c1ee72e0
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-22 07:54:13 +0200

    solved 3 and 4

--- JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/04-movies.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/04-movies.js
@@ -1,4 +1,37 @@
-function movieStore(arr) {}
+function movieStore(arr) {
+    let movies = [];
+
+    for (let line of arr) {
+        if (line.includes('addMovie')) {
+            let movieName = line.split('addMovie ')[1];
+            let movieObj = { name: movieName }; //store in obj
+            movies.push(movieObj); //push info to movies arr
+        } else if (line.includes('directedBy')) {
+            let [movieName, director] = line.split(' directedBy ');
+
+            // check the arr for exsisting obj key movie.name
+            let movie = movies.find((movie) => movie.name === movieName);
+
+            // if we have truthy value
+            if (movie) {
+                movie.director = director;
+            }
+        } else if (line.includes(' onDate ')) {
+            let [movieName, date] = line.split(' onDate ');
+
+            let movie = movies.find((movie) => movie.name === movieName);
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    }
+
+    for(let movie of movies){
+        if(movie.name && movie.director && movie.date){
+            console.log(JSON.stringify(movie))
+        }
+    }
+}
 
 movieStore([
     'addMovie Fast and Furious',
