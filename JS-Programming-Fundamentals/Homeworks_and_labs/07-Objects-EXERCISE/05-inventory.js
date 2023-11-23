659b9715390a7535aa3b79fb938fa41a9bf25a7c
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-23 08:51:22 +0200

    Updated 5

--- JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/05-inventory.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/07-Objects-EXERCISE/05-inventory.js
@@ -1,4 +1,33 @@
-function inventory(input) {}
+function inventory(input) {
+    // empty arr to save hero infos
+    let heroData = [];
+
+    for (let line of input) {
+        let [heroName, heroLevel, items] = line.split(' / ');
+
+        // Object for each hero data
+        let hero = {
+            name: heroName,
+            level: heroLevel,
+            items: items,
+        };
+
+        // push object to general arr
+        heroData.push(hero);
+    }
+
+    // sort output by LEVEL in ASCENDING (a - b)
+    let sortedHeroData = heroData.sort((a, b) => a.level - b.level);
+
+    // print format
+    for(let hero of sortedHeroData){
+
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`Items => ${hero.items}`);
+    }
+
+}
 inventory([
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
