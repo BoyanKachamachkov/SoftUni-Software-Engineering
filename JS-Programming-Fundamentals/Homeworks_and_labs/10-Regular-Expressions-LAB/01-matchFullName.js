5fddad4616e47537d81d01609ebc87913e2017ba
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-25 18:34:56 +0200

    updated tasks

--- JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-LAB/01-matchFullName.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-LAB/01-matchFullName.js
@@ -1,7 +1,9 @@
 function matchNames(text) {
-    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/gm;
+
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
 
     let match = text.match(pattern);
+
     console.log(match.join(' '));
 }
 
