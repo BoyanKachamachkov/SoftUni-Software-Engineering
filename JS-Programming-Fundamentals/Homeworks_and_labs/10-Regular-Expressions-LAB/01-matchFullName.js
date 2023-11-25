4c47fd08eb1ad51bc25f54e1be3e4f7d3d0cdf70
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-25 13:03:18 +0200

    1, solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-LAB/01-matchFullName.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-LAB/01-matchFullName.js
@@ -1,10 +1,10 @@
 function matchNames(text) {
-    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/gm;
 
-    let names = text.match(pattern);
-
-    console.log(names.join(' '));
+    let match = text.match(pattern)
+    console.log(match.join(' '));
 }
+
 
 matchNames(
     'Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov'
