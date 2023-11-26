477cb9b01ebb0f118afc0ca883f277999abfc312
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-26 11:05:08 +0200

    2 solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/02-race.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/02-race.js
@@ -1,22 +1,23 @@
 function race(arr) {
-    // first line list of partic, sep by ,
     let names = arr.shift().split(', ');
+    // console.log(names);
     let results = {};
     names.forEach((name) => (results[name] = 0));
+    // console.log(results);
 
-    let letterPattern = /[A-Za-z]/g;
-    //    let letterPattern = /[a-z]/gi;  works the same as above with the flag "i" = case insensitive
-
+    let letterPattern = /[A-Z[a-z]/g;
     let digitPattern = /\d/g;
 
     let command = arr.shift();
-    // while cycle until end of race
-    while (command != 'end of race') {
-        let letterMatches = command.match(letterPattern);
-        let name = letterMatches.join('');
+    // console.log(command);
 
-        let digitMatches = command.match(digitPattern);
-        let distance = digitMatches.map(Number).reduce((acc, val) => acc + val);
+    while (command != 'end of race') {
+        let letterMatch = command.match(letterPattern);
+        let name = letterMatch.join('');
+
+        let digitMatch = command.match(digitPattern);
+
+        let distance = digitMatch.map(Number).reduce((acc, val) => acc + val);
 
         if (name in results) {
             results[name] += distance;
@@ -25,15 +26,12 @@ function race(arr) {
         command = arr.shift();
     }
 
-    // we cannot sort object directly so we transform
-    let sortedResult = Object.entries(results).sort((a,b) => b[1] - a[1])
+    let sortedResults = Object.entries(results).sort((a, b) => b[1] - a[1]);
 
-    // nested array access [0] is our whole arr (name + distance), name is always firste lement, so it stays 0 for each
-    console.log(`1st place: ${sortedResult[0][0]}`);
-    console.log(`2nd place: ${sortedResult[1][0]}`);
-    console.log(`3rd place: ${sortedResult[2][0]}`);
+   console.log(`1st place: ${sortedResults[0][0]}`);
+   console.log(`2nd place: ${sortedResults[1][0]}`);
+   console.log(`3rd place: ${sortedResults[2][0]}`);
 }
-
 race([
     'George, Peter, Bill, Tom',
     'G4e@55or%6g6!68e!!@ ',
