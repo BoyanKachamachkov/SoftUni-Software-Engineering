f1939662271e3f7ae2162231280b52821630de1e
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-26 11:23:51 +0200

    2 removed comments

--- JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/02-race.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/02-race.js
@@ -1,15 +1,12 @@
 function race(arr) {
     let names = arr.shift().split(', ');
-    // console.log(names);
     let results = {};
     names.forEach((name) => (results[name] = 0));
-    // console.log(results);
 
     let letterPattern = /[A-Z[a-z]/g;
     let digitPattern = /\d/g;
 
     let command = arr.shift();
-    // console.log(command);
 
     while (command != 'end of race') {
         let letterMatch = command.match(letterPattern);
@@ -28,9 +25,9 @@ function race(arr) {
 
     let sortedResults = Object.entries(results).sort((a, b) => b[1] - a[1]);
 
-   console.log(`1st place: ${sortedResults[0][0]}`);
-   console.log(`2nd place: ${sortedResults[1][0]}`);
-   console.log(`3rd place: ${sortedResults[2][0]}`);
+    console.log(`1st place: ${sortedResults[0][0]}`);
+    console.log(`2nd place: ${sortedResults[1][0]}`);
+    console.log(`3rd place: ${sortedResults[2][0]}`);
 }
 race([
     'George, Peter, Bill, Tom',
