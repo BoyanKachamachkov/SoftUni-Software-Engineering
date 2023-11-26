a0520202afb3db11f7d54f5a44bd6c9f4d88f559
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-26 09:57:30 +0200

    re-solving 1

--- JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/01-furniture.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/10-Regular-Expressions-EXERCISES/01-furniture.js
@@ -26,21 +26,23 @@ function furniture(arr) {
     }
 
     console.log('Bought furniture:');
-    if (items.length > 0) {
-        console.log(items.join('\n'));
+    if (items.length > 1) {
+        for (let fur of items) {
+            console.log(fur);
+        }
     }
     console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
 }
 furniture(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']);
-// furniture([
-//     '>>Laptop<<312.2323!3',
-//     '>>TV<<300.21314!5',
-//     '>Invalid<<!5',
-//     '>>TV<<300.21314!20',
-//     '>>Invalid<!5',
-//     '>>TV<<30.21314!5',
-//     '>>Invalid<<!!5',
-//     'Purchase',
-// ]);
+furniture([
+    '>>Laptop<<312.2323!3',
+    '>>TV<<300.21314!5',
+    '>Invalid<<!5',
+    '>>TV<<300.21314!20',
+    '>>Invalid<!5',
+    '>>TV<<30.21314!5',
+    '>>Invalid<<!!5',
+    'Purchase',
+]);
 
-// furniture(['>Invalid<<!4', '>Invalid<<!2', '>Invalid<<!5', 'Purchase']);
+furniture(['>Invalid<<!4', '>Invalid<<!2', '>Invalid<<!5', 'Purchase']);
