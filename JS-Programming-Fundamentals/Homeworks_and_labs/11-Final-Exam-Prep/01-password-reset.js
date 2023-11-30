374a0a3bcf8c74ab487a56dfcd4fd178ac7d7e04
kachamachkov <kachamachkovboyan@gmail.com>
2023-11-30 11:46:09 +0200

    01-password-reset, fully solved

--- JS-Programming-Fundamentals/Homeworks_and_labs/11-Final-Exam-Prep/01-password-reset.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/11-Final-Exam-Prep/01-password-reset.js
@@ -5,33 +5,43 @@ function pwReset(input) {
     while (command != 'Done') {
         let tokens = command.split(' ');
         let action = tokens[0];
-        let newRawPw = '';
 
         switch (action) {
             case 'TakeOdd':
+                let newRawPw = '';
                 for (let i = 0; i < password.length; i++) {
                     if (i % 2 != 0) {
                         newRawPw += `${password[i]}`;
                     }
                 }
-                console.log(newRawPw);
+                password = newRawPw;
+                console.log(password);
                 break;
 
             case 'Cut':
                 let idx = Number(tokens[1]);
                 let length = Number(tokens[2]);
 
-                let substring = newRawPw.substring(idx, length)
-                newRawPw = newRawPw.replace(substring, '')
-                console.log(newRawPw);
+                let substring = password.substring(idx, idx + length);
+                password = password.replace(substring, '');
+                console.log(password);
+
                 break;
 
             case 'Substitute':
+                let oldValue = tokens[1];
+                let newValue = tokens[2];
+                if (password.includes(oldValue)) {
+                    password = password.replace(new RegExp(oldValue, 'g'), newValue);
+                    console.log(password);
+                } else {
+                    console.log('Nothing to replace!');
+                }
                 break;
         }
         command = input.shift();
     }
-    // console.log(`Your password is: ${password}`);
+    console.log(`Your password is: ${password}`);
 }
 pwReset([
     'up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy',
@@ -40,12 +50,14 @@ pwReset([
     'Substitute ! ***',
     'Substitute ? .!.',
     'Done',
+
 ]);
-// pwReset([
-//     'Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr',
-//     'TakeOdd',
-//     'Cut 15 3',
-//     'Substitute :: -',
-//     'Substitute | ^',
-//     'Done',
-// ]);
+console.log('---------');
+pwReset([
+    'Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr',
+    'TakeOdd',
+    'Cut 15 3',
+    'Substitute :: -',
+    'Substitute | ^',
+    'Done',
+]);
