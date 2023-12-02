10267cbd9a6169f26c3026ee0d7832b7dc04ea15
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-02 12:28:38 +0200

    solving practice exam

---
+++ JS-Programming-Fundamentals/Homeworks_and_labs/11-Final-Exam-Prep/Exam-prep-practice/problem1.js
@@ -0,0 +1,11 @@
+function chat(input) {
+    let message = input.shift();
+    console.log(message);
+}
+chat([
+    'heVVodar!gniV',
+    'ChangeAll:|:V:|:l',
+    'Reverse:|:!gnil',
+    'InsertSpace:|:5',
+    'Reveal',
+]);
