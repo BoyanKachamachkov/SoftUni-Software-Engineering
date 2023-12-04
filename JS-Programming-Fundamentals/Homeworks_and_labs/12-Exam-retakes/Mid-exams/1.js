980fe36d12f78bfdd21f8c45b193eb21a18e7586
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-04 17:27:25 +0200

    Update 1.js

--- JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/1.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/1.js
@@ -1,20 +1,35 @@
 function pig(arr) {
-    let food = Number(arr.shift()) / 1000;
-    let hay = Number(arr.shift()) / 1000;
-    let cover = Number(arr.shift()) / 1000;
-    let weight = Number(arr.shift()) / 1000;
-    let hayCover = weight / 0.33;
+    let food = Number(arr[0]) * 1000;
+    let hay = Number(arr[1]) * 1000;
+    let cover = Number(arr[2]) * 1000;
+    let weigth = Number(arr[3]) * 1000;
 
-    for (let i = 1; i < 30; i++) {
+    let coverChange = weigth / 3;
+
+    for (let i = 1; i <= 30; i++) {
         food -= 300;
-
         if (i % 2 == 0) {
             hay -= food * 0.05;
-        } else if (i % 3 == 0) {
-            hay -= hayCover;
+        }
+
+        if (i % 3 == 0) {
+            cover -= coverChange;
+        }
+
+        if (food <= 0 || hay <= 0 || cover <= 0) {
+            console.log('Merry must go to the pet store!');
+            return;
         }
     }
 
-    console.log(food, cover, hay);
+    food /= 1000;
+    hay /= 1000;
+    cover /= 1000;
+
+    console.log(
+        `Everything is fine! Puppy is happy! Food: ${food.toFixed(
+            2
+        )}, Hay: ${hay.toFixed(2)}, Cover: ${cover.toFixed(2)}.`
+    );
 }
 pig(['10', '5', '5.2', '1']);
