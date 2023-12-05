78f1b40ee87f081f6eeb2d5d1f2bf40add78b9da
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-05 21:35:10 +0200

    Solving 2

--- JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/02-mu-online.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/02-mu-online.js
@@ -1,12 +1,39 @@
 function mu(rooms) {
-    let hp = 100;
+    let health = 100;
     let bitcoin = 0;
 
-    let commands = rooms.split('|')
+    let commands = rooms.split('|');
 
     for (const room of commands) {
+        let tokens = room.split(' ');
+        let action = tokens[0];
+        let roomCount = 0;
+        roomCount++;
 
-        console.log(room);
+        if (action == 'potion') {
+            let amount = Number(tokens[1]);
+
+            if (health + amount > 100) {
+                amount = 100 - health;
+                health += amount;
+            } else {
+                health += amount;
+            }
+            console.log(`You healed for ${amount} hp.`);
+            console.log(`Current health: ${health} hp.`);
+        } else if (action == 'chest') {
+            let bitcoinFound = Number(tokens[1]);
+            console.log(`You found ${bitcoinFound} bitcoins.`);
+        } else {
+            let damage = Number(tokens[1])
+            health -= damage;
+            if(health > 0){
+                console.log(`You slayed ${action}`);
+            }else{
+                console.log(`You died! Killed by ${action}.`);
+                console.log(`Best room: ${roomCount}`);
+            }
+        }
         
     }
 }
