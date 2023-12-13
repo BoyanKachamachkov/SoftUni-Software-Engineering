762232a633e56804a0b473580b134177da2786cd
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-13 21:38:36 +0200

    Solved

--- JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Final-exams/01-solving-again.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Final-exams/01-solving-again.js
@@ -10,29 +10,34 @@ function emailValidation(arr) {
             email = email.toLowerCase();
             console.log(email);
         } else if (command.includes('GetDomain')) {
-            let tokens = command.split('GetDomain');
+            let tokens = command.split('GetDomain ');
             let count = Number(tokens[1]);
 
-            let substring = email.substring(email.lenght - count);
+            let substring = email.substring(email.length - count);
             console.log(substring);
         } else if (command.includes('GetUsername')) {
-            if(!email.includes('@')){
+            if (!email.includes('@')) {
                 console.log(`The email ${email} doesn't contain the @ symbol.`);
-            }else{
-                let index = email.indexOf('@')
-                let substring = email.substring(0,index)
+            } else {
+                let index = email.indexOf('@');
+                let substring = email.substring(0, index);
 
                 console.log(substring);
             }
         } else if (command.includes('Replace')) {
-            let tokens = command.split('Replace')
+            let tokens = command.split('Replace ');
             let char = tokens[1];
 
-            email = email.split(char).join('-')
+            email = email.split(char).join('-');
             console.log(email);
-
-
         } else if (command.includes('Encrypt')) {
+            let emptyArr = [];
+
+            for (let i = 0; i < email.length; i++) {
+                let char = email.charCodeAt(i);
+                emptyArr.push(char);
+            }
+            console.log(emptyArr.join(' '));
         }
         command = arr.shift();
     }
@@ -44,3 +49,19 @@ emailValidation([
     'Replace a',
     'Complete',
 ]);
+
+emailValidation([
+    'Mike123@somemail.com',
+    'Make Upper',
+    'GetDomain 3',
+    'GetUsername',
+    'Encrypt',
+    'Complete',
+]);
+
+emailValidation(["Another@Mail.com",
+"Make Lower",
+"GetUsername",
+"GetDomain 3",
+"Encrypt",
+"Complete"])
