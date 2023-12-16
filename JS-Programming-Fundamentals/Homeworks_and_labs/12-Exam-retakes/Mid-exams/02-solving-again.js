ef0d0959c66b1af0149438f19d90b0def83d9c6f
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-16 21:33:48 +0200

    2 solved again

--- JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/02-solving-again.js
+++ JS-Programming-Fundamentals/Homeworks_and_labs/12-Exam-retakes/Mid-exams/02-solving-again.js
@@ -12,15 +12,65 @@ function solve(arr) {
             // console.log(numbers);
 
             nums = numbers.concat(nums);
-            console.log(nums);
-        } else if (command.includes('remove greater than'))
-            command = arr.shift();
+        } else if (command.includes('remove greater than')) {
+            let value = command.split('remove greater than ')[1];
+            nums = nums.filter((el) => el <= value);
+        } else if (command.includes('replace')) {
+            let tokens = command.split('replace ');
+            let numbersFromTokens = tokens[1].split(' ').map(Number);
+            let value = numbersFromTokens[0];
+            let replacement = numbersFromTokens[1];
+
+            let index = nums.indexOf(value);
+            if (index != -1) {
+                nums[index] = replacement;
+            } else {
+                command = arr.shift();
+                continue;
+            }
+        } else if (command.includes('remove at index')) {
+            let num = command.split('remove at index ').map(Number)[1];
+
+            if (num >= 0 && num < nums.length) {
+                let remove = nums.indexOf(num);
+                nums.splice(remove, 1);
+            } else {
+                command = arr.shift();
+                continue;
+            }
+        } else if (command.includes('find even')) {
+            let even = nums.filter((num) => num % 2 == 0);
+            console.log(even.join(' '));
+        } else if (command.includes('find odd')) {
+            let odd = nums.filter((num) => num % 2 != 0);
+            console.log(odd.join(' '));
+        }
+        command = arr.shift();
     }
+
+    console.log(nums.join(', '));
 }
 solve([
     '1 2 3 10 10 6 4 10',
     'add to start 1 2 3',
     'remove greater than 5',
     'find even',
+    'END',
+]);
+console.log('----');
+
+solve([
+    '1 2 3 10 10 6 4 10',
+    'replace 10 1',
+    'remove at index 0',
+    'add to start 4 2',
+    'END',
+]);
+console.log('----');
+solve([
+    '1 2 3 10 10 6 4 10',
+    'find odd',
+    'replace 4 1',
+    'remove greater than 5',
     'END',
 ]);
