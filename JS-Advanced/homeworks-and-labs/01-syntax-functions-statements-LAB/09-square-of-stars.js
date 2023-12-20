e0b3a86ada1881de2935067a86f2a65e2402c8b2
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-20 11:53:59 +0200

    01-lab-09-solved

---
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/09-square-of-stars.js
@@ -0,0 +1,12 @@
+function solve(num) {
+    let star = '* ';
+
+    for (let i = 0; i < num; i++) {
+        console.log(star.repeat(num));
+    }
+}
+
+// solve(1);
+// solve(2);
+solve(5);
+// solve(7);
