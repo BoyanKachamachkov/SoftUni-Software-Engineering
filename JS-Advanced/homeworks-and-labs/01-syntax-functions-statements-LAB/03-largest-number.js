5741c4f581b813db315611cb9cf3b59d074a116e
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-20 09:23:19 +0200

    01-lab-03-solved

---
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/03-largest-number.js
@@ -0,0 +1,8 @@
+function solve(arg1, arg2, arg3) {
+    let largest = Math.max(arg1, arg2, arg3);
+
+    console.log(`The largest number is ${largest}.`);
+}
+
+solve(5, -3, 16);
+solve(-3, -5, -22.5);
