3325aa08bf51b565362a63791323830832cdcbf8
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-23 20:07:45 +0200

    01-exe-05-solved

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
@@ -2,10 +2,17 @@ function timeToWalk(steps, footprint, speed) {
 	let distanceInMeters = steps * footprint;
 	let speedInMetersPerSec = speed / 3.6;
 	let restSeconds = Math.floor(distanceInMeters / 500) * 60;
+	let timeInSeconds = distanceInMeters / speedInMetersPerSec + restSeconds;
 
-	
-	console.log({ distanceInMeters });
-	console.log({ speedInMetersPerSec });
-	console.log({ restSeconds });
+	let timeHr = Math.floor(timeInSeconds / 3600);
+	let timeMin = Math.floor(timeInSeconds / 60);
+	let timeSec = Math.round(timeInSeconds % 60);
+
+	console.log(
+		`${timeHr < 10 ? '0' : ''}${timeHr}:${
+			timeMin < 10 ? '0' : ''
+		}${timeMin}:${timeSec}`
+	);
 }
 timeToWalk(4000, 0.6, 5); //00:32:48
+timeToWalk(2564, 0.7, 5.5);
