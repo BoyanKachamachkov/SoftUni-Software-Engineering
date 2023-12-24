807464e49dc6391b1e56ecb67197ca33296d7824
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-24 11:33:58 +0200

    01-exe-06-solved

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/06-road-radar.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/06-road-radar.js
@@ -2,27 +2,42 @@ function roadRadar(speed, area) {
 	let limit = 0;
 
 	switch (area) {
-		case 'resential':
-      limit = 20;
+		case 'residential':
+			limit = 20;
 			break;
-
 		case 'city':
-      limit = 50;
+			limit = 50;
 			break;
-
 		case 'interstate':
-      limit = 90;
+			limit = 90;
 			break;
-
 		case 'motorway':
-      limit = 130;
+			limit = 130;
 			break;
 	}
+	const speeding = speed - limit;
 
-  console.log(area);
+	if (speeding <= 0) {
+		console.log(`Driving ${speed} km/h in a ${limit} zone`);
+		return;
+	}
+
+	let status = '';
+	if (speeding <= 20) {
+		status = 'speeding';
+	} else if (speeding <= 40) {
+		status = 'excessive speeding';
+	} else {
+		status = 'reckless driving';
+	}
+
+	let difference = speed - limit;
+	console.log(
+		`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`
+	);
 }
 
-roadRadar(40, 'city');
+// roadRadar(40, 'city');
 roadRadar(200, 'motorway');
 roadRadar(120, 'interstate');
 roadRadar(21, 'residential');
