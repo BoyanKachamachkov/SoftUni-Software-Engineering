a8d29502c0b7964c036a2a50e25f3fc526ccc5b3
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-27 15:31:01 +0200

    solve 04-lab-03

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
@@ -1,19 +1,12 @@
 function extractText() {
-	// TODO
+    const list = document.getElementById('items');
+    const items = Array.from(list.children);
 
-	// 1. find all elements
-	// 2. extract their information
-	// 3. put the string inside the input
+    const result = items
+    .map(li => li.textContent)
+    .join('\n');
 
-	const els = document.getElementById('items');
-	// console.log(els); //returns ITEMS
+    const output = document.getElementById('result');
+    output.value = result;
 
-	const items = document.getElementById('items').children; //returns HTML collections with the childs
-
-	// console.log(items);
-
-	const itemArray = Array.from(items);
-	const result = itemArray.map((li) => li.textContent).join('\n');
-
-	document.getElementById('result').value = result;
 }
