f114ba040da77d6a5aa50d3ccaba884dc3099a64
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-27 08:47:43 +0200

    solve 04-lab-01 again

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/01.Edit Element/EditElement.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/01.Edit Element/EditElement.js
@@ -1,6 +1,8 @@
 function editElement(ref, match, replacement) {
-    let pattern = new RegExp(match, 'g'); //save all matches with G flag to this var
-	const text = ref.textContent; //connect with the ref text
-	const result = text.replace(pattern, replacement); //replace all pattern matches
-	ref.textContent = result; //establish the new ref value
+	// doing again
+	const text = ref.textContent;
+	console.log(text);
+    const pattern = new RegExp(match, 'g')
+    const result = text.replace(pattern, replacement)
+    ref.textContent = result;
 }
