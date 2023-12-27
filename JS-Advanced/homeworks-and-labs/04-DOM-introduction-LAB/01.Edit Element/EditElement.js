e40d0c36c0c645ecb9eb5ca8a11ceb1123703e4d
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-27 08:51:30 +0200

    04-lab-01 2 solutions

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/01.Edit Element/EditElement.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/01.Edit Element/EditElement.js
@@ -1,8 +1,14 @@
 function editElement(ref, match, replacement) {
-	// doing again
-	const text = ref.textContent;
-	console.log(text);
-    const pattern = new RegExp(match, 'g')
-    const result = text.replace(pattern, replacement)
+	// SOL 1
+	// const text = ref.textContent;
+	// console.log(text);
+    // const pattern = new RegExp(match, 'g')
+    // const result = text.replace(pattern, replacement)
+    // ref.textContent = result;
+
+    // SOL 2
+    const text = ref.textContent;
+    const result = text.split(match).join(replacement);
     ref.textContent = result;
+    
 }
