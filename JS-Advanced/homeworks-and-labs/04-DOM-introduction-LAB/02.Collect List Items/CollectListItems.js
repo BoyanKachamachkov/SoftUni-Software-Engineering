a0961620a9f53c7981761e955540bd6761aa0f44
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-27 14:22:05 +0200

    solve-04-lab-02

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,19 @@
 function extractText() {
-    // TODO
+	// TODO
+
+	// 1. find all elements
+	// 2. extract their information
+	// 3. put the string inside the input
+
+	const els = document.getElementById('items');
+	// console.log(els); //returns ITEMS
+
+	const items = document.getElementById('items').children; //returns HTML collections with the childs
+
+	// console.log(items);
+
+	const itemArray = Array.from(items);
+	const result = itemArray.map((li) => li.textContent).join('\n');
+
+	document.getElementById('result').value = result;
 }
