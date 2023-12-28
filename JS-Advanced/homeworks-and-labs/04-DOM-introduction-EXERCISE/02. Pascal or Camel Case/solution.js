0b41469979050a5fe9bc88b82e793f3838e374ad
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-28 12:10:40 +0200

    solve 04-exe-02 mid-way

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+	// select first param
+	let text = document.getElementById('text').value;
+	console.log(text);
+
+	// select second param
+	let changer = document.getElementById('naming-convention').value;
+	console.log(changer);
+
+	let output = '';
+  text = text.toLowerCase();
+	console.log(text);
+
+	if (changer == 'Pascal Case') {
+
+
+
+	} else if (changer == 'Camel Case') {
+
+
+
+	} else {
+		output = 'Error!';
+	}
 }
