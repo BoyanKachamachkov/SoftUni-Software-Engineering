6c566d433b2aa0397edc2031236c37041280fa11
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-29 11:13:02 +0200

    re-solve 04-exe-01

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/01. Subtraction/subtract.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/01. Subtraction/subtract.js
@@ -2,6 +2,8 @@ function subtract() {
 	const num1 = document.getElementById('firstNumber').value;
 	const num2 = document.getElementById('secondNumber').value;
 
-	const result = Number(num1) - Number(num2);
-	document.getElementById('result').textContent = result;
+	const sum = Number(num1) - Number(num2);
+
+	const output = document.getElementById('result');
+	output.textContent = sum;
 }
