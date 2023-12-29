15844baeb26880bffeaf3b08018f8ac1376cd8b9
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-29 17:23:28 +0200

    solve-DOM-exe-04-mid-way

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
@@ -1,3 +1,9 @@
 function search() {
-   // TODO
+	let searchElement = document.getElementById('searchText');
+
+	let allLiElements = Array.from(document.querySelectorAll('#towns li'));
+
+	let searchText = searchElement.value;
+
+	allLiElements.forEach((el) => (el.style['font-weight'] = 'normal'));
 }
