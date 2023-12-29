2980615270ee156fe9c86adfc9f659a7a9034247
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-29 12:30:29 +0200

    DOM-04-exe-03

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/03. Accordion/accordion.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+	// 1. Select elements
+	let btn = document.getElementsByClassName('button')[0];
+	let text = document.getElementById('extra');
+
+	// show/hide text and change button text
+	// change button text
+	btn.textContent = btn.textContent === 'More' ? 'Less' : 'More';
+
+	// show/hide content
+	text.style.display = text.style.display === 'block' ? 'none' : 'block';
 }
