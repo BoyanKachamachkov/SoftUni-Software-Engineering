6c1e87e0c466178ae7dde2b3e8b2fc6bf3af90f2
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-05 17:01:40 +0200

    Solve 04-DOM-lab-p2

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/02.Collect List Items/CollectListItems.js
@@ -1,10 +1,20 @@
+/*
+Write a JS function that scans a given HTML list and appends all collected list items’ text to a textarea on the same page when the user clicks on a button.
+*/
 function extractText() {
+	let list = document.getElementById('items');
+	console.log(list); // returns ul element
 
-  const list = document.getElementById('items');
-  const items = Array.from(list.children);
+	let items = list.children;
+	console.log(items); // returns HTML coll. of LIs
 
-  const output = items.map(li => li.textContent).join('\n');
+	items = Array.from(items);
+	console.log(items); // ret. 3 LI els.
 
-  const result = document.getElementById('result')
-  result.value = output;
+	const output = items.map((li) => li.textContent).join('\n');
+	console.log(output); //ret. text from each item on new line
+
+	const result = document.getElementById('result');
+	console.log(result); //ret. res. fied
+	result.value = output; //set res. field text
 }
