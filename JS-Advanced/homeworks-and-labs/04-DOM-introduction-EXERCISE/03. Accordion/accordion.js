12b67c75b3a7167805440d00d1c40613697781d7
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-06 16:13:07 +0200

    Solve 04-DOM-exe-p3

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/03. Accordion/accordion.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/03. Accordion/accordion.js
@@ -1,12 +1,7 @@
 function toggle() {
-	// 1. Select elements
 	let btn = document.getElementsByClassName('button')[0];
 	let text = document.getElementById('extra');
 
-	// show/hide text and change button text
-	// change button text
 	btn.textContent = btn.textContent === 'More' ? 'Less' : 'More';
-
-	// show/hide content
-	text.style.display = text.style.display === 'block' ? 'none' : 'block';
+    text.style.display = text.style.display === 'block' ? 'none' : 'block'
 }
