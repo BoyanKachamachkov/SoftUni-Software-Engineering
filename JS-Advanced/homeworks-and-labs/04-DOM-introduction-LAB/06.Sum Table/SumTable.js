c3f81c6ad193b9fd13eaa22e487117911edb2c9f
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-06 09:49:27 +0200

    Solve 04-DOM-lab-p6

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/06.Sum Table/SumTable.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-LAB/06.Sum Table/SumTable.js
@@ -1,11 +1,8 @@
 function sumTable() {
+	const tdEls = Array.from(document.querySelectorAll('td:nth-child(2)'));
 
-  const costs = document.querySelectorAll('td:nth-child(2)')
-
-  const arrCosts = Array.from(costs)
-  const lastEl = arrCosts.pop();
-  const sum = arrCosts.reduce((a,v) => a + Number(v.textContent),0);
-
-  const output = document.getElementById('sum')
-  output.textContent = sum;
+	tdEls.pop();
+	const outputSum = tdEls.reduce((a, v) => a + Number(v.textContent), 0);
+	const sumEl = document.getElementById('sum');
+	sumEl.textContent = outputSum;
 }
