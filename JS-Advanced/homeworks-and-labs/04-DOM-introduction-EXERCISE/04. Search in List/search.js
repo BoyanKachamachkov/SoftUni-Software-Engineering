4ba8312ce5b19faefc0eef393b6c6099b70363c3
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-07 16:02:45 +0200

    Prepare pseudocode 04-DOM-exe-p4

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
@@ -1,12 +1,29 @@
+/* 
+An HTML page holds a list of towns, a search box, and a [Search] button. Implement the search function to bold and underline the items from the list which include the text from the search box. Also, print the number of items the current search matches in the format `${matches} matches found`.
+Note: It is necessary to clear the results of the previous search.
+Write your JavaScript code in this file:
+ */
+
 function search() {
-	let searchElement = document.getElementById('searchText');
+	/* 
+	1.	Target the input field with a variable
+	2.	Target the result ID for the end output
 
-	let allLiElements = Array.from(document.querySelectorAll('#towns li'));
+	3.	Extract all list elements's text
+			queryAll + li
+			transfer to arr
 
-	let searchText = searchElement.value;
+	4.	Iterate through all items
+			-transfer to TEXT CONTENT of the item
+			-use counter 
+			includes searched text?
+				true?
+				-underline (style.fontWeight) + style.textDecoration
 
-	allLiElements.forEach((el) => (el.style['font-weight'] = 'normal'));
+				false?
+				-reset styles to prepare for new search
+
+	5. Set new value of the result 
+
+	*/
 }
-
-
-
