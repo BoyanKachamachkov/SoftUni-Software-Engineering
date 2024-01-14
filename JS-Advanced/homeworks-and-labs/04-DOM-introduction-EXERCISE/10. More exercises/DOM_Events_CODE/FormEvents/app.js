c638400087147d3c5dbca7e83cf176b49e68684a
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-14 16:54:41 +0200

    Practice DOM forms and events

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/10. More exercises/DOM_Events_CODE/FormEvents/app.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/10. More exercises/DOM_Events_CODE/FormEvents/app.js
@@ -1,4 +1,29 @@
 const tweetForm = document.querySelector('#tweetForm');
+const tweets = document.querySelector('#tweets');
+
 tweetForm.addEventListener('submit', function (e) {
-	alert('SUBMITTED!');
+	// Prevent the default event of sending the form so we can use it
+	e.preventDefault();
+
+	// comment value old way!
+	// const usernameInput = document.querySelectorAll('input')[0].value;
+	// const tweetInput = document.querySelectorAll('input')[1].value;
+
+	// Check elements contentes
+	const username = tweetForm.elements.username;
+	const tweet = tweetForm.elements.tweet;
+
+	addTweet(username.value, tweet.value);
+	username.value = '';
+	tweet.value = '';
 });
+
+const addTweet = (username, tweet) => {
+	// create new Tweet, append everything to it
+	const newTweet = document.createElement('li');
+	const bTag = document.createElement('b');
+	bTag.append(username);
+	newTweet.append(bTag);
+	newTweet.append(` - ${tweet}`);
+	tweets.append(newTweet);
+};
