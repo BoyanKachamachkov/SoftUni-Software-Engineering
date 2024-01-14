8a1a58f1cf88aedb3ab7afe132b6febb624c1d3c
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-14 10:51:47 +0200

    Add more exercises from Udemy in DOM

---
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/10. More exercises/DOM_Intro_CODE/Pokemon/app.js
@@ -0,0 +1,20 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+/* 
+	-select container
+	-create baseURL link
+	-loop over all the pokomens
+
+	for each pokemon
+		-create div element
+		-create img element
+		-create span element
+
+		-assign src to img with logic
+		-assign text to span with logic
+		-assign class to both
+		-append both img and span to the new div
+
+		-finally append div to the container!
+*/
+
+const container = 
