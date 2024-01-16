69a408b23b268097853205b8f4506fc9fd119db4
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-16 12:11:38 +0200

    Add logic for player 1 and 2, ping-pong, game over

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/10. More exercises/ScoreKeeper/app.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/10. More exercises/ScoreKeeper/app.js
@@ -6,12 +6,25 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 
+let winningScore = 5;
+let isGameOver = false;
+
 p1Button.addEventListener('click', function () {
-	p1Score++;
-	p1Display.innerText = p1Score;
+	if (!isGameOver) {
+		p1Score++;
+		if (p1Score === winningScore) {
+			isGameOver = true;
+		}
+		p1Display.innerText = p1Score;
+	}
 });
 
 p2Button.addEventListener('click', function () {
-	p2Score++;
-	p2Display.innerText = p2Score;
+	if (!isGameOver) {
+		p2Score++;
+		if (p2Score === winningScore) {
+			isGameOver = true;
+		}
+		p2Display.innerText = p2Score;
+	}
 });
