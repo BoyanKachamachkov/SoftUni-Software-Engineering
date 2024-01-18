c1628b3f7527f1dcb70355061682d4ddc5f5736f
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-18 17:51:35 +0200

    Solve 100/100 01-P5

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
@@ -1,21 +1,18 @@
 function timeToWalk(stepsCount, footPrintMeters, speedKmh) {
-	// every 500 meters , + 1 minute for break
+	let distanceMeters = stepsCount * footPrintMeters;
+	let speedMetersPerSec = speedKmh / 3.6;
+	let restSeconds = Math.floor(distanceMeters / 500) * 60;
+	let timeInSeconds = distanceMeters / speedMetersPerSec + restSeconds;
 
-	let output = '';
+	let timeHr = Math.floor(timeInSeconds / 3600);
+	let timeMin = Math.floor(timeInSeconds / 60);
+	let timeSec = Math.round(timeInSeconds % 60);
 
-	let totalDistanceMeters = stepsCount * footPrintMeters;
-	console.log(totalDistanceMeters);
-
-	let speedMetersSec = speedKmh / 3.6;
-	console.log({ speedMetersSec });
-
-	let totalTimeNeededMinutes = totalDistanceMeters / speedMetersSec / 60;
-	console.log(totalTimeNeededMinutes);
-
-    let delays = Math.floor(totalDistanceMeters / 500)
-    console.log(delays);
-
-    
+	console.log(
+		`${timeHr < 10 ? '0' : ''}${timeHr}:${
+			timeMin < 10 ? '0' : ''
+		}${timeMin}:${timeSec}`
+	);
 }
 
 timeToWalk(4000, 0.6, 5);
