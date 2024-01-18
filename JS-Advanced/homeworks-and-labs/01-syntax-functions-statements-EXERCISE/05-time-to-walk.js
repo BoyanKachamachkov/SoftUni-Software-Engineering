00f1f20bdcc84c0f9212d6d6aa8ede89a42ad099
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-18 17:36:35 +0200

    Solve 01-p5 mid-way

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/05-time-to-walk.js
@@ -1,9 +1,21 @@
-function timeToWalk(steps, stepsInMeters, speedKmh) {
-	const distanceMeters = steps * stepsInMeters; //2600 metres to uni.
-	const speedMeterSec = speedKmh / 3.6;
+function timeToWalk(stepsCount, footPrintMeters, speedKmh) {
+	// every 500 meters , + 1 minute for break
 
-    const totalTimeSec = distanceMeters * speedMeterSec /60
-    console.log(totalTimeSec);
+	let output = '';
+
+	let totalDistanceMeters = stepsCount * footPrintMeters;
+	console.log(totalDistanceMeters);
+
+	let speedMetersSec = speedKmh / 3.6;
+	console.log({ speedMetersSec });
+
+	let totalTimeNeededMinutes = totalDistanceMeters / speedMetersSec / 60;
+	console.log(totalTimeNeededMinutes);
+
+    let delays = Math.floor(totalDistanceMeters / 500)
+    console.log(delays);
+
+    
 }
-timeToWalk(4000, 0.6, 5); //00:32:48
-// timeToWalk(2564, 0.7, 5.5);
+
+timeToWalk(4000, 0.6, 5);
