e1441f098af8a55aaa3c39e78937ba461cba5bd0
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-19 13:04:24 +0200

    Solve 04-DOM-p4

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/04. Search in List/search.js
@@ -1,37 +1,17 @@
 function search() {
-	/* 
-	1.	Target the input field with a variable
+	let searchInput = document.querySelector('#searchText').value;
+	const liEls = Array.from(document.querySelectorAll('li'));
 
-	2.	Extract all list elements's text
-			queryAll + li
-			transfer to arr
-
-	3.		Iterate through all items
-			-transfer to TEXT CONTENT of the item
-			-use counter 
-			includes searched text?
-				true?
-				-underline (style.fontWeight) + style.textDecoration
-
-				false?
-				-reset styles to prepare for new search
-
-	4. Set new value of the result 
-
-	*/
-
-	let searchFieldText = document.getElementById('searchText').value;
-	let items = Array.from(document.querySelectorAll('#towns li'));
-	let counter = 0;
-
-	items.forEach((item) => {
-		if (item.textContent.includes(searchFieldText)) {
-			item.style.fontWeight = 'bold';
-			item.style.textDecoration = 'underline';
-			return counter++;
+	let matches = 0;
+	for (const li of liEls) {
+		if (li.textContent.includes(searchInput)) {
+			matches++;
+			li.style.textDecoration = 'underline';
+			li.style.fontWeight = 'bold';
 		}
-		item.style.fontWeight = '';
-		item.style.textDecoration = 'none';
-	});
-	document.getElementById('result').textContent = `${counter} matches found`;
+	}
+
+	const resultEl = document.getElementById('result');
+	let output = `${matches} matches found`;
+	resultEl.textContent = output;
 }
