6a6a2861a91b24b71fbe0db6647a722ecc568e14
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-19 09:59:07 +0200

    Solve DOM p1

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/01. Subtraction/subtract.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/01. Subtraction/subtract.js
@@ -1,9 +1,8 @@
 function subtract() {
 	const firstNumEl = document.getElementById('firstNumber').value;
 	const secNumEl = document.getElementById('secondNumber').value;
-
-	const resultEl = document.getElementById('result');
+	const result = document.getElementById('result');
 
 	const output = Number(firstNumEl) - Number(secNumEl);
-	resultEl.textContent = output;
+	result.textContent = output;
 }
