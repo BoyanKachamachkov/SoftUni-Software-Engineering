d297e6b46b7e084b6b862e2a255cdd63a0ade093
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-21 18:37:24 +0200

    Solve 04-DOM-exe-p5

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/05. Table - Search Engine/solution.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/05. Table - Search Engine/solution.js
@@ -1,12 +1,29 @@
 function solve() {
+	document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-	// check all cells for input string
-	// if match(full words or single letters)
-	// 		- if any row contains it -> add class select to row
-	// 		- if NO match -> nothing happens
-	// after every search
-	//		- clear input field
-	// 		- remove all already selected classes
+	function onClick() {
+		//   TODO:
+		// check all cells for input string
+		// if match(full words or single letters) check lower, lower
+		// 		- if any row contains it -> add class select to row
+		// 		- if NO match -> nothing happens
+		// after every search
+		//		- clear input field
+		//
 
-	
+		const inputFieldEl = document
+			.getElementById('searchField')
+			.value.toLowerCase();
+		const rows = document.querySelectorAll('tbody tr');
+
+		for (let row of rows) {
+			const rowTextToLower = row.textContent.toLowerCase();
+
+			if (rowTextToLower.includes(inputFieldEl)) {
+				row.setAttribute('class', 'select');
+			} else {
+				row.removeAttribute('class');
+			}
+		}
+	}
 }
