c4d2554e507a896ce408f403cee9d2669b7b2914
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-21 19:00:42 +0200

    Add pseudo-code to 04-DOM-exe-p6

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/06. Format the Text/solution.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+	//TODO
+	/* 
+  target textarea
+  split by .
+  check if every sentence has at least 1 char
+
+  generate html Ps
+  use interpolation string `<p> {text} </p>`
+
+  append to div with ID "output"
+  */
+
+	const textArea = Array.from(document.getElementById('input').value);
+
+	debugger;
 }
