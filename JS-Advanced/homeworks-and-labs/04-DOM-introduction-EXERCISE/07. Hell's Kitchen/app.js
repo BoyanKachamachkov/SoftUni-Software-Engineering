6713ecd1f0fd18f2bfd5e661cff414bf80d66221
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-22 12:28:59 +0200

    Solve DOM exe p7, mid-way

--- JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/07. Hell's Kitchen/app.js
+++ JS-Advanced/homeworks-and-labs/04-DOM-introduction-EXERCISE/07. Hell's Kitchen/app.js
@@ -1,8 +1,42 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+	document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+	function onClick() {
+		//   TODO:
+
+		// 1. select elements for input
+		const textAreaInput = document.querySelector('textarea');
+
+		// 2. extract/parase input data
+		let text = textAreaInput.value;
+		let inputArr = JSON.parse(text);
+
+		// 3. add object to keep restaurant and workers data
+		let restaurants = {};
+
+		// 4. fill restaurant object with input data
+		for (let index = 0; index < inputArr.length; index++) {
+			let [restaurantName, workersString] = inputArr[index].split(' - ');
+			let workers = workersString.split(', ').map((w) => {
+				let [name, salary] = w.split(' ');
+				return { name, salary: Number(salary) };
+			});
+
+			if (!restaurants[restaurantName]) {
+				restaurants[restaurantName] = {
+					workers: [],
+					getAverageSalary: function () {
+						return (
+							this.workers.reduce((acc, el) => acc + el, 0) /
+							this.workers.length
+						);
+					},
+				};
+			}
+		}
+
+		// 5. calculate best restaurant
+
+		// 6. insert best restaurant and workers into DOM
+	}
 }
