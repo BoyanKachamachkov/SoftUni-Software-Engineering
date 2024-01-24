c4f876161a523a0910c35209e255c1fc7c2257b2
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-24 10:08:53 +0200

    Solve 05-DOM-p1-lab

--- JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/01. List-Of-Items/app.js
+++ JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/01. List-Of-Items/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+	// 1. Select input and read it
+	// 2. create li element
+	// 3. select the exsisting ul ID
+	// 4. append newly created element to the UL as a child
+
+	const input = document.querySelector('#newItemText').value;
+	const newLiElement = document.createElement('li');
+	newLiElement.textContent = input;
+
+	const ulEl = document.querySelector('#items');
+	ulEl.appendChild(newLiElement);
+
+    // ! clear input in the end
+	document.querySelector('#newItemText').value = ''
 }
