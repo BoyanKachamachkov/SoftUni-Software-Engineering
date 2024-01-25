bdec4c4c6b7f48a4f39e38d78b00b63b8bfd777f
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-25 10:08:04 +0200

    Progress DOM lab p3

--- JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/02. Delete-From-Table/app.js
+++ JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/02. Delete-From-Table/app.js
@@ -23,3 +23,28 @@ function deleteByEmail() {
 	// if match is true ,  textContent of result -> "deleted"
 	// if not found , textContent -> not found.
 }
+function deleteByEmail() {
+	// select and gather input info
+	const input = document.getElementsByName('email')[0].value;
+	const rows = document.querySelectorAll('#customers tr td:nth-child(2)');
+
+	// loop over exsisting info to check for matches
+
+	for (let row of rows) {
+		console.log(row);
+
+		if (row.textContent == input) {
+			let rowElement = row.parentNode;
+			rowElement.parentNode.removeChild(rowElement);
+			document.getElementById('result').textContent = 'Deleted.';
+			document.getElementsByName('email')[0].value = '';
+
+			return;
+		}
+		document.getElementById('result').textContent = 'Not found.';
+	}
+	document.getElementsByName('email')[0].value = '';
+
+	// if match is true ,  textContent of result -> "deleted"
+	// if not found , textContent -> not found.
+}
--- JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/03. Add-Delete/app.js
+++ JS-Advanced/homeworks-and-labs/05-DOM-Manipulation-and-Events-LAB/03. Add-Delete/app.js
@@ -1,3 +1,14 @@
 function addItem() {
-    //TODO...
+	//TODO...
+
+	// slect input
+
+	const input = document.querySelector('#newItemText').value;
+	console.log(input);
+	const items = document.querySelector('#items');
+
+	const newLi = document.createElement('li');
+	newLi.textContent = input;
+	items.appendChild(newLi);
+
 }
