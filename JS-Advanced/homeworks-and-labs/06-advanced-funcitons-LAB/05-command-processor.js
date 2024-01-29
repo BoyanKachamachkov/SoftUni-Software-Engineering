b4883f0f1d744094f44687865ac356c39632bf49
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-29 19:18:44 +0200

    Prepare files and folders for tasks

--- JS-Advanced/homeworks-and-labs/06-advanced-funcitons-LAB/05-command-processor.js
+++ JS-Advanced/homeworks-and-labs/06-advanced-funcitons-LAB/05-command-processor.js
@@ -1,22 +1,4 @@
-function solution() {
-	let result = '';
 
-	let print = function () {
-		console.log(result);
-	};
-	return {
-		append: function (text) {
-			result += text;
-		},
-		removeStart(n) {
-			result = result.substring(n);
-		},
-		removeEnd: (n) => {
-			result = result.substring(0, result.length - n);
-		},
-		print, //same as print: print
-	};
-}
 
 let firstZeroTest = solution();
 
