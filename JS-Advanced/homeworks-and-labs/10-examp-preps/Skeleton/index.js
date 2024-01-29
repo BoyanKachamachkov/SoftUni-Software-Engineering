c13c7166e8dc7a61116b0338a38b4e5dbbbfe486
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-29 20:03:53 +0200

    Progress DOM task, exam prep

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Skeleton/index.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Skeleton/index.js
@@ -1,3 +1,16 @@
 function solve() {
-    console.log('TODO');
-};
+	let buttonElement = document.querySelector('.admin-view .action button');
+
+	buttonElement.addEventListener('click', (e) => {
+		// stop reloading the page (natural behaviour)
+		e.preventDefault();
+
+		// input + unique name? --> input[name=""]
+		let lectureNameElement = document.querySelector('input[name="lecture-name"]');
+		let lectureDateElement = document.querySelector('input[name="lecture-date"]');
+		let lectureModuleElement = document.querySelector('select[name="lecture-module"]')
+
+
+		console.log(lectureModuleElement);
+	});
+}
