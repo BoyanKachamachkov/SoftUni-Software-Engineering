5713883bbb8c33eef43f09d418ae8727f3e803a5
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-30 22:36:08 +0200

    Progress task exam prep DOM

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Skeleton/index.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Skeleton/index.js
@@ -6,33 +6,48 @@ function solve() {
 		e.preventDefault();
 
 		// input + unique name? --> input[name=""]
-		let lectureNameElement = document.querySelector('input[name="lecture-name"]');
-		let lectureDateElement = document.querySelector('input[name="lecture-date"]');
-		let lectureModuleElement = document.querySelector('select[name="lecture-module"]')
-
+		let lectureNameElement = document.querySelector(
+			'input[name="lecture-name"]'
+		);
+		let lectureDateElement = document.querySelector(
+			'input[name="lecture-date"]'
+		);
+		let lectureModuleElement = document.querySelector(
+			'select[name="lecture-module"]'
+		);
 
 		// valid input
-		if (!lectureNameElement.value || !lectureDateElement.value || lectureModuleElement == 'Select module') {
+		if (
+			!lectureNameElement.value ||
+			!lectureDateElement.value ||
+			lectureModuleElement == 'Select module'
+		) {
 			return;
 		}
 
-
 		// TODO Create lecture
-		let liElement = document.createElement('li')
-		liElement.classList.add('flex')
-		let courseHeadingElement = document.createElement('h4')
-		courseHeadingElement.textContent = `${lectureNameElement.value} - ${formatDate(lectureDateElement.value)}`
+		let liElement = document.createElement('li');
+		liElement.classList.add('flex');
+		let courseHeadingElement = document.createElement('h4');
+		courseHeadingElement.textContent = `${lectureNameElement.value
+			} - ${formatDate(lectureDateElement.value)}`;
+
+		let deleteButtonElement = document.createElement('button')
+		deleteButtonElement.classList.add('red')
+		deleteButtonElement.textContent = 'del'
 
 
+		liElement.appendChild(courseHeadingElement)
+		liElement.appendChild(deleteButtonElement)
 
+		console.log(liElement);
 		// TODO: Create module
 	});
 
-
 	function formatDate(dateInput) {
-		let [date, time] = dateInput.split('T')
-		date = date.replace(/-/g, '/')
+		let [date, time] = dateInput.split('T');
+		date = date.replace(/-/g, '/');
 
-		return `${date} - ${time}`
+		return `${date} - ${time}`;
 	}
 }
