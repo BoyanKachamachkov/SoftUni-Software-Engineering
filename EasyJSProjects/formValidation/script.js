5f4f25fa839039f5e8e824f305b4d61cf99ac17f
kachamachkov <kachamachkovboyan@gmail.com>
2024-01-31 20:25:44 +0200

    Implemented name check, password char. length checks (6 and 20) and password blockage.

--- EasyJSProjects/formValidation/script.js
+++ EasyJSProjects/formValidation/script.js
@@ -10,7 +10,20 @@ form.addEventListener('submit', (e) => {
     let errorMessages = []
 
     if (nameInput.value === '' || nameInput.value === null) {
-        errorMessages.push('You need to type a name!')
+        errorMessages.push('Name is required!')
+    }
+
+
+    if (password.value.length <= 6) {
+        errorMessages.push('Password must be longer than 6 characters')
+    }
+
+    if (password.value.length >= 20) {
+        errorMessages.push('Password must be less than 20 characters')
+    }
+
+    if (password.value == 'password') {
+        errorMessages.push('Sorry, you cannot set your password to be "password". Please choose another one.')
     }
 
     if (errorMessages.length > 0) {
