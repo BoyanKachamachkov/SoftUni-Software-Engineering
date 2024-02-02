0884156b91b6df67a2a7d05b8b08e6e6409f31e3
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-02 18:59:21 +0200

    Solve p1 p4 Classes LAB

--- JS-Advanced/homeworks-and-labs/08-Classes-LAB/Person.js
+++ JS-Advanced/homeworks-and-labs/08-Classes-LAB/Person.js
@@ -14,8 +14,28 @@ class Person {
   toString() {
     return `${this.firstName}, ${this.lastName}, ${this.age}, ${this.email} `;
   }
+
+  static greet() {
+    console.log("Hello world");
+  }
+
+  static clone(person) {
+    const result = new Person(
+      person.firstName,
+      person.lastName,
+      person.age,
+      person.email
+    );
+    return result;
+  }
 }
 
 const myPerson = new Person("John", "Smith", 32, "john@abv.bg");
 console.log(myPerson);
 console.log(myPerson.toString());
+
+console.log(myPerson instanceof Person);
+Person.greet();
+
+const myClone = Person.clone(myPerson);
+console.log(myClone);
