f84b8197fdfbcc51d12f38371952a40fa23c5a55
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-06 09:43:19 +0200

    Solve unit-testing-lab p3

--- JS-Advanced/homeworks-and-labs/07-Unit-testing-LAB/02-playingCards.js
+++ JS-Advanced/homeworks-and-labs/07-Unit-testing-LAB/02-playingCards.js
@@ -1,9 +1,4 @@
 function playingCards(face, suit) {
-  // returs card obj that holds card face and suit
-  // throw an error if card has invalid face
-  // •	Valid card faces are: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
-  // •	Valid card suits are: S (♠), H (♥), D (♦), C (♣)
-
   let faces = [
     '2',
     '3',
