e589c6fc28e5c87e0409d8449281e6f4c42582d6
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-10 14:02:00 +0200

    Solve p2-Online-Shop exam prep

---
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-Preparation-1_Resources/02-Online-shop.js
@@ -0,0 +1,155 @@
+class OnlineShop {
+  warehouseSpace;
+  products = [];
+  sales = [];
+
+  constructor(warehouseSpace) {
+    this.warehouseSpace = warehouseSpace;
+  }
+
+  loadingStore(product, quantity, spaceRequired) {
+    if (this.warehouseSpace < spaceRequired) {
+      throw new Error('Not enough space in the warehouse.');
+    }
+
+    this.products.push({
+      product,
+      quantity,
+    });
+
+    this.warehouseSpace -= spaceRequired;
+
+    return `The ${product} has been successfully delivered in the warehouse.`;
+  }
+  quantityCheck(product, minimalQuantity) {
+    const data = this.findProduct(product);
+
+    if (minimalQuantity <= 0) {
+      throw new Error('The quantity cannot be zero or negative.');
+    }
+
+    if (data.quantity >= minimalQuantity) {
+      return `You have enough from product ${product}.`;
+    } else {
+      const difference = minimalQuantity - data.quantity;
+      data.quantity = minimalQuantity;
+      return `You added ${difference} more from the ${product} products.`;
+    }
+  }
+
+  sellProduct(product) {
+    const data = this.findProduct(product);
+
+    // if product is not found from the method above it will throw the error
+    data.quantity--;
+
+    this.sales.push({
+      product,
+      quantity: 1,
+    });
+
+    return `The ${product} has been successfully sold.`;
+  }
+
+  revision() {
+    if (this.sales.length == 0) {
+      throw new Error('There are no sales today!');
+    }
+    const result = [
+      `You sold ${this.sales.length} products today!`,
+      'Products in the warehouse:',
+    ];
+
+    for (let { product, quantity } of this.products) {
+      result.push(`${product}-${quantity} more left`);
+    }
+    return result.join('\n');
+  }
+
+  findProduct(product) {
+    const data = this.products.find((p) => p.product == product);
+
+    if (!data) {
+      throw new Error(`There is no ${product} in the warehouse.`);
+    }
+
+    return data;
+  }
+}
+
+// test1();
+// test2();
+test3();
+test4();
+
+function test1() {
+  const myOnlineShop = new OnlineShop(500);
+  console.log(myOnlineShop.loadingStore('headphones', 10, 200));
+  console.log(myOnlineShop.loadingStore('laptop', 5, 200));
+  console.log(myOnlineShop.loadingStore('TV', 40, 500));
+
+  /* The headphones has been successfully delivered in the warehouse. 
+  The laptop has been successfully delivered in the warehouse.
+  Uncaught Error Error: Not enough space in the warehouse.
+   */
+}
+
+function test2() {
+  // const myOnlineShop = new OnlineShop(500);
+  // console.log(myOnlineShop.loadingStore('headphones', 10, 200));
+  // console.log(myOnlineShop.loadingStore('laptop', 5, 200));
+  // console.log(myOnlineShop.quantityCheck('headphones', 10));
+  // console.log(myOnlineShop.quantityCheck('laptop', 10));
+  // console.log(myOnlineShop.quantityCheck('TV', 40));
+  /* The headphones has been successfully delivered in the warehouse. 
+The laptop has been successfully delivered in the warehouse. 
+You have enough from product headphones. 
+You added 5 more from the laptop products.
+Uncaught Error Error: There is no TV in the warehouse.
+ */
+}
+
+function test3() {
+  // // Example 3
+  // const myOnlineShop = new OnlineShop(500);
+  // console.log(myOnlineShop.loadingStore('headphones', 10, 200));
+  // console.log(myOnlineShop.loadingStore('laptop', 5, 200));
+  // console.log(myOnlineShop.quantityCheck('headphones', 10));
+  // console.log(myOnlineShop.quantityCheck('laptop', 10));
+  // console.log(myOnlineShop.sellProduct('headphones'));
+  // console.log(myOnlineShop.sellProduct('laptop'));
+  // console.log(myOnlineShop.sellProduct('keyboard'));
+  /* The headphones has been successfully delivered in the warehouse. 
+The laptop has been successfully delivered in the warehouse. 
+You have enough from product headphones. 
+You added 5 more from the laptop products. 
+The headphones has been successfully sold. 
+The laptop has been successfully sold.
+Uncaught Error Error: There is no keyboard in the warehouse.
+ */
+}
+
+function test4() {
+  const myOnlineShop = new OnlineShop(500);
+  console.log(myOnlineShop.loadingStore('headphones', 10, 200));
+  console.log(myOnlineShop.loadingStore('laptop', 5, 200));
+
+  console.log(myOnlineShop.quantityCheck('headphones', 10));
+  console.log(myOnlineShop.quantityCheck('laptop', 10));
+
+  console.log(myOnlineShop.sellProduct('headphones'));
+  console.log(myOnlineShop.sellProduct('laptop'));
+  console.log(myOnlineShop.revision());
+
+  /* The headphones has been successfully delivered in the warehouse. 
+The laptop has been successfully delivered in the warehouse. 
+You have enough from product headphones. 
+You added 5 more from the laptop products. 
+The headphones has been successfully sold. 
+The laptop has been successfully sold. 
+You sold 2 products today!
+Products in the warehouse: 
+headphones-9 more left 
+laptop-9 more left
+ */
+}
