edb3272847273a80356ade86c3d6e51f91e906dc
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-11 10:35:26 +0200

    Progress p1, exam prep 1, DOM

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-Preparation-1_Resources/01. Hotel reservation-Resources/app.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-Preparation-1_Resources/01. Hotel reservation-Resources/app.js
@@ -6,7 +6,7 @@ function solve() {
   // on clicking next move the info in reservation and make next gray
   // edit and continue must be enabled
 
-  //    обект за всичките инпути
+  // group all inputs into object for easier use
   const inputs = {
     firstName: document.getElementById('first-name'),
     lastName: document.getElementById('last-name'),
@@ -21,6 +21,35 @@ function solve() {
   function onNextClick(event) {
     event.preventDefault();
 
-    const firstName = document.getElementById('first-name').value;
+    // if any of our inputs is empty string, we terminate the program
+    if (
+      inputs.firstName.value == '' ||
+      inputs.lastName.value == '' ||
+      inputs.dateIn.value == '' ||
+      inputs.dateOut.value == '' ||
+      inputs.peopleCount.value == ''
+    ) {
+      return;
+    }
+
+    // all input values content
+    const firstName = inputs.firstName.value;
+    const lastName = inputs.lastName.value;
+    const dateIn = new Date(inputs.dateIn.value);
+    const dateOut = new Date(inputs.dateOut.value);
+    const peopleCount = Number(inputs.peopleCount.value);
+    console.log(firstName, lastName, dateIn, dateOut, peopleCount);
+    // TODO! validate date in is before date out
+    if (dateIn.getTime() >= dateOut.getTime()) {
+      console.log('Check in is after check out!');
+      return;
+    }
+
+    // clear fields in the end
+    inputs.firstName.value = '';
+    inputs.lastName.value = '';
+    inputs.dateIn.value = '';
+    inputs.dateOut.value = '';
+    inputs.peopleCount.value = '';
   }
 }
