30615a31aef4fbc60a78fa41a70d5434bac0731f
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-11 10:16:25 +0200

    Progress p1, exam prep 1, DOM

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-Preparation-1_Resources/01. Hotel reservation-Resources/app.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-Preparation-1_Resources/01. Hotel reservation-Resources/app.js
@@ -1,10 +1,26 @@
 window.addEventListener('load', solve);
 
 function solve() {
+  // take input fields
+  // validate they are correct
+  // on clicking next move the info in reservation and make next gray
+  // edit and continue must be enabled
 
-    }
+  //    обект за всичките инпути
+  const inputs = {
+    firstName: document.getElementById('first-name'),
+    lastName: document.getElementById('last-name'),
+    dateIn: document.getElementById('date-in'),
+    dateOut: document.getElementById('date-out'),
+    peopleCount: document.getElementById('people-count'),
+  };
 
+  // закачаме функция на некст бутона
+  document.getElementById('next-btn').addEventListener('click', onNextClick);
 
+  function onNextClick(event) {
+    event.preventDefault();
 
-    
-    
+    const firstName = document.getElementById('first-name').value;
+  }
+}
