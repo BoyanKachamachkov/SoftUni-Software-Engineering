d3104b866893b333d3ec04b788b0224865ff8180
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-14 09:47:45 +0200

    Progress on DOM p1, hotel reservation

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-jan-24/01. Hotel reservation-Resources/app2.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-jan-24/01. Hotel reservation-Resources/app2.js
@@ -12,6 +12,8 @@ function solve() {
     peopleCount: document.getElementById('people-count'),
   };
 
+  const infoList = document.querySelector('.info-list');
+
 
   const nextBtn = document.getElementById('next-btn');
   nextBtn.addEventListener('click', onNextClick);
@@ -22,11 +24,11 @@ function solve() {
 
     // check for empty strs
     if (
-      firstName.value === '' ||
-      lastName.value === '' ||
-      dateIn.value === '' ||
-      dateOut.value === '' ||
-      peopleCount.value === ''
+      inputs.firstName.value === '' ||
+      inputs.lastName.value === '' ||
+      inputs.dateIn.value === '' ||
+      inputs.dateOut.value === '' ||
+      inputs.peopleCount.value === ''
     ) {
       return;
     }
@@ -38,8 +40,63 @@ function solve() {
     const dateOut = inputs.dateOut.value;
     const peopleCount = Number(inputs.peopleCount.value);
 
-    
+    if (new Date(dateIn).getTime() >= new Date(dateOut).getTime()) {
+      console.log('Check out is before check in!');
+      return;
+    }
+
+    // reset form
+    nextBtn.parentElement.reset();
+    nextBtn.disabled = true;
+
+    // pass to f to create us the whole LI 
+    const result = createPreview(
+      firstName,
+      lastName,
+      dateIn,
+      dateOut,
+      peopleCount
+    );
 
 
+    // assign the newly created LI tree to info-list (reservation field)
+    infoList.appendChild(result);
+  }
+
+  function createPreview(firstName, lastName, dateIn, dateOut, peopleCount) {
+    const element = e('li');
+    element.className = 'reservation-content';
+
+    const article = e('article');
+    article.appendChild(e('h3', `Name: ${firstName} ${lastName}`));
+    article.appendChild(e('p', `From date: ${dateIn}`));
+    article.appendChild(e('p', `To date: ${dateOut}`));
+    article.appendChild(e('p', `For ${peopleCount} people`));
+
+    element.appendChild(article);
+
+    const editBtn = e('button', 'Edit');
+    editBtn.className = 'edit-btn';
+    const continueBtn = e('button', 'Continue');
+    continueBtn.className = 'continue-btn';
+
+    article.appendChild(editBtn);
+    article.appendChild(continueBtn);
+
+    // our new tree of li element is fully created
+    return element;
+
+  }
+
+  // factory element creator f
+  function e(type, content) {
+
+    const element = document.createElement(type);
+    if (content) {
+      element.textContent = content;
+
+    }
+
+    return element;
   }
 }
