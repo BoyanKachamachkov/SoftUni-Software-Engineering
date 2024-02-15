d59911ab721f281873b4cd81039485862605f60b
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-15 12:44:49 +0200

    Progress on p2, class, sep-23, exam-prep-1, Triathlon

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-Sep-23/02. Triathlon/02. Triathlon.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-Sep-23/02. Triathlon/02. Triathlon.js
@@ -1,3 +1,27 @@
 class Triathlon {
+  competitionName;
+  participants;
+  lisftOfFinalists;
+
+  constructor(competitionName) {
+    this.competitionName = competitionName;
+    this.participants = {};
+    this.lisftOfFinalists = [];
+
+  };
+  addParticipant(participantName, participantGender) { }
+
+  completeness(participantName, condition) { }
+
+  rewarding(participantName) { }
+
+  showRecord(criteria) { }
+
 
 }
+
+
+const contest = new Triathlon("Dynamos");
+console.log(contest.addParticipant("Peter", "male"));
+console.log(contest.addParticipant("Sasha", "female"));
+console.log(contest.addParticipant("Peter", "male"));
