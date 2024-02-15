ab54c269a44650e497bf90e4400b0fa90d4a33cb
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-15 16:39:03 +0200

    Progress p3, unit test, movie threater, sep23 exam prep 1, 70/100

---
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-Sep-23/03. Movie Theater/03.test.js
@@ -0,0 +1,21 @@
+import { movieTheater } from "./03. Movie Theater.js";
+import { assert, expect } from 'chai';
+
+describe('Suit tests', () => {
+  it('test ageRestrictions', () => {
+    expect(movieTheater.ageRestrictions('G')).to.equal(`All ages admitted to watch the movie`);
+    expect(movieTheater.ageRestrictions('PG')).to.equal(`Parental guidance suggested! Some material may not be suitable for pre-teenagers`);
+    expect(movieTheater.ageRestrictions('R')).to.equal(`Restricted! Under 17 requires accompanying parent or adult guardian`);
+    expect(movieTheater.ageRestrictions('NC-17')).to.equal(`No one under 17 admitted to watch the movie`);
+    expect(movieTheater.ageRestrictions()).to.equal(`There are no age restrictions for this movie`);
+  });
+
+  it('test moneySpent functionality', () => {
+    expect(movieTheater.moneySpent(1, ['Nachos', 'Popcorn'], ['Soda', 'Water'])).to.equal(`The total cost for the purchase is 29.50`);
+    expect(movieTheater.moneySpent(3, ['Nachos', 'Popcorn'], ['Soda', 'Water'])).to.equal(`The total cost for the purchase with applied discount is 47.60`);
+
+    expect(() => movieTheater.moneySpent('1', ['Nachos'], ['Water'])).to.throw('Invalid input');
+    expect(() => movieTheater.moneySpent(1, 'Nachos', ['Water'])).to.throw('Invalid input');
+    expect(() => movieTheater.moneySpent(1, ['Nachos'], '2')).to.throw('Invalid input');
+  });
+});
