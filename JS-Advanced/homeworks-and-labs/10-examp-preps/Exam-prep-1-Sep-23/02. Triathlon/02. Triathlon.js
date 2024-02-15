eab6668f41a4832fbf8cdbde2c3d1e55edb29256
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-15 13:27:37 +0200

    Progress 50/100  on p2, class, sep-23, exam-prep-1, Triathlon

--- JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-Sep-23/02. Triathlon/02. Triathlon.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/Exam-prep-1-Sep-23/02. Triathlon/02. Triathlon.js
@@ -1,19 +1,43 @@
 class Triathlon {
-  competitionName;
-  participants;
-  lisftOfFinalists;
-
-  constructor(competitionName) {
-    this.competitionName = competitionName;
+  constructor(name) {
+    this.name = name;
     this.participants = {};
     this.lisftOfFinalists = [];
 
   };
-  addParticipant(participantName, participantGender) { }
+  addParticipant(participant, gender) {
 
-  completeness(participantName, condition) { }
+    if (this.participants[participant] === undefined) {
+      this.participants[participant] = gender;
+      return `A new participant has been added - ${participant}`;
+    } else {
+      return `${participant} has already been added to the list`;
+    }
+  }
 
-  rewarding(participantName) { }
+  completeness(participant, condition) {
+    if (this.participants[participant] === undefined) {
+      throw new Error(`${participant} is not in the current participants list`);
+    } else if (condition < 30) {
+      throw new Error(`${participant} is not well prepared and cannot finish any discipline`);
+    }
+
+    let completedCount = Math.floor(condition / 30);
+    if (completedCount < 3) {
+      return `${participant} could only complete ${completedCount} of the disciplines`;
+
+    } else {
+      let participantGender = this.participants[participant];
+      this.lisftOfFinalists.push({
+        name: participant,
+        gender: participantGender,
+      });
+      delete this.participants[participant];
+      return `Congratulations, ${participant} finished the whole competition`;
+    }
+  }
+
+  rewarding(participant) { }
 
   showRecord(criteria) { }
 
@@ -21,7 +45,24 @@ class Triathlon {
 }
 
 
+// const contest = new Triathlon("Dynamos");
+// console.log(contest.addParticipant("Peter", "male"));
+// console.log(contest.addParticipant("Sasha", "female"));
+// console.log(contest.addParticipant("Peter", "male"));
+
+
 const contest = new Triathlon("Dynamos");
 console.log(contest.addParticipant("Peter", "male"));
 console.log(contest.addParticipant("Sasha", "female"));
-console.log(contest.addParticipant("Peter", "male"));
+console.log(contest.addParticipant("George", "male"));
+console.log(contest.completeness("Peter", 100));
+console.log(contest.completeness("Sasha", 70));
+console.log(contest.completeness("George", 20));
+
+
+// A new participant has been added - Peter
+// A new participant has been added - Sasha
+// A new participant has been added - George
+// Congratulations, Peter finished the whole competition
+// Sasha could only complete 2 of the disciplines
+// Uncaught Error: George is not well prepared and cannot finish any discipline
