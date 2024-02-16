70e0b29facea9484375f121d6a4f92231b5cb398
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-16 11:45:23 +0200

    Progress 36/100 class task, FlightBooking

--- JS-Advanced/homeworks-and-labs/10-examp-preps/21-Oct-23/02.FlightBookingSystem.js
+++ JS-Advanced/homeworks-and-labs/10-examp-preps/21-Oct-23/02.FlightBookingSystem.js
@@ -25,11 +25,35 @@ class FlightBookingSystem {
       return `Flight ${flightNumber} to ${destination} has been added to the system.`;
     }
   }
+
+  bookFlight(passengerName, flightNumber) {
+
+    if (!this.flights.find(flight => flight.flightNumber == flightNumber)) {
+
+      return `Flight ${flightNumber} is not available for booking.`;
+
+    } else {
+
+      let newBooking = {
+        passengerName,
+        flightNumber
+
+      };
+
+      this.bookings.push(newBooking);
+      this.bookingsCount++;
+
+
+      return `Booking for passenger ${passengerName} on flight ${flightNumber} is confirmed.`;
+    }
+
+  }
 }
 
 
 const system = new FlightBookingSystem("TravelWorld");
 console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
 console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
-console.log(system.addFlight("CC303", "Chicago", "11:45 AM", 120));
-console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+console.log(system.bookFlight("Alice", "AA101"));
+console.log(system.bookFlight("Bob", "BB202"));
+console.log(system.bookFlight("Charlie", "CC303"));
