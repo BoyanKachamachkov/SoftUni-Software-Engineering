0886893bcac5c27c1c7ecf15ac32d91b1993e95b
kachamachkov <kachamachkovboyan@gmail.com>
2024-02-25 21:12:10 +0200

    Progress P2 LAB, ASYNC JS

--- JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.Github-Repos/scripts/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.Github-Repos/scripts/app.js
@@ -1,16 +1,16 @@
 function loadRepos() {
 	// take username input
 	const userInput = document.getElementById('username').value;
-
 	const baseUrl = 'https://api.github.com/users/';
 
+	// fetch all github repos for user
 	fetch(`${baseUrl}${userInput}/repos`)
 		.then(res => res.json())
-		.then(x => {
-			console.log(x);
-		});
+		.then((arr) => arr.forEach((arr) => {
+			console.log(arr.full_name);
+			console.log(arr.html_url);
+		}));
 
-	// fetch all github repos for user
 	// each repository will be separate LI item
 	// utilize full_name and html_url from the returned object
 
