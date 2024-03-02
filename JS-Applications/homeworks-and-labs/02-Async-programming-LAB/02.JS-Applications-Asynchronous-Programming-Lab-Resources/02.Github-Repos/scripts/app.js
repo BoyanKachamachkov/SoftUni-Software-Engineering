e8e9d8c06d92737e28bc46c86ab1abf63d5a9652
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-02 09:37:20 +0200

    Solve P2, LAB, ASYNC

--- JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
@@ -1,15 +1,12 @@
-function loadRepos() {
-	const list = get.getElementById('repos');
+const list = document.getElementById('repos');
 
+function loadRepos() {
 	const username = document.getElementById('username').value;
 	const url = `https://api.github.com/users/${username}/repos`;
-
 	fetch(url)
 		.then(onHeaders)
 		.then(onSuccess)
 		.catch(onError);
-
-
 }
 
 function onHeaders(response) {
@@ -21,8 +18,20 @@ function onHeaders(response) {
 
 function onSuccess(data) {
 
+	list.replaceChildren(...data.map(createListItem));
 }
 
 function onError(error) {
+	list.textContent = error;
 
+}
+
+function createListItem({ html_url, full_name }) {
+	const item = document.createElement('li');
+	const anchor = document.createElement('a');
+	anchor.href = html_url;
+	anchor.textContent = full_name;
+
+	item.appendChild(anchor);
+	return item;
 }
