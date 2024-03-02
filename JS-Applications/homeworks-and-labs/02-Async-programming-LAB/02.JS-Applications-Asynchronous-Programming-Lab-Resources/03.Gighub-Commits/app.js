0a3636daa9b9c4401be284ec5b76e5ba7595cecf
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-02 12:38:18 +0200

    Progress P3 LAB Async

--- JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-LAB/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,28 @@
+const list = document.getElementById('commits');
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(onHeaders)
+        .then(displayCommits)
+        .catch(onError);
+}
+
+
+function onHeaders(response) {
+    if (!response.ok) {
+        throw 'Error';
+    }
+    return response.json();
+}
+
+function displayCommits(data) {
+
+    console.log(data);
+}
+
+function onError(err) {
+    console.error(err);
 }
