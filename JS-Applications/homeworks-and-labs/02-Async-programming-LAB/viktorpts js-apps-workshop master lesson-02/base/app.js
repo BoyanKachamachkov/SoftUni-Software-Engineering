820c82b44d92d46e4db58ff847ca9e7af0744170
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-03 12:57:00 +0200

    Progress on p4 lab, async

--- JS-Applications/homeworks-and-labs/02-Async-programming-LAB/viktorpts js-apps-workshop master lesson-02/base/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-LAB/viktorpts js-apps-workshop master lesson-02/base/app.js
@@ -1,4 +1,28 @@
-// window.addEventListener('load');
+window.addEventListener('load', start);
+
+async function start() {
+  const main = document.querySelector('main');
+
+  const recipes = await getRecipes();
+
+  main.replaceChildren(...Object.values(recipes).map(createCard));
+}
+
+function createCard({ name, img }) {
+
+  const element = document.createElement('article');
+  element.className = 'preview';
+  element.innerHTML = `
+    <div class="title">
+    <h2>${name}</h2>
+    </div>
+    <div class="small">
+      <img src="${img}">
+    </div>`;
+
+  return element;
+
+}
 
 async function getRecipes() {
   const url = 'http://localhost:3030/jsonstore/cookbook/recipes';
@@ -10,6 +34,7 @@ async function getRecipes() {
 
     return recipes;
   } catch (err) {
+    alert(err);
     throw err;
   }
 
@@ -17,5 +42,14 @@ async function getRecipes() {
 
 async function getDetails(id) {
 
-  const url = `http://localhost:3030/jsonstore/cookbook/details/:id`;
+  const url = `http://localhost:3030/jsonstore/cookbook/details/${id}`;
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+
+    return details;
+  } catch (err) {
+    alert(err);
+    throw err;
+  }
 }
