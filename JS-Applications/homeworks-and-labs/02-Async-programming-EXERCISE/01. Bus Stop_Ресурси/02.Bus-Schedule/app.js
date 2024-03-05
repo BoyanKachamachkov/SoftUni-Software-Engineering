0abcf1fe7e305f253aec53e99210642985697a9b
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-05 09:30:51 +0200

    Progress p2 exe async

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/02.Bus-Schedule/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/02.Bus-Schedule/app.js
@@ -1,7 +1,23 @@
 function solve() {
-
     function depart() {
-        console.log('Depart TODO...');
+        let nextStopId = 'depot';
+        let stopInfoSpan = document.querySelector('#info .info');
+        let departButton = document.getElementById('depart');
+        let arriveButton = document.getElementById('arrive');
+
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+            .then(body => body.json())
+            .then(stopInfo => {
+
+                stopInfoSpan.setAttribute('data-next-stop-name', stopInfo.name);
+                stopInfoSpan.setAttribute('data-next-stop-id', stopInfo.next);
+
+                stopInfo.textContent = stopInfo.name;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+
+
+            });
     }
 
     function arrive() {
