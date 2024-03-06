6e860f08a19ce2bd1e972cfcf3ad1bbcee0ed143
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-06 08:07:35 +0200

    Progress p3 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app.js
@@ -4,13 +4,42 @@ function attachEvents() {
     getWeatherButton.addEventListener('click', getWeatherHandler);
     let locationInput = document.getElementById('location');
 
+    let conditions = {
+        Sunny: () => '☀',
+        'Partly sunny': () => '⛅',
+        Overcast: () => '☁',
+        Rain: () => '☂',
+        Degrees: () => '°'
+    };
+
 
     function getWeatherHandler() {
-        let locationName = locationInput.value;
 
         fetch(`http://localhost:3030/jsonstore/forecaster/locations`)
-        .then(body => body.json())
-        .then
+            .then(body => body.json())
+            .then(locations => {
+                let locationName = locationInput.value;
+                let location = locations.find(x => x.name === locationName);
+                console.log(location);
+
+                return fetch(`http://localhost:3030/jsonstore/forecaster/today/${location.code}`);
+            })
+            .then(body => body.json())
+            .then(currentWeatherReport => {
+                console.log(currentWeatherReport);
+            });
+
+        function createCurrentWeatherElement(forecast) {
+            let forecastsDiv = document.createElement('div');
+            forecastsDiv.classList.add('forecasts');
+
+            let conditionSymbolSpan = document.createElement('span');
+            conditionSymbolSpan.classList.add('condition', 'symbol');
+            conditionSymbolSpan.textContent = conditions[forecast.condition];
+
+            let conditionSpan = document.createElement('span');
+        }
+
 
     }
 }
