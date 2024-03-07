4e7a3d7d0e9ac792f6cf7f94294ee407c286842f
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-07 10:28:41 +0200

    Progress p4 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
@@ -6,11 +6,74 @@ function lockedProfile() {
 
     })();
 
-    function createHtmlProfile(userId) {
+    function createHtmlProfile(userIndex, username, email, age) {
+
 
     }
 
+    let profileDiv = document.createElement('div');
+    profileDiv.classList.add('profile');
+
+    let profileImage = document.createElement('img');
+    profileImage.src = './iconProfile2.png';
+    profileImage.classList.add('user-icon');
+
+    let lockLabel = document.createElement('label');
+    lockLabel.textContent = 'Lock';
+
+    let lockRadio = document.createElement('input');
+    lockRadio.type = 'radio';
+    lockRadio.name = `user${userIndex}Locked`;
+    lockRadio.value = 'lock';
+    lockRadio.checked = true;
 
 
+    // unlock
+    let unlockLabel = document.createElement('label');
+    unlockLabel.textContent = 'Unlock';
+
+    let unlockRadio = document.createElement('input');
+    unlockRadio.type = 'radio';
+    unlockRadio.name = `user${userIndex}Locked`;
+    unlockRadio.value = 'unlock';
+
+
+    let br = document.createElement('br');
+    let hr = document.createElement('hr');
+
+    let usernameLabel = document.createElement('label');
+    usernameLabel.textContent = 'Username';
+
+    let usernameInput = document.createElement('input');
+    usernameInput.name = `user${userIndex}Username`;
+    usernameInput.value = username;
+    usernameInput.readOnly = true;
+    usernameInput.disabled = true;
+
+
+    let hiddenFieldsDiv = document.createElement('div');
+    hiddenFieldsDiv.id = `user${userIndex}HiddenFields`;
+
+    let hiddenFieldsHr = document.createElement('hr');
+
+    // email
+    let emailLabel = document.createElement('label');
+    emailLabel.textContent = 'Email:';
+
+    let emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.value = email;
+    emailInput.readOnly = true;
+    emailInput.disabled = true;
+
+    // age
+    let ageLabel = document.createElement('label');
+    ageLabel.textContent = 'Age:';
+
+    let ageInput = document.createElement('input');
+    ageInput.type = 'email';
+    ageInput.value = age;
+    ageInput.readOnly = true;
+    ageInput.disabled = true;
 
 }
