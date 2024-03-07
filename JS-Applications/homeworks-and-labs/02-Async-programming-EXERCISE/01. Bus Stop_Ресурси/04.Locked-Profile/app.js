76885ecbb3a22254249757fc852e474c029f26cc
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-07 10:15:07 +0200

    Progress p4 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
@@ -1,3 +1,16 @@
 function lockedProfile() {
-    console.log('TODO...')
+    (async () => {
+        const profilesData = await fetch('http://localhost:3030/jsonstore/advanced/profiles');
+        const profiles = await profilesData.json();
+        console.log(profiles);
+
+    })();
+
+    function createHtmlProfile(userId) {
+
+    }
+
+
+
+
 }
