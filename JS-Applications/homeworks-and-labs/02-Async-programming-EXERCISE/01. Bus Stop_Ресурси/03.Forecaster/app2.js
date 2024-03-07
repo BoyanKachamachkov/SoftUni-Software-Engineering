2a94db560b1477478bb5b413ed4b21b833e75ecc
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-07 14:25:44 +0200

    Progress p3, async exe, again

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app2.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app2.js
@@ -2,9 +2,13 @@ function attachEvents() {
 
   const getWeatherBtn = document.getElementById('submit');
   getWeatherBtn.addEventListener('click', getWeather);
+  const forecastRef = document.getElementById('forecast');
+  const currentRef = document.getElementById('current');
+  const upcomingRef = document.getElementById('upcoming');
+  const baseLocationsURL = 'http://localhost:3030/jsonstore/forecaster/locations';
 
   function getWeather(e) {
-    debugger;
+    forecastRef.style.display = 'block';
   }
 
 }
