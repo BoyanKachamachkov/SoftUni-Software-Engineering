e3e84a52c336528198b7611a85b30f3c9ab3d22d
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-07 10:40:10 +0200

    Progress p4 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/04.Locked-Profile/app.js
@@ -3,77 +3,116 @@ function lockedProfile() {
         const profilesData = await fetch('http://localhost:3030/jsonstore/advanced/profiles');
         const profiles = await profilesData.json();
         console.log(profiles);
+        let mainSection = document.getElementById('main');
+
+        Object.keys(profiles).forEach((key, i) => {
+            let profile = profiles[key];
+            let htmlProfile = createHtmlProfile(i + 1, profile.username, profile.email, profile.age);
+            mainSection.appendChild(htmlProfile);
+        });
 
     })();
 
     function createHtmlProfile(userIndex, username, email, age) {
+        let profileDiv = document.createElement('div');
+        profileDiv.classList.add('profile');
 
+        let profileImage = document.createElement('img');
+        profileImage.src = './iconProfile2.png';
+        profileImage.classList.add('user-icon');
+
+        let lockLabel = document.createElement('label');
+        lockLabel.textContent = 'Lock';
+
+        let lockRadio = document.createElement('input');
+        lockRadio.type = 'radio';
+        lockRadio.name = `user${userIndex}Locked`;
+        lockRadio.value = 'lock';
+        lockRadio.checked = true;
+
+
+        // unlock
+        let unlockLabel = document.createElement('label');
+        unlockLabel.textContent = 'Unlock';
+
+        let unlockRadio = document.createElement('input');
+        unlockRadio.type = 'radio';
+        unlockRadio.name = `user${userIndex}Locked`;
+        unlockRadio.value = 'unlock';
+
+
+        let br = document.createElement('br');
+        let hr = document.createElement('hr');
+
+        let usernameLabel = document.createElement('label');
+        usernameLabel.textContent = 'Username';
+
+        let usernameInput = document.createElement('input');
+        usernameInput.name = `user${userIndex}Username`;
+        usernameInput.value = username;
+        usernameInput.readOnly = true;
+        usernameInput.disabled = true;
+
+
+        let hiddenFieldsDiv = document.createElement('div');
+        hiddenFieldsDiv.id = `user${userIndex}HiddenFields`;
+
+        let hiddenFieldsHr = document.createElement('hr');
+
+        // email
+        let emailLabel = document.createElement('label');
+
+        emailLabel.textContent = 'Email:';
+
+        let emailInput = document.createElement('input');
+        emailInput.name = `user${userIndex}Email`;
+        emailInput.type = 'email';
+        emailInput.value = email;
+        emailInput.readOnly = true;
+        emailInput.disabled = true;
+
+        // age
+        let ageLabel = document.createElement('label');
+        ageLabel.textContent = 'Age:';
+
+        let ageInput = document.createElement('input');
+        ageInput.name = `user${userIndex}Age`;
+        ageInput.type = 'email';
+        ageInput.value = age;
+        ageInput.readOnly = true;
+        ageInput.disabled = true;
+
+        hiddenFieldsDiv.appendChild(hiddenFieldsHr);
+        hiddenFieldsDiv.appendChild(emailLabel);
+        hiddenFieldsDiv.appendChild(emailInput);
+        hiddenFieldsDiv.appendChild(ageLabel);
+        hiddenFieldsDiv.appendChild(ageInput);
+
+        let showMoreButton = document.createElement('button');
+        showMoreButton.textContent = 'Show More';
+        showMoreButton.addEventListener('click', showHiddenInfoHandler);
+
+        profileDiv.appendChild(profileImage);
+        profileDiv.appendChild(lockLabel);
+        profileDiv.appendChild(lockRadio);
+        profileDiv.appendChild(unlockLabel);
+        profileDiv.appendChild(unlockRadio);
+        profileDiv.appendChild(br);
+        profileDiv.appendChild(hr);
+        profileDiv.appendChild(usernameLabel);
+        profileDiv.appendChild(usernameInput);
+        profileDiv.appendChild(hiddenFieldsDiv);
+        profileDiv.appendChild(showMoreButton);
+
+        return profileDiv;
 
     }
 
-    let profileDiv = document.createElement('div');
-    profileDiv.classList.add('profile');
-
-    let profileImage = document.createElement('img');
-    profileImage.src = './iconProfile2.png';
-    profileImage.classList.add('user-icon');
-
-    let lockLabel = document.createElement('label');
-    lockLabel.textContent = 'Lock';
-
-    let lockRadio = document.createElement('input');
-    lockRadio.type = 'radio';
-    lockRadio.name = `user${userIndex}Locked`;
-    lockRadio.value = 'lock';
-    lockRadio.checked = true;
 
 
-    // unlock
-    let unlockLabel = document.createElement('label');
-    unlockLabel.textContent = 'Unlock';
+    function showHiddenInfoHandler() {
 
-    let unlockRadio = document.createElement('input');
-    unlockRadio.type = 'radio';
-    unlockRadio.name = `user${userIndex}Locked`;
-    unlockRadio.value = 'unlock';
+    }
 
-
-    let br = document.createElement('br');
-    let hr = document.createElement('hr');
-
-    let usernameLabel = document.createElement('label');
-    usernameLabel.textContent = 'Username';
-
-    let usernameInput = document.createElement('input');
-    usernameInput.name = `user${userIndex}Username`;
-    usernameInput.value = username;
-    usernameInput.readOnly = true;
-    usernameInput.disabled = true;
-
-
-    let hiddenFieldsDiv = document.createElement('div');
-    hiddenFieldsDiv.id = `user${userIndex}HiddenFields`;
-
-    let hiddenFieldsHr = document.createElement('hr');
-
-    // email
-    let emailLabel = document.createElement('label');
-    emailLabel.textContent = 'Email:';
-
-    let emailInput = document.createElement('input');
-    emailInput.type = 'email';
-    emailInput.value = email;
-    emailInput.readOnly = true;
-    emailInput.disabled = true;
-
-    // age
-    let ageLabel = document.createElement('label');
-    ageLabel.textContent = 'Age:';
-
-    let ageInput = document.createElement('input');
-    ageInput.type = 'email';
-    ageInput.value = age;
-    ageInput.readOnly = true;
-    ageInput.disabled = true;
 
 }
