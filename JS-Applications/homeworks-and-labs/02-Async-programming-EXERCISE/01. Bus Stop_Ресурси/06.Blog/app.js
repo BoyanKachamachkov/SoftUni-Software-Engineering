f25e05e347916ece6f11ab756cec55ca04920412
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-09 09:22:05 +0200

    Progress p6 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/06.Blog/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/06.Blog/app.js
@@ -1,5 +1,21 @@
 function attachEvents() {
-    console.log('TODO...');
+    // Posts - http://localhost:3030/jsonstore/blog/posts
+    // Comments - http://localhost:3030/jsonstore/blog/comments
+    document.getElementById('btnLoadPosts').addEventListener('click', loadPosts);
+    document.getElementById('btnViewPost').addEventListener('click', viewPost);
+
+
+    const postsURL = 'http://localhost:3030/jsonstore/blog/posts';
+    const commentsURL = 'Comments - http://localhost:3030/jsonstore/blog/comments';
+
+    async function loadPosts(ev) {
+        const res = await fetch(postsURL);
+        const postsData = await res.json();
+        console.log(postsData);
+
+    }
+
+    async function viewPost(ev) { }
 }
 
 attachEvents();
