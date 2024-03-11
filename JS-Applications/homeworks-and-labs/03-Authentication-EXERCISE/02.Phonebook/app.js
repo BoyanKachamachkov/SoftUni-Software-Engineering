bfe74437ba18a410c1d4d4c47a931d7c54694f88
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-11 13:14:05 +0200

    Progress p2, data auth. exe

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/02.Phonebook/app.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/02.Phonebook/app.js
@@ -1,5 +1,23 @@
 function attachEvents() {
-    console.log('TODO...');
+    const URL = 'http://localhost:3030/jsonstore/phonebook'
+    const ulRef = document.getElementById('phonebook')
+    document.getElementById('btnLoad').addEventListener('click', onLoadRecords)
+
+    async function onLoadRecords(e) {
+        let response = await fetch(URL)
+        let data = await response.json()
+
+        Object.values(data).forEach(rec => {
+            createAndAppendLi(rec)
+        })
+    }
+
+    function createAndAppendLi(data) {
+        let li = document.createElement('li')
+        li.textContent = `${data.person}: ${data.phone}`
+
+        ulRef.appendChild(li)
+    }
 }
 
 attachEvents();
