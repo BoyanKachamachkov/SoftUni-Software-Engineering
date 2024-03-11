d2602d452ff5e3dd9e93734a06f2d1265953f437
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-11 13:22:23 +0200

    Progress p2, data auth. exe

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/02.Phonebook/app.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/02.Phonebook/app.js
@@ -1,11 +1,12 @@
 function attachEvents() {
     const URL = 'http://localhost:3030/jsonstore/phonebook'
     const ulRef = document.getElementById('phonebook')
-    document.getElementById('btnLoad').addEventListener('click', onLoadRecords)
+    document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords)
 
-    async function onLoadRecords(e) {
+    async function onLoadAllRecords(e) {
         let response = await fetch(URL)
         let data = await response.json()
+        ulRef.innerHTML = ''
 
         Object.values(data).forEach(rec => {
             createAndAppendLi(rec)
@@ -16,7 +17,24 @@ function attachEvents() {
         let li = document.createElement('li')
         li.textContent = `${data.person}: ${data.phone}`
 
+        let btn = document.createElement('button')
+        btn.textContent = 'Delete'
+        btn.dataset.id = data._id
+        btn.addEventListener('click', onDelete)
+
+        li.appendChild(btn)
         ulRef.appendChild(li)
+    }
+
+    async function onDelete(e) {
+        let id = e.target.dataset.id
+
+        await fetch(URL + '/' + id, {
+            method: 'DELETE'
+
+        })
+        onLoadAllRecords()
+
     }
 }
 
