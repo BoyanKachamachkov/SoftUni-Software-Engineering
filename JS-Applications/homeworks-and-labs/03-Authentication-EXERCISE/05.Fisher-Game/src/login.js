95a71338da328539b52a9b3e344bd65ace308bc5
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-12 20:30:41 +0200

    Progress p5 data exe, login form

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/login.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/login.js
@@ -1,1 +1,9 @@
-console.log('TODO:// Implement Login functionality');
+document.querySelector('form').addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    debugger;
+}
