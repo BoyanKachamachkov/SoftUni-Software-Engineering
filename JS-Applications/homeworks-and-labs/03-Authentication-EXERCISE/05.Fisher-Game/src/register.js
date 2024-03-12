31ea03c6030480bcb73c23831f12fe20537c673f
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-12 20:16:09 +0200

    Progress p5 data exe

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/register.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/register.js
@@ -1,8 +1,34 @@
 document.querySelector('form').addEventListener('submit', onSubmit);
-
+const url = 'http://localhost:3030/users/register';
 function onSubmit(e) {
     e.preventDefault();
-    let formData = new FormData(e.target)
-    debugger
+    let formData = new FormData(e.target);
+    let email = formData.get('email');
+    let password = formData.get('password');
+    let rePass = formData.get('rePass');
+
+
+    if (!email || !password || !rePass || password !== rePass) {
+        return; //TODO load err msg
+    }
+
+    createUser({ email, password });
 }
 
+async function createUser(data) {
+    const option = createOption('POST', data);
+    let response = await fetch(url, option);
+    let userData = await response.json()
+    debugger;
+
+}
+
+function createOption(method, data) {
+    return {
+        method,
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+}
