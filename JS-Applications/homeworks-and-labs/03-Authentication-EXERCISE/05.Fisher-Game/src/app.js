fc572d6b12bda674cc181fd038e8e6ff1a00d57f
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-13 09:00:50 +0200

    Progress p5, exe data

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/app.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/app.js
@@ -1,18 +1,45 @@
 // if we have logged in user?
-document.querySelector("a[id='home']").classList.add('active')
+document.querySelector("a[id='home']").classList.add('active');
+document.getElementById('logout').addEventListener('click', onLogout);
+
 let userData = JSON.parse(sessionStorage.getItem('userData'));
+
+
+const userNavRef = document.getElementById('user');
+const guestNavRef = document.getElementById('guest');
+const endpoints = {
+    logout: 'http://localhost:3030/users/logout',
+
+
+};
+
+updateNav();
 //update nav
+function updateNav() {
+    if (userData) {
+        document.querySelector('nav p span').textContent = userData.email;
+        userNavRef.style.display = 'inline-block';
+        guestNavRef.style.display = 'none';
 
-if (userData) {
-    document.querySelector('nav p span').textContent = userData.email;
-    document.querySelector("a[id='login']").style.display = 'none';
-    document.querySelector("a[id='register']").style.display = 'none';
-    document.querySelector("a[id='logout']").style.display = 'inline-block';
+    } else {
+        document.querySelector('nav p span').textContent = 'guest';
+        userNavRef.style.display = 'none';
+        guestNavRef.style.display = 'inline-block';
+    }
+}
 
-} else {
-    document.querySelector('nav p span').textContent = 'guest';
 
-    document.querySelector("a[id='login']").style.display = 'inline-block';
-    document.querySelector("a[id='register']").style.display = 'inline-block';
-    document.querySelector("a[id='logout']").style.display = 'none';
+
+async function onLogout(e) {
+    let option = {
+        method: 'GET',
+        headers: {
+            'X-Authorization': userData.accessToken
+        }
+    };
+    await fetch(endpoints.logout, option);
+    sessionStorage.clear();
+    userData = null;
+    updateNav();
+
 }
