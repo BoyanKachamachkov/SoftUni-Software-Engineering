ddc15fdb135e036b0cfd251815f494621292e40f
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-13 08:27:08 +0200

    Progress p5, data exe, added login page

--- JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/login.js
+++ JS-Applications/homeworks-and-labs/03-Authentication-EXERCISE/05.Fisher-Game/src/login.js
@@ -1,13 +1,38 @@
 document.querySelector('form').addEventListener('submit', onSubmit);
+const url = 'http://localhost:3030/users/login';
 
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const email = formData.get('email');
     const password = formData.get('password');
 
-    if(!email || !password){
+    if (!email || !password) {
         return; //todo load err msg
     }
+    await loginUser({ email, password });
+
+    e.target.reset();
+    window.location = 'index.html'
+
+}
+
+async function loginUser(data) {
+    const option = createOption('POST', data);
+
+    const response = await fetch(url, option);
+    const userData = await response.json();
+    sessionStorage.setItem('userData', JSON.stringify(userData));
     debugger
+}
+
+
+function createOption(method, data) {
+    return {
+        method,
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
 }
