303e27bd67462611056d61299904e5d11043aa23
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-16 10:19:37 +0200

    Progress P1 async programming

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/01.Bus-Stop/app.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/01.Bus-Stop/app.js
@@ -1,7 +1,6 @@
 async function getInfo() {
     // take value from stopid
     // get to  http://localhost:3030/jsonstore/bus/businfo/:busId
-
     // place name inside div stopname
     //      list each item as "Bus {busId} arrives in {time} minutes"
     // display Error as stopName 
@@ -11,9 +10,23 @@ async function getInfo() {
     const stopIdRef = document.getElementById('stopId');
     const stopId = stopIdRef.value;
     const baseURL = 'http://localhost:3030/jsonstore/bus/businfo/';
+    const stopNameRef = document.getElementById('stopName');
+    const ulRef = document.getElementById('buses')
 
-    const response = await fetch(baseURL + stopId);
-    const data = await response.json();
+    try {
+        const response = await fetch(baseURL + stopId);
+        const data = await response.json();
+        stopNameRef.textContent = data.name;
+
+        console.log(data)
+
+
+
+    } catch (error) {
+
+    }
+
+
 
     debugger;
 }
