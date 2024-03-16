0210d3ad0da56bb2731aa266728691521ae3c141
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-16 16:30:53 +0200

    Solve p3 forecaster, async

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app3.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app3.js
@@ -3,6 +3,8 @@ function attachEvents() {
     const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
     const forecastRef = document.getElementById('forecast');
     const currForRef = document.getElementById('current');
+    const upcomingRef = document.getElementById('upcoming');
+
 
     const getWBtn = document.getElementById('submit');
     getWBtn.addEventListener('click', getWeather);
@@ -41,7 +43,7 @@ function attachEvents() {
         const data = await response.json();
 
         const todayInfo = createForecastDivToday(data);
-        // currForRef.appendChild(todayInfo);
+        currForRef.appendChild(todayInfo);
     }
 
     function createForecastDivToday(data) {
@@ -57,13 +59,10 @@ function attachEvents() {
         forecastsDiv.appendChild(condSymbolSpan);
 
         // 2nd span with another F
+        const spanContainer = spanGenerator('condition', 'forecast-data', data.name, data.forecast);
 
-        // const spanContainer = generateSpan('condition', 'forecast-data', data.name, data.forecast)
-        debugger;
-
-
-
-
+        forecastsDiv.appendChild(spanContainer);
+        return forecastsDiv;
     }
 
 
@@ -72,18 +71,50 @@ function attachEvents() {
         const url = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
         const response = await fetch(url);
         const data = await response.json();
+
+        const upcomingInfo = createForecasDivUpcoming(data);
+        upcomingRef.appendChild(upcomingInfo);
+    }
+
+    function createForecasDivUpcoming(data) {
+
+        const forecastsDiv = document.createElement('div');
+        forecastsDiv.classList.add('forecast-info');
+
+        const upcoming1 = spanGenerator('upcoming', 'symbol', data.name, data.forecast[0]);
+        const upcoming2 = spanGenerator('upcoming', 'symbol', data.name, data.forecast[1]);
+        const upcoming3 = spanGenerator('upcoming', 'symbol', data.name, data.forecast[2]);
+
+        forecastsDiv.appendChild(upcoming1);
+        forecastsDiv.appendChild(upcoming2);
+        forecastsDiv.appendChild(upcoming3);
+        return forecastsDiv;
     }
 
 
-    function generateSpan(classContainer, classSpan, name, data) {
+    function spanGenerator(classContainer, classSpan, name, data) {
 
-        const fatherSpan = document.createElement('span');
-        fatherSpan.classList.add(classContainer);
+        const parentSpan = document.createElement('span');
+        parentSpan.classList.add(classContainer);
 
         const nameOrSymbolSpan = document.createElement('span');
         nameOrSymbolSpan.classList.add(classSpan);
         // check when each?
         classSpan === 'symbol' ? nameOrSymbolSpan.textContent = conditions[data.condition] : nameOrSymbolSpan.textContent = name;
+
+        const degreeSpan = document.createElement('span');
+        degreeSpan.classList.add('forecast-data');
+        degreeSpan.textContent = `${data.low}°/${data.high}°`;
+
+        const cityCondSpan = document.createElement('span');
+        cityCondSpan.classList.add('forecast-data');
+        cityCondSpan.textContent = data.condition;
+
+        parentSpan.appendChild(nameOrSymbolSpan);
+        parentSpan.appendChild(degreeSpan);
+        parentSpan.appendChild(cityCondSpan);
+
+        return parentSpan;
     }
 
 }
