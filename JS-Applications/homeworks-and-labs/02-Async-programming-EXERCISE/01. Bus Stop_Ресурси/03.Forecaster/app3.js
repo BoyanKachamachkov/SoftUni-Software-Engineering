fa072e88bd46eac34f558919ecb50aca57d4634b
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-16 15:38:49 +0200

    Progess p3, async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app3.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app3.js
@@ -1,28 +1,31 @@
 function attachEvents() {
-
-    // http://localhost:3030/jsonstore/forecaster/locations
-    /* 
-    { 
-    name: locationName,
-    code: locationCode
-}
- */
     const inputLocationRef = document.getElementById('location');
     const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
+    const forecastRef = document.getElementById('forecast');
 
     const getWBtn = document.getElementById('submit');
     getWBtn.addEventListener('click', getWeather);
 
     async function getWeather() {
-        const userInput = inputLocationRef.value;
-        const response = await fetch(baseURL);
-        const data = await response.json();
+        try {
+            const userInput = inputLocationRef.value;
+            forecastRef.style.display = 'block';
 
-        // Find the object, corresponding to the name that the user submitted in the input field with ID "location" and use its code value to make two more GET requests:
-        
-        debugger;
+            const response = await fetch(baseURL);
+            const data = await response.json();
+
+            const currentLocationData = data.find(x => x.name === userInput);
+
+            await fillTodayData(currentLocationData.code);
+            debugger;
+
+        } catch (error) {
+            forecastRef.textContent = 'Error';
+
+        }
     }
 
+    async function fillTodayData(code) { }
 
 
 }
