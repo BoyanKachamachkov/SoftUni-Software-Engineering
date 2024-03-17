aa5921a354b63418d5bf5b36696a98f8b8004f34
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 15:47:34 +0200

    Progress p6 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/06.Blog/app3.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/06.Blog/app3.js
@@ -1,22 +1,37 @@
 function attachEvents() {
-    // Posts - http://localhost:3030/jsonstore/blog/posts
-    // Comments - http://localhost:3030/jsonstore/blog/comments
-
-    const loadPostsBtn = document.getElementById('btnLoadPosts');
+    const btnLoadPosts = document.getElementById('btnLoadPosts');
+    const postsSelect = document.querySelector('select#posts');
 
 
+    // add events
+    btnLoadPosts.addEventListener('click', handleLoadPosts);
+    let commonData;
+
+    function handleLoadPosts() {
+        // get posts
+        fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then(res => res.json())
+            .then(data => addPosts(data));
 
 
-    loadPostsBtn.addEventListener('click', loadAllPosts);
+        function addPosts(data) {
+            commonData = data;
 
-    async function loadAllPosts(e) {
-        const url = 'http://localhost:3030/jsonstore/blog/posts';
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Failed to fetch posts');
+            postsSelect.innerHTML = '';
+
+
+            for (let [id, postInfo] of Object.entries(data)) {
+                // create option
+                let option = document.createElement('option');
+                option.value = id;
+                option.textContent = postInfo.title;
+                option.dataset.body = postInfo.body;
+
+                postsSelect.appendChild(option);
+
+            }
+
         }
-        const data = await response.json();
-        debugger;
     }
 
 }
