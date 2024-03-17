a5c668e5423e6ba701167f03524de3e1110f3fa6
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 12:42:29 +0200

    Progress p5 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion.js
@@ -5,31 +5,56 @@ async function solution() {
     // 3. on click fetch details data
     // 4. update html with data
 
+    // http://localhost:3030/jsonstore/advanced/articles/details/:id  
+
 
     try {
         const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
         let response = await fetch(url);
 
-        if(!response.ok){
-            throw new Error('Error obtaining article list')
+        if (!response.ok) {
+            throw new Error('Error obtaining article list');
         }
 
-        let data = await response.json()
-
-        data.foreach(articleInfo =>{
-
-            let articleEleemnt = document.createElement('div')
-            articleEleemnt.classList.add('accordion')
-            articleEleemnt.innerHTML = `
+        let data = await response.json();
+        data.forEach(articleInfo => {
+            let articleElement = document.createElement('div');
+            articleElement.classList.add('accordion');
+            articleElement.innerHTML =
+                `
             <div class="head">
-                <span>${articleInfo.title}
-            `
-
-        })
+                <span>${articleInfo.title}</span>
+                <button class="button" id="${articleInfo._id}" onclick="moreOnclick(e)">More</button>
+            </div>
+            <div class="extra"></div>
+            `;
+            let main = document.getElementById('main');
+            main.appendChild(articleElement);
+        });
     } catch (error) {
+        console.log(error);
+    }
+
+    async function moreOnclick(e) {
+        try {
+            let currentTarget = e.currentTarget;
+            let url = `http://localhost:3030/jsonstore/advanced/articles/details/${currentTarget.id}`;
+            let parent = currentTarget.parentNode.parentNode;
+            let extraDiv = parent.querySelector('div.extra');
+
+            let response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Error obtainig article ID details');
+            }
+
+            let data = await response.json();
+            debugger;
+        } catch (error) {
+
+        }
 
     }
 
 }
 
-solution()
+solution();
