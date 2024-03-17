55d7ce0350e65cc733e3438ce218445f2bf50abf
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 10:28:13 +0200

    Solve 4th time forecast, async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app4.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app4.js
@@ -2,14 +2,33 @@ function attachEvents() {
 
     const getWeatherBtn = document.getElementById('submit');
     getWeatherBtn.addEventListener('click', getWeatherData);
-    const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations'
+    const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
+    const usrInputRef = document.getElementById('location');
 
     async function getWeatherData(e) {
+        const usrInput = usrInputRef.value;
 
-        const response = await fetch(baseURL)
-        const data = await response.json()
-        debugger
+        const response = await fetch(baseURL);
+        const data = await response.json();
+        const currentLocation = data.find(x => x.name === usrInput);
+
+        const todayData = getTodayData(currentLocation.code);
     }
+
+    async function getTodayData(code) {
+        const url = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        debugger;
+
+    }
+
+    function createTodayForecast(){
+
+    }
+
 
 
 }
