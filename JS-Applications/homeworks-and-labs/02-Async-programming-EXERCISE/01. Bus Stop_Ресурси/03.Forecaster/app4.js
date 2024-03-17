3de6ef8d17df518a883ace61a7d574d273ed43e3
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 10:42:56 +0200

    Solve 4th time forecast, async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app4.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/03.Forecaster/app4.js
@@ -4,15 +4,26 @@ function attachEvents() {
     getWeatherBtn.addEventListener('click', getWeatherData);
     const baseURL = 'http://localhost:3030/jsonstore/forecaster/locations';
     const usrInputRef = document.getElementById('location');
+    const forecastDiv = document.getElementById('forecast');
+    const currentDiv = document.getElementById('current');
+
+    const conditions = {
+        Sunny: '☀',
+        'Partly Sunny': '⛅',
+        Overcast: '☁',
+        Rain: '☂',
+        Degrees: '°',
+    };
 
     async function getWeatherData(e) {
         const usrInput = usrInputRef.value;
+        forecastDiv.style.display = 'block';
 
         const response = await fetch(baseURL);
         const data = await response.json();
         const currentLocation = data.find(x => x.name === usrInput);
 
-        const todayData = getTodayData(currentLocation.code);
+        await getTodayData(currentLocation.code);
     }
 
     async function getTodayData(code) {
@@ -20,12 +31,32 @@ function attachEvents() {
 
         const response = await fetch(url);
         const data = await response.json();
-        
-        debugger;
 
+        const todayInfo = createTodayForecast(data);
+        currentDiv.appendChild(todayInfo);
     }
 
-    function createTodayForecast(){
+
+    function createTodayForecast(data) {
+
+        const div = document.createElement('div');
+        div.classList.add('forecasts');
+
+        const span = document.createElement('span');
+        span.classList.add('condition');
+        span.classList.add('symbol');
+        span.textContent = conditions[data.forecast.condition];
+
+        div.appendChild(span);
+
+        const spanContainer = spanGenerator('condition', 'forecast-data', data.name, data.forecast);
+
+        div.appendChild(spanContainer);
+        return div;
+    }
+
+    function spanGenerator(classContainer, classSpan, name, data) {
+        
 
     }
 
