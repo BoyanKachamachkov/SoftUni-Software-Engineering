159bee0bd8fd2e100b72ddd25c99b00e7b63413a
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 12:18:43 +0200

    Progress p5 async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion.js
@@ -1,3 +1,35 @@
-function solution() {
-    //TODO .....
+async function solution() {
+
+    // 1. fetch all articles from server
+    // 2. create divs with titles //togle buttons
+    // 3. on click fetch details data
+    // 4. update html with data
+
+
+    try {
+        const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+        let response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error('Error obtaining article list')
+        }
+
+        let data = await response.json()
+
+        data.foreach(articleInfo =>{
+
+            let articleEleemnt = document.createElement('div')
+            articleEleemnt.classList.add('accordion')
+            articleEleemnt.innerHTML = `
+            <div class="head">
+                <span>${articleInfo.title}
+            `
+
+        })
+    } catch (error) {
+
+    }
+
 }
+
+solution()
