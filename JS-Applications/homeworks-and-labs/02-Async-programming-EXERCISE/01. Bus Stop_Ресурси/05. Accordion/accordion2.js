f9358be4e5816199be7e15973fbcb91d0f796962
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-17 14:07:59 +0200

    Solve p5 accordion async exe

--- JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion2.js
+++ JS-Applications/homeworks-and-labs/02-Async-programming-EXERCISE/01. Bus Stop_Ресурси/05. Accordion/accordion2.js
@@ -16,14 +16,13 @@ async function solution() {
         }
 
         const data = await response.json();
-        debugger;
         data.forEach(articleInfo => {
             let artEl = document.createElement('div');
             artEl.classList.add('accordion');
             artEl.innerHTML = `
             <div class="head">
                 <span>${articleInfo.title}</span>
-                <button class="button" id="${articleInfo._id}" onclick="onMoreClick(e)">More</button>
+                <button class="button" id="${articleInfo._id}" onclick="onMoreClick(event)">More</button>
             </div>
             <div class="extra"></div>
             `;
@@ -38,7 +37,38 @@ async function solution() {
     }
 }
 
+async function onMoreClick(e) {
 
-async function onMoreClick(e) { }
+    try {
+        let currentTarget = e.currentTarget;
+
+        let url = `http://localhost:3030/jsonstore/advanced/articles/details/${currentTarget.id}`;
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch article ID');
+        }
+        const data = await response.json();
+
+        let parent = currentTarget.parentNode.parentNode;
+        let extraDiv = parent.querySelector('div.extra');
+
+        extraDiv.innerHTML = `<p>${data.content}</p>`;
+
+        if (currentTarget.textContent == 'More') {
+            currentTarget.textContent = 'Less';
+            extraDiv.style.display = 'block';
+
+        } else {
+            currentTarget.textContent = 'More';
+            extraDiv.style.display = 'none';
+        }
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
 
 solution();
