aef694f3938626f1562875a1f145c55d017de01e
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-18 20:26:57 +0200

    Progress p3, data auth exe

--- JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
+++ JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
@@ -10,7 +10,45 @@ const form = document.getElementById('form');
 form.addEventListener('submit', addStudent);
 const table = document.querySelector('#results tbody');
 
-async function loadStudents() { }
+async function loadStudents() {
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Can't fetch data");
+        }
+
+        const data = await response.json();
+
+        Object.values(data).forEach(r => {
+            // Implement generator function
+            let student = createElement('tr',
+            createElement('td', r.firstName),
+            createElement('td', r.lastName),
+            createElement('td', r.facultyNumber),
+            createElement('td', r.grade),
+            )
+            table.appendChild(student)
+        });
+    } catch (error) {
+        alert(error.message)
+    }
+}
+// 'li' 'hello' => li tag + textNode.textContent = hello, li.appendChild(textNode)
+// 'ul', HTML ELEMENT li tag  => ul tag => ul.appendChild(HTML ELEMENT)
+function createElement(type, ...content){
+    let element = document.createElement(type)
+
+    content.forEach( c=>{
+        if(typeof c === 'number' || typeof c == 'string'){
+            c = document.createTextNode(c)
+        }
+        element.appendChild(c)
+    })
+
+    return element;
+}
 
 
 
