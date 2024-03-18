7f6e150122d88c8c5e7ad8d88bad536653db4a3c
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-18 20:40:58 +0200

    Solve p3 data exe

--- JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
+++ JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
@@ -24,28 +24,29 @@ async function loadStudents() {
         Object.values(data).forEach(r => {
             // Implement generator function
             let student = createElement('tr',
-            createElement('td', r.firstName),
-            createElement('td', r.lastName),
-            createElement('td', r.facultyNumber),
-            createElement('td', r.grade),
-            )
-            table.appendChild(student)
+                createElement('td', r.firstName),
+                createElement('td', r.lastName),
+                createElement('td', r.facultyNumber),
+                createElement('td', r.grade),
+            );
+            table.appendChild(student);
         });
     } catch (error) {
-        alert(error.message)
+        alert(error.message);
     }
 }
 // 'li' 'hello' => li tag + textNode.textContent = hello, li.appendChild(textNode)
 // 'ul', HTML ELEMENT li tag  => ul tag => ul.appendChild(HTML ELEMENT)
-function createElement(type, ...content){
-    let element = document.createElement(type)
+// last iteration appends only, as its called with 1 param only
+function createElement(type, ...content) {
+    let element = document.createElement(type);
 
-    content.forEach( c=>{
-        if(typeof c === 'number' || typeof c == 'string'){
-            c = document.createTextNode(c)
+    content.forEach(c => {
+        if (typeof c === 'number' || typeof c == 'string') {
+            c = document.createTextNode(c);
         }
-        element.appendChild(c)
-    })
+        element.appendChild(c);
+    });
 
     return element;
 }
@@ -55,9 +56,36 @@ function createElement(type, ...content){
 async function addStudent(e) {
     e.preventDefault();
 
-    const formData = new FormData(form);
-    formData.get(firstName);
-    debugger;
+    const dataForm = new FormData(form);
+    const infoArr = [...dataForm.values()];
+
+    const gradeNum = Number(infoArr[3].trim());
+
+
+    table.replaceChildren();
+    
+    try {
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName: infoArr[0],
+                lastName: infoArr[1],
+                facultyNumber: infoArr[2],
+                grade: gradeNum
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Cant craete new record');
+        }
+        // reload
+        loadStudents();
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
 
