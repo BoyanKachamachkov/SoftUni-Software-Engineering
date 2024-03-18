03d1746028f81e2141bfac9f4e225c2af6c43e5c
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-18 20:14:30 +0200

    Progress p3, data auth exe

--- JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
+++ JS-Applications/homeworks-and-labs/03-Remote-data-and-Authentication-EXERCISE/03.Students/app.js
@@ -1,1 +1,25 @@
-console.log('TODO...');
+// 1. add event on window load
+// 2. fetch all studs from server
+// 3. update html with received data
+// 4. on submit get value from inputs
+// 5. use the data and create POST req
+// 6. update student list
+window.addEventListener('load', loadStudents);
+const url = 'http://localhost:3030/jsonstore/collections/students';
+const form = document.getElementById('form');
+form.addEventListener('submit', addStudent);
+const table = document.querySelector('#results tbody');
+
+async function loadStudents() { }
+
+
+
+async function addStudent(e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.get(firstName);
+    debugger;
+}
+
+
