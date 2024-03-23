2c70781474a5abb84cdaf1792bbe56a515386ab7
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-23 17:38:28 +0200

    Finish framework for exam

--- JS-Applications/homeworks-and-labs/10-Prep-framework/src/util.js
+++ JS-Applications/homeworks-and-labs/10-Prep-framework/src/util.js
@@ -11,3 +11,16 @@ export function getUserData() {
 export function clearUserData() {
     localStorage.removeItem('user');
 }
+
+// TODO Add custom validation
+export function createSubmitHandler(callback) {
+    return function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = [...formData.entries()].map(([k, v]) => [k, v.trim()]);
+
+        // event.tar is the form so that the onSubmit f in view can have access to the form and be able to call form.reset() or lock the fields
+        callback(Object.fromEntries(data), event.target);
+    };
+}
