5c0a514fe10fe13a023f3ecdfe6f74036edf92a4
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-23 16:35:35 +0200

    Progress framework creation, Add localStorage

---
+++ JS-Applications/homeworks-and-labs/10-Prep-framework/src/data/request.js
@@ -0,0 +1,12 @@
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+    // TODO add authorization
+}
---
+++ JS-Applications/homeworks-and-labs/10-Prep-framework/src/util.js
@@ -0,0 +1,13 @@
+export function setUserData(data) {
+    localStorage.setItem('user', JSON.stringify(data));
+}
+
+export function getUserData() {
+    return JSON.parse(localStorage.getItem('user'));
+
+
+}
+
+export function clearUserData() {
+    localStorage.removeItem('user');
+}
