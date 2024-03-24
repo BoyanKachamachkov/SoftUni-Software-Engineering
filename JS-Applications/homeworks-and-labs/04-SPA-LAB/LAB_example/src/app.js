7ef349b03ccda56d6aaff9dd35787087bd7ae68b
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-24 12:57:21 +0200

    Add register form

--- JS-Applications/homeworks-and-labs/04-SPA-LAB/LAB_example/src/app.js
+++ JS-Applications/homeworks-and-labs/04-SPA-LAB/LAB_example/src/app.js
@@ -1,9 +1,7 @@
 let headerElement = document.querySelector('.header .nav').addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.tagName == 'A') {
-        console.log('clicked');
         console.log(e.target.getAttribute('data-link'));
-        console.log(e.target.href);
     } else {
         console.log('not A');
     }
