ef896c40742b3a3c24dcef69522bbf00b25f808a
kachamachkov <kachamachkovboyan@gmail.com>
2024-03-24 12:36:01 +0200

    Progress lab exercise SPA, add data-links to all li elements

--- JS-Applications/homeworks-and-labs/04-SPA-LAB/LAB_example/src/app.js
+++ JS-Applications/homeworks-and-labs/04-SPA-LAB/LAB_example/src/app.js
@@ -1,1 +1,10 @@
-console.log('loaded')
+let headerElement = document.querySelector('.header .nav').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.tagName == 'A') {
+        console.log('clicked');
+        console.log(e.target.getAttribute('data-link'));
+        console.log(e.target.href);
+    } else {
+        console.log('not A');
+    }
+});
