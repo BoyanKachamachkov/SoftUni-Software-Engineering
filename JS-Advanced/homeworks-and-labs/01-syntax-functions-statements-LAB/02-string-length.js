7a2c82fc06e576da2fd642272bfc80e7ad768e0d
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-03 21:30:25 +0300

    Solve p2, lab-1, JS-A

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/02-string-length.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/02-string-length.js
@@ -1,13 +1,9 @@
-function solve(arg1, arg2, arg3) {
-    let l1 = arg1.length;
-    let l2 = arg2.length;
-    let l3 = arg3.length;
-    let sum = l1 + l2 + l3;
-    let avg = Math.floor(sum / 3);
+function stringLength(arg1, arg2, arg3) {
 
-    console.log(sum);
-    console.log(avg);
+    let avgLength = arg1.length + arg2.length + arg3.length;
+    console.log(avgLength)
+    console.log(Math.floor(avgLength/3))
 }
 
-solve('chocolate', 'ice cream', 'cake');
-solve('pasta', '5', '22.3');
+
+stringLength('chocolate', 'ice cream', 'cake');
