ee7e8e51d561f9b0a36a74490ca27c120a7d2ef7
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-05 15:49:36 +0300

    Add LAB examples, JS A, lab-1, syntax

---
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/labNotes.js
@@ -0,0 +1,33 @@
+// Function declaration
+function printSomething(text) {
+    console.log(text);
+}
+printSomething('Pesho e gotin');
+
+// Function Expression
+let printMsg = function (text) {
+    console.log(text);
+};
+printMsg('Pesho e oshte po-gotin!');
+
+// Arrow function
+let sum = (a, b) => {
+    console.log(a + b);
+};
+sum(2, 10);
+
+
+// function return
+function calculate(a, b) {
+    return a + b;
+}
+let result = calculate(2, 20);
+console.log(result);
+
+// function expression return
+let calculateExp = function (a, b) {
+    return a + b;
+};
+
+let resultExp = calculateExp(3, 5);
+console.log(resultExp);
