1278f30acd05cc3ba4be6439cc605c0137e33aa2
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-07 12:16:59 +0300

    Solve, JS-A, LAB, Syntax, P7

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/07-day-of-the-week.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/07-day-of-the-week.js
@@ -1,43 +1,17 @@
-function solve(str) {
-    let res = 0;
-
-    switch (str) {
-        default:
-            console.log('error');
-            return;
-
-        case 'Monday':
-            res = 1;
-            break;
-
-        case 'Tuesday':
-            res = 2;
-            break;
-
-        case 'Wednesday':
-            res = 3;
-            break;
-
-        case 'Thursday':
-            res = 4;
-            break;
-
-        case 'Friday':
-            res = 5;
-            break;
-
-        case 'Saturday':
-            res = 6;
-
-            break;
-
-        case 'Sunday':
-            res = 7;
-            break;
+function dayOfTheWeek(day) {
+    let res = '';
+    switch (day) {
+        case 'Monday': res = 1; break;
+        case 'Tuesday': res = 2; break;
+        case 'Wednesday': res = 3; break;
+        case 'Thursday': res = 4; break;
+        case 'Friday': res = 5; break;
+        case 'Saturday': res = 6; break;
+        case 'Sunday': res = 7; break;
+        default: res = 'error'; break;
     }
-    console.log(res);
+    console.log(res)
 }
 
-solve('Monday');
-solve('Friday');
-solve('Invalid');
+dayOfTheWeek('Friday');
+dayOfTheWeek('Invalid');
