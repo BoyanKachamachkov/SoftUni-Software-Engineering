434de51570fdccca4f684132bb5b4cdac2a7e1a7
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-07 11:54:34 +0300

    Solve JS-A lab, p4

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/04-circle-area.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-LAB/04-circle-area.js
@@ -1,11 +1,10 @@
 function solve(arg) {
-    if (typeof arg === 'number') {
-        let result = Math.pow(arg, 2) * Math.PI;
-        console.log(result.toFixed(2));
+
+    if (typeof arg == 'number') {
+        let res = Math.pow(arg, 2) * Math.PI;
+        console.log(res.toFixed(2));
     } else {
-        console.log(
-            `We can not calculate the circle area, because we receive a ${typeof arg}.`
-        );
+        console.log(`We can not calculate the circle area, because we receive a ${typeof (arg)}.`);
     }
 }
 
