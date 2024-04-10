cab19df8a92c6a5c136d8744ebf04bdf068a487c
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-10 22:17:28 +0300

    Progress JS-A, lab-01, p7

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/07-cooking-by-numbers.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/07-cooking-by-numbers.js
@@ -1,4 +1,15 @@
-function cooking(num, op1, op2, op3, op4, op5) {}
+function cooking(num, op1, op2, op3, op4, op5) {
+
+    let n = Number(num)
+
+    switch(command){
+        case 'chop':    break;
+        case 'dice':    break;
+        case 'spice':    break;
+        case 'bake':    break;
+        case 'fillet':    break;
+    }
+}
 
 cooking('32', 'chop', 'chop', 'chop', 'chop', 'chop');
 cooking('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
