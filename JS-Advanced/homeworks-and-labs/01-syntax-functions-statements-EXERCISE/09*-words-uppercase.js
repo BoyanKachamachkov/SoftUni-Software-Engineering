1f1da0e0572b389b2ade7d215073ab0e93316f65
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-12 20:52:28 +0300

    Progress p8, JS-A, Syntax, exe

--- JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/09*-words-uppercase.js
+++ JS-Advanced/homeworks-and-labs/01-syntax-functions-statements-EXERCISE/09*-words-uppercase.js
@@ -1,4 +1,19 @@
-function words(string) {}
+function words(string) {
 
-words('Hi, how are you?');
+    let upperString = string.toUpperCase();
+
+    let res = []
+    res.push(upperString)
+    
+    
+
+
+}
+
+words('Hi, how are you?'); //HI, HOW, ARE, YOU
 words('hello');
+
+
+// Write a program that extracts all words from a passed-in string and converts them to upper case. The extracted words in the upper case must be printed on a single line separated by ", ".
+// The input comes as a single string argument - the text to extract and convert words from.
+// The output should be a single line containing the converted string.
