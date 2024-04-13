98d9e22cf2849c4287b7c45e3916f93c9d66b9b9
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-13 20:36:48 +0300

    Solve p1, Arrays, JS-A, LAB

--- JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-EXERCISE/01-print-array-delimeter.js
+++
@@ -1,7 +0,0 @@
-function printArrayWithGivenDelimiter(array, delimiter) {
-  console.log(array.join(delimiter));
-}
-
-printArrayWithGivenDelimiter(['One', 'Two', 'Three', 'Four', 'Five'], '-');
-console.log('-------------------');
-printArrayWithGivenDelimiter(['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_');
