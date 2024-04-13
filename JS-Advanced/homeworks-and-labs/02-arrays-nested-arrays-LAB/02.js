18e12eb2ba5171e064c1db363a1187cbef45ac54
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-13 20:57:36 +0300

    Solve p2, Arrays, JS-A, LAB

---
+++ JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/02.js
@@ -0,0 +1,13 @@
+function solve(n, k) {
+
+    let res = [1];
+    for (let i = 0; i < n.length; i++) {
+
+        if(res.length - k >= 0){
+            let sum = 
+        }
+        res.push()
+    }
+}
+
+solve(6, 3);
