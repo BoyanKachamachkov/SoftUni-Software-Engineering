14b3fa884f20eaf5e81fdfb499d43ac634a2e0af
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-14 17:50:02 +0300

    Solve JS-A, Arrays, p4

--- JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/04.js
+++ JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/04.js
@@ -1,4 +1,19 @@
 function solve(arr) {
 
+    let res = [];
+
+    for (const element of arr) {
+
+        if (element < 0) {
+            res.unshift(element);
+        } else {
+            res.push(element);
+        }
+    }
+
+    for (const el of res) {
+        console.log(el);
+    }
 }
 solve([7, -2, 8, 9]);
+solve([3, -2, 0, -1]);
