facd32283e5250c7790d369a3e06235bae380bae
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-14 21:41:29 +0300

    Solve JS-A, Arrays, p9

---
+++ JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/09.js
@@ -0,0 +1,26 @@
+function solve(matrix) {
+    // start from the first num
+    // let biggest = matrix[0][0];
+    let biggest = Number.MIN_SAFE_INTEGER;
+
+    for (const arr of matrix) {
+
+        for (const element of arr) {
+            if (element > biggest) {
+                biggest = element;
+            }
+        }
+    }
+    return biggest;
+}
+
+
+solve([
+    [20, 50, 245],
+    [8, 33, 145]
+]);
+
+solve([[3, 5, 7, 12],
+[-1, 4, 33, 2],
+[8, 3, 0, 4]]
+);
