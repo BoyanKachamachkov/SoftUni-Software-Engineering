af5f3c21aaeb74354dcd8b90c232d73fb40aff18
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-15 11:56:03 +0300

    Solve, JS-A, LAB, P10, solution 2

--- JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/10.js
+++ JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/10.js
@@ -1,27 +1,22 @@
 function solve(matrix) {
 
-    // main diagonal sum
     let mainSum = 0;
+    let secondarySum = 0;
+
     for (let i = 0; i < matrix.length; i++) {
         mainSum += matrix[i][i];
-    }
-
-    // secondary diagonal sum
-    let secondarySum = 0;
-    for (let i = 0; i < matrix.length; i++) {
         secondarySum += matrix[i][matrix.length - 1 - i];
     }
 
-    let output = [];
-    output.push(mainSum, secondarySum);
-    console.log(output.join(' '));
+    console.log(mainSum, secondarySum);
 }
 
 solve([
     [20, 40],
     [10, 60]
 ]);
-solve([[3, 5, 17],
+solve([
+    [3, 5, 17],
     [-1, 7, 14],
-    [1, -8, 89]]
-   )
+    [1, -8, 89]
+]);
