5d6524109b78451985d72bbe9a65bbd72d22f04a
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-15 12:45:59 +0300

    Solve, JS-A, LAB, p2

--- JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/02.js
+++ JS-Advanced/homeworks-and-labs/02-arrays-nested-arrays-LAB/02.js
@@ -1,5 +1,15 @@
 function solve(n, k) {
+    let sequence = [1];
 
+    for (let i = 1; i < n; i++) {
+
+        let sum = 0;
+        for (let j = 1; j <= k && i - j >= 0; j++) {
+            sum += sequence[i - j];
+        }
+        sequence.push(sum);
+    }
+    return sequence;
 }
-
-solve(6, 3);
+solve(6, 3); //[1, 1, 2, 4, 7, 13]
+solve(8, 2); //[1, 1, 2, 3, 5, 8, 13, 21]
