95095a00de69e7d3bab30e1945e46b8d461029fa
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-20 12:44:08 +0300

    Add JS Backend and ReactJS folders for courses

---
+++ ReactJS/01-Intro-to-React-JSX/Demo.js
@@ -0,0 +1,16 @@
+// React is JS library for building UI
+// focused on reusable components
+// developed by Facebook
+// JSX is JS superset
+// has all JS Features and more!
+
+// unique mixing of HTML & JS
+
+
+// compiles to plain JS example below
+<div className="red">Children Text</div>;
+
+React.createEelement('div',
+    { className: 'red' },
+    'Children Text'
+);
