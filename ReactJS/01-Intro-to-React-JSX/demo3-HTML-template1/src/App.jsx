c18ef551d6b8c5ae033a6088cd3886ffbf3ba792
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-21 15:10:16 +0300

    Finish demo3 - HTML template1 React

--- ReactJS/01-Intro-to-React-JSX/demo3-HTML-template1/src/App.jsx
+++ ReactJS/01-Intro-to-React-JSX/demo3-HTML-template1/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Info from "./components/Info";
+import OurClients from "./components/OurClients";
+import OurTeam from "./components/OurTeam";
+import Services from "./components/Services";
+import Why from "./components/Why";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Services />
+            <About />
+            <Why />
+            <OurTeam />
+            <OurClients />
+            <Info />
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ ReactJS/01-Intro-to-React-JSX/demo3-HTML-template1/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <section className="footer_section">
+            <div className="container">
+                <p>
+                    &copy; <span id="displayYear"></span> All Rights Reserved By
+                    <a href="https://html.design/">Free Html Templates</a>
+                </p>
+            </div>
+        </section>
+    );
+};
+export default Footer;
