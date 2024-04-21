3b03991481b92f360cf187eb9bc774f70b17f8c7
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-21 19:13:41 +0300

    Start react project

---
+++ ReactJS/01-Intro-to-React-JSX/React-project/src/App.jsx
@@ -0,0 +1,30 @@
+import About from "./Components/About";
+import NavBar from "./Components/Navbar";
+import Carousel from "./Components/Carousel";
+import Service from "./Components/Service";
+import Offer from "./Components/Offer";
+import Reservation from "./Components/Reservation";
+import Testimonial from "./Components/Testimonial";
+import Footer from "./Components/Footer";
+import BackToTop from "./Components/BackToTop";
+
+function App() {
+
+
+    return (
+        <>
+            <NavBar />
+            <Carousel />
+            <About />
+            <Service />
+            <Offer />
+            <Service />
+            <Reservation />
+            <Testimonial />
+            <Footer />
+            <BackToTop />
+        </>
+    );
+}
+
+export default App;
