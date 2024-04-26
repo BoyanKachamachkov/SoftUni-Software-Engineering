2dfc41943ecf3426e23b3bb0c9030b394ed62a8f
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-26 20:02:16 +0300

    Add timer example for useState

--- ReactJS/02-Components-Basic-idea/demo1/src/App.jsx
+++ ReactJS/02-Components-Basic-idea/demo1/src/App.jsx
@@ -3,6 +3,7 @@ import MovieList from './components/MovieList';
 import BookList from './components/BookList';
 import movies from './assets/movies';
 import books from './assets/books';
+import Timer from './components/Timer';
 
 function App() {
 
@@ -12,9 +13,15 @@ function App() {
                 My first DYNAMIC react app!
             </h1>
 
+            <Timer time={5} />
+            <Timer time={6} />
+            <Timer time={7} />
+
             {/* Подаваме масива от филми на movielist */}
             <MovieList headingText="Movie List" movies={movies} />
             <BookList bookHeading="Book List" books={books} />
+
+
         </div>
 
 
---
+++ ReactJS/02-Components-Basic-idea/demo1/src/components/Timer.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Timer(props) {
+
+    // time - read-only immutable, setTime will move the time
+    // useState will hold closure to update the components
+    const [time, setTime] = useState(props.time);
+
+    // Don't use, only example, useEffect is for that purpose!
+    setTimeout(() => {
+        setTime(time + 1);
+    }, 1000);
+    return (
+
+        <div>
+            <h1>Timer</h1>
+
+            <p>{time}</p>
+        </div>
+    );
+}
