37f19ab443c24626c1413054c30b60541e0a63fb
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-28 10:41:36 +0300

    Add Timer exercise example

--- ReactJS/02-Components-Basic-idea/demo2-homework/src/Components/Timer.jsx
+++ ReactJS/02-Components-Basic-idea/demo2-homework/src/Components/Timer.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 export default function Timer(props) {
 
+    const [time, setTime] = useState(0);
+
+    // do not use, just example purposes before useEffect Hook introduced!
+    setTimeout(() => {
+        setTime(time + 1);
+    }, 1000);
 
     return (
         <>
-
-
-
+            <h2>Timer Exercise with useState Hook</h2>
+            <h3>[{time}] Seconds have passed!</h3>
         </>
     );
 }
