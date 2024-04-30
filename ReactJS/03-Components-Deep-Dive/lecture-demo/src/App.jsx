724a113a6655d372cfb38f8ad01f0e543358d894
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-30 13:10:03 +0300

    Add fetch demo with swapi dev

--- ReactJS/03-Components-Deep-Dive/lecture-demo/src/App.jsx
+++ ReactJS/03-Components-Deep-Dive/lecture-demo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import React from 'react';
+import Starwars from './Starwars';
 
 
 function App() {
@@ -29,14 +30,15 @@ function App() {
     return (
 
         <div>
-            <ul>
+            <Starwars />
+            {/* <ul>
                 {nums.map((num, idx) => <li data-key={idx} key={idx}>{num * 2}</li>)}
             </ul>
 
             <h3>Count: {count}</h3>
 
             <button onClick={onClick}>Remove</button>
-            <button onClick={() => setCount(c => c + 1)}>+</button>
+            <button onClick={() => setCount(c => c + 1)}>+</button> */}
 
         </div>
 
