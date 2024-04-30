bd1c19f55d20061472a55191e559991a1ee7c9ba
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-30 16:22:15 +0300

    Finish todo lecture demo

--- ReactJS/03-Components-Deep-Dive/lecture-todolist/client/src/components/TodoList.jsx
+++ ReactJS/03-Components-Deep-Dive/lecture-todolist/client/src/components/TodoList.jsx
@@ -3,15 +3,32 @@ import TodoItem from "./Todo";
 
 export default function TodoList(props) {
 
+    const [todos, setTodos] = useState([]);
+
     useEffect(() => {
         fetch(`http://localhost:3030/jsonstore/todos`)
             .then(response => response.json())
             .then(data => {
-                const todos = Object.values(data);
-                console.log(todos);
+                setTodos(Object.values(data));
             })
             .catch(err => console.log(err));
     }, []);
+
+
+    const changeStatusHandler = (todoId) => {
+
+        // взимаме определеното туду, което е натиснато
+        // създаваме му нова референция с плитко копие и спред
+        // създаваме обект, защото и оригиналното туду е обект, но му сменяме 1-но пропърти
+        // проверяваме дали ИД-тата са еднакви
+        // ако са - обръщаме СТОЙНОСТТА на пропъртито "isCompleted" наобратно
+        // ако не са - не пипаме статуса и го връщаме
+        setTodos(state => state.map(todo => todo._id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo));
+
+    };
+
+
+
 
     return (
         <section className="todo-list-container">
@@ -39,7 +56,15 @@ export default function TodoList(props) {
                     </thead>
                     <tbody>
 
-                        <TodoItem />
+                        {todos.map(todo => (
+                            <TodoItem
+                                _id={todo._id}
+                                key={todo._id}
+                                text={todo.text}
+                                isCompleted={todo.isCompleted}
+                                changeStatusHandler={changeStatusHandler}
+                            />
+                        ))}
                     </tbody>
                 </table>
             </div>
