da77d5de0145a704c0f386677d309f9ff119a528
kachamachkov <kachamachkovboyan@gmail.com>
2024-04-30 08:34:13 +0300

    Progress lecture exercises again

--- ReactJS/03-Components-Deep-Dive/lecture-demo/src/App.jsx
+++ ReactJS/03-Components-Deep-Dive/lecture-demo/src/App.jsx
@@ -1,36 +1,17 @@
 import './App.css';
-import { useState } from 'react';
-import UseEffectExample from './UseEffectExample';
-import Starwars from './Starwars';
+
 
 function App() {
+    let numbers = [1, 2, 3, 4, 5];
+    let doubled = numbers.map(n => n * 2);
 
-    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
-
-    const onClick = () => {
-        setNumbers(oldState => oldState.slice(1));
-    };
-
-
-    // initially key of last num is 4, at the end its 0
-    // meaning = using the INDEX works half the times, but its not good because it makes REACT re-render all LI elements, (it changes the keys at each button click)
-
-    // they will come from the database with unique keys
-    // only parents elements require key!! 
+    console.log(doubled);
 
     return (
-        <Starwars />
-        // <div>
-        //     <ul>
-        //         {numbers.map((number, index) =>
-        //             <li data-key={index} key={index}
-        //             >{number * 2}</li>)}
-        //     </ul>
 
-        //     <button onClick={onClick}>Remove item on top</button>
-        // </div>
-
-        // <UseEffectExample />
+        <ul>
+            {doubled}
+        </ul>
     );
 }
 
