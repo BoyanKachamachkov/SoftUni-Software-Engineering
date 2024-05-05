bd6f0d6fa6cdac96d4fd5bb52f883ea293173f35
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-05 11:15:03 +0300

    Create Header/Footer components

--- ReactJS/04-Workshop-Components-Homework/client/src/App.jsx
+++ ReactJS/04-Workshop-Components-Homework/client/src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './styles.css';
 
 function App() {
 
     return (
         <>
-            
+            <Header />
+
+
+            <Footer />
         </>
     );
 }
