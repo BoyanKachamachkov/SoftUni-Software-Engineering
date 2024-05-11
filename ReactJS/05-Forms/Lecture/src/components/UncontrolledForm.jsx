791350f14ddde581642d4dcec33b29aed07b6a3f
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-11 18:02:43 +0300

    Add basic uncontrolled form, extract data from it with the event object. HTML elements contian the values, not REACT, so DOM controls the state, sub-optimal

--- ReactJS/05-Forms/Lecture/src/components/UncontrolledForm.jsx
+++ ReactJS/05-Forms/Lecture/src/components/UncontrolledForm.jsx
@@ -1,10 +1,43 @@
 
 
-export default function UncontrolledForm () {
+export default function UncontrolledForm() {
 
-    return(
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        console.log(formData.get('username'));
+        console.log(formData.get('password'));
+    };
+
+    return (
         <>
-        <h1>Uncontrolled form</h1>
+            <h1>Uncontrolled form</h1>
+
+            <form onSubmit={submitHandler}>
+
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" name="username" id="username" />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+
+                <div>
+                    <label htmlFor="age">Age:</label>
+                    <input type="numer" name="age" id="age" />
+                </div>
+
+                <div>
+                    {/* By def. BTN submits form when inside form */}
+                    <button>Register</button>
+                    <button type="button">Reset</button>
+                </div>
+            </form>
         </>
-    )
+    );
 }
