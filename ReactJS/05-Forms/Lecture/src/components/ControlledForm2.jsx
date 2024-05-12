a428eafe002e284ee7d0ac5125ee30747c882cfd
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-12 19:56:15 +0300

    Finish form control exercise, add states for each input and change events

--- ReactJS/05-Forms/Lecture/src/components/ControlledForm2.jsx
+++ ReactJS/05-Forms/Lecture/src/components/ControlledForm2.jsx
@@ -1,27 +1,41 @@
-
+import { useState } from "react";
 
 export default function ControlledForm2() {
 
+    const [username, setUsername] = useState('');
+    const [pass, setPass] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    };
 
     return (
 
         <div>
             <h1>Controlled form - exercise</h1>
-
-
             <form >
 
                 <div>
                     <label htmlFor="username">Username:</label>
-                    <input type="text" name="username" id="username" />
+                    <input
+                        type="text" name="username" id="username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="password">Password:</label>
-                    <input type="text" name="password" id="password" />
+                    <input
+                        type="text" name="password" id="password"
+                        value={pass}
+                        onChange={e => setPass(e.target.value)}
+
+                    />
                 </div>
 
-                <button>Submit</button>
+                <button type="button" onClick={submitHandler}>Submit</button>
             </form>
 
 
