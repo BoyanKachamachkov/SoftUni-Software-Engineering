8bdddb6f9deb3db07fcf0bc06b260dea80f347ea
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-16 16:10:13 +0300

    Add basic routing to about 404 pages

--- JS-Back-End/Net-ninja-Crash-Course/server-example/server.js
+++ JS-Back-End/Net-ninja-Crash-Course/server-example/server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 
 
+
+
 const server = http.createServer((req, res) => {
 
     console.log(req.url, req.method);
@@ -10,8 +12,25 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-type', 'text/html');
 
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+
+        case '/about':
+            path += 'about.html';
+            break;
+
+        default:
+            path += '404.html';
+            break;
+    }
+
+
     // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
