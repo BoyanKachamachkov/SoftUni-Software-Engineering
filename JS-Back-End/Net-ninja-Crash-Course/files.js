5e4a551f18b857c06ec24a7cc57d918ea9478293
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-16 10:57:49 +0300

    Add write and read files examples with FS in node

---
+++ JS-Back-End/Net-ninja-Crash-Course/files.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+// reading files
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+
+//     if (err) {
+//         console.log(err);
+//     }
+//     // console.log(data);  returns BUFFER
+//     console.log(data.toString());
+
+// });
+// console.log('last line');
+
+
+// // writing files
+// fs.writeFile('./docs/blog1.txt', 'hello world', () => {
+//     console.log('file was written');
+// });
+
+// // example with non-existent file
+// fs.writeFile('./docs/blog2.txt', 'will you create file for me?', () => {
+//     console.log('file was written');
+// });
+
+
+// directories
+
+
+// delete files
