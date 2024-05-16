6a964fea51625519733f387d99aeb8dadd0812b1
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-16 16:15:14 +0300

    Add redirect on about-me as an example

--- JS-Back-End/Net-ninja-Crash-Course/server-example/server.js
+++ JS-Back-End/Net-ninja-Crash-Course/server-example/server.js
@@ -1,10 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-
-
-
-
 const server = http.createServer((req, res) => {
 
     console.log(req.url, req.method);
@@ -12,19 +8,28 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-type', 'text/html');
 
-
     let path = './views/';
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
 
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+
+        // redirect
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end()
             break;
 
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
