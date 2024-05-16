fea1a0b14991074ac53b621fc43950397169a04d
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-16 11:05:23 +0300

    Add FS directories and delete examples

--- JS-Back-End/Net-ninja-Crash-Course/files.js
+++ JS-Back-End/Net-ninja-Crash-Course/files.js
@@ -25,6 +25,29 @@ const fs = require('fs');
 
 
 // directories
+// if (!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log('folder created');
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log('folder is removed');
+//     });
+// }
 
 
-// delete files
+// // delete files
+// if(fs.existsSync('./docs/deleteme.txt')){
+//     fs.unlink('./docs/deleteme.txt', (err) => {
+//         if(err){
+//             console.log(err)
+//         }
+//         console.log('file has been deleted')
+//     })
+// }
