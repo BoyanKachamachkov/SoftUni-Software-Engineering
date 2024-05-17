60021ff536ed092b36ba228a9729dfc39d92f4b8
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-17 10:35:24 +0300

    Add util JS RFS handler and call HTML via path system, instead of hardcode

--- JS-Back-End/cat-shelter/src/index.js
+++ JS-Back-End/cat-shelter/src/index.js
@@ -2,13 +2,16 @@ const http = require('http');
 const { homeHandler } = require('./handlers/home');
 const { errorHandler } = require('./handlers/404');
 
-
+const routes = {
+    '/': homeHandler,
+    '/index.html': homeHandler,
+};
 
 http.createServer((req, res) => {
-    if (req.url == '/' || req.url == '/index.html') {
-        homeHandler(req, res);
+    const route = routes[req.url]; //if we have match, it returns func
+    if (typeof route == 'function') {
+        route(req, res);
     } else {
         errorHandler(req, res);
     }
-
 }).listen(3000);
---
+++ JS-Back-End/cat-shelter/src/util.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+function readFile(path) {
+
+    const data = fs.readFileSync(path);
+    return data.toString();
+}
+
+module.exports = {
+    readFile
+};
