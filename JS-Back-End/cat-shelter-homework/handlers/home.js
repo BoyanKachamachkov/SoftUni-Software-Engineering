add1c08d73d45af79b0e178e242dcdfa4ccacb44
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-18 15:24:31 +0300

    Add home/util/index project setup

---
+++ JS-Back-End/cat-shelter-homework/handlers/home.js
@@ -0,0 +1,19 @@
+const { readFile } = require("../src/util");
+
+
+function homeHandler(req, res) {
+
+    const html = readFile('./views/home/index.html')
+
+    res.writeHead(200, [
+        'Content-type', 'text/html'
+    ]);
+    res.write(html);
+    res.end();
+
+}
+
+
+module.exports = {
+    homeHandler
+};
