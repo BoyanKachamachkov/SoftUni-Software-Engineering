56117f5634b2440a8e11c74d18b1b4925dfe5f33
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-20 16:39:31 +0300

    Progress handlers addition

--- JS-Back-End/cat-shelter-v2/index.js
+++ JS-Back-End/cat-shelter-v2/index.js
@@ -1,12 +1,14 @@
 const http = require('http');
 const port = 3001;
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, [
-        'Content-Type', 'text/plain'
-    ]);
 
-    res.write('Hello JS world');
-    res.end();
+    for (const handler of handlers) {
+        if (!handler(req, res)) {
+            break;
+        }
+
+    }
 
 }).listen(port);
