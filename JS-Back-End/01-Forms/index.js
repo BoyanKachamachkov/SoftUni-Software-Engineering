8d5d74199f546fc266b8c44026a59f483e4eda03
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-21 16:45:41 +0300

    Finish image upload vanilla NODEJS example

--- JS-Back-End/01-Forms/index.js
+++ JS-Back-End/01-Forms/index.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const qs = require('querystring');
+const path = require('path');
+const { EOL } = require('os');
+const fs = require('fs');
 
 http.createServer((req, res) => {
 
@@ -75,16 +78,38 @@ http.createServer((req, res) => {
             body.push(chunk);
         });
 
+        req.on('close', () => {
+            // 1. use buffer
+            const dataBuffer = Buffer.concat(body);
+            // 2. create binary string with buffer
+            const data = dataBuffer.toString('binary');
 
-        res.writeHead(301, {
-            'location': '/'
+            // 3.
+            const boundary = req.headers['content-type'].split('boundary=').at(1);
+            const parts = data.split(`--${boundary}`);
+
+            // 4. split by 2 end lines depending on OS with OS module
+            const [meta, imageData] = parts[3].split(EOL + EOL);
+
+            // 5. extract file name from metaData
+            const fileName = meta.match(/filename="(.+)"/)[1]; //return the group (1), which is the file name.
+
+            const savePath = path.join(__dirname, 'uploads', fileName); //save file here
+
+            // 6. save picture
+            fs.writeFile(savePath, imageData, 'binary', (err) => {
+                if (err) {
+                    return res.end();
+                }
+                console.log('Image uploaded successfully!');
+                res.writeHead(302, {
+                    'location': '/'
+                });
+                res.end();
+            });
+
+
         });
-        res.end();
-
     }
-
-    res.end();
-
-
 }).listen(5001);
 console.log('Server is listening on port: 5001');
