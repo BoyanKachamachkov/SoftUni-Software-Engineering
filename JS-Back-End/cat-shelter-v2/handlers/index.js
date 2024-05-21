33d13020620a17696873e95a7e4e5419c5c4fb12
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-21 11:13:49 +0300

    Add cat handlers

--- JS-Back-End/cat-shelter-v2/handlers/index.js
+++ JS-Back-End/cat-shelter-v2/handlers/index.js
@@ -1,7 +1,9 @@
+const cat = require('./cat');
 const homeHandler = require('./home');
 const staticFiles = require('./static-files');
 
 module.exports = [
     homeHandler,
-    staticFiles
+    staticFiles,
+    cat
 ];
