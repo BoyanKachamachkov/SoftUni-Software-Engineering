7e1d5c1e8880ffb2c9fd7d6ee1998fa9651791a8
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-22 11:52:04 +0300

    Add examples for special chars

--- JS-Back-End/03-ExpressJS-and-Templating/index.js
+++ JS-Back-End/03-ExpressJS-and-Templating/index.js
@@ -1,7 +1,9 @@
 const express = require('express'); //import express
 
 const app = express(); // raise express instance of a server
-
+app.listen(5005, () => {
+    console.log('listening on 5005');
+});
 // method + path couple ---> handler
 app.get('/', (req, res) => {
     res.header({
@@ -11,6 +13,24 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1>Hey</h1>');
 });
 
-app.listen(5005, () => {
-    console.log('listening on 5005');
+app.get('/cats', (req, res) => {
+    res.send('cat has been');
+});
+
+app.post('/cats', (req, res) => {
+    res.send('created a cat');
+});
+
+// rarely used, for ALL methods response!
+app.all('/dogs', (req, res) => {
+    res.send('this is an app for cats');
+});
+
+app.get(/.*dogs/, (req, res) => {
+    res.status(301).send('Regexp example');
+});
+
+// wildcard PATH *
+app.all('*', (req, res) => {
+    res.status(404).send('Page not found - Error 404');
 });
