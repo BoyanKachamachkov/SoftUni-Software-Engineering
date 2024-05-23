55d10bbb3fa622abb474ef52833cd9a7fd3efef2
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-23 16:06:23 +0300

    Extract express and handlebars configs

--- JS-Back-End/03-ExpressJS-Workshop/src/index.js
+++ JS-Back-End/03-ExpressJS-Workshop/src/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
-
 const routes = require('./routes');
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
 
 const app = express();
 const port = 5007;
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+configHandlebars(app);
+configExpress(app);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes); // delegate routing to routes
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`ExpressJS Server is listening on ${port}...`);
