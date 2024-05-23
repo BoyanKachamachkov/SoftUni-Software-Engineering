32d4a5bed5aa01d6613d55e0507ca80f86851b86
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-23 16:44:47 +0300

    Add hardcoded movie

--- JS-Back-End/03-ExpressJS-Workshop/src/services/movieService.js
+++ JS-Back-End/03-ExpressJS-Workshop/src/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = [];
+const movies = [{
+    title: 'Harry Potter',
+    genre: 'Fantasy',
+    director: 'David Yates',
+    date: '2003',
+    imageUrl: 'HP',
+    rating: '5/5',
+    description: "The story begins when 11-year-old orphan Harry discovers that his parents were wizards and he starts his education in magic at Hogwart's School of Witchcraft and Wizardry. There he makes two close friends, Ron and Hermione, who share his adventures.\r\n"
+}]; // нямаме персистенция на данните
 
 exports.create = (movieData) => {
+    movies.push(movieData);
     console.log(movieData);
 };
