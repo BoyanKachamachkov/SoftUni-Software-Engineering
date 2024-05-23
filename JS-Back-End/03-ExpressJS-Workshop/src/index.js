65252f9993429abb364545c6d938b23d43c690bf
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-23 12:31:02 +0300

    Setup express

--- JS-Back-End/03-ExpressJS-Workshop/src/index.js
+++ JS-Back-End/03-ExpressJS-Workshop/src/index.js
@@ -1,1 +1,12 @@
-console.log('init')
+const express = require('express');
+
+const app = express();
+const port = 5007;
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.listen(port, () => {
+    console.log(`ExpressJS Server is listening on ${port}...`);
+});
