a9c53a8483e8ed6663d7ad6a92259b4f04c103f9
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-23 15:29:00 +0300

    Add create movie page

--- JS-Back-End/03-ExpressJS-Workshop/src/routes.js
+++ JS-Back-End/03-ExpressJS-Workshop/src/routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router(); // create express instance and use router from it
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
 
-router.use(homeController)
 
+router.use(homeController);
+router.use(movieController);
 
 
 module.exports = router;
