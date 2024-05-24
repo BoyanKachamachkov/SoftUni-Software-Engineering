ae96852d58f9ebe76453a7e814ae713928f3dfba
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-24 17:07:57 +0300

    Extract routers, add home controller

--- JS-Back-End/03-ExpressJS-Workshop-homework/src/index.js
+++ JS-Back-End/03-ExpressJS-Workshop-homework/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const routes = require('./routes')
 
 const app = express();
 const port = 5006;
@@ -17,9 +18,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/', (req, res) => {
-    res.render('home', { layout: false });
-});
+app.use(routes)
 
 // setup static files
 app.use(express.static(path.join(__dirname, 'public')))
