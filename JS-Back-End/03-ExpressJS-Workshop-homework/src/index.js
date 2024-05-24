e9d4919ede1093d01dc81beca231eb16113614a3
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-24 16:07:37 +0300

    Configure express handlebars

--- JS-Back-End/03-ExpressJS-Workshop-homework/src/index.js
+++ JS-Back-End/03-ExpressJS-Workshop-homework/src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 5006;
+app.listen(port, () => console.log(`Listening on ${port}`));
+
+// добавяме нов енджин, откъде го взима? хендълбарс.енджин
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+// сетваме промениливата вю енджин да е хбс за цялото приложение
+app.set('view engine', 'hbs');
+
+// по принцип търси вюс в дифолтната папка на проекта, ама при нас е в СРЦ, затова трябва да го сетнем
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
-    res.send('Test');
+    res.render('home', { layout: false });
 });
-
-app.listen(port, () => {
-    console.log(`Server is listening on ${port}`)
-})
-
