f8421cbb5dd7f740c419578eca85a24a69a17081
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-25 15:52:30 +0300

    Prepare CSS resources and solve task 1

--- JS-Back-End/03-ExpressJS-Workshop-homework/src/controllers/homeController.js
+++ JS-Back-End/03-ExpressJS-Workshop-homework/src/controllers/homeController.js
@@ -4,9 +4,13 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
-router.get('/about', (req,res) => {
-    res.render('about')
-})
+router.get('/about', (req, res) => {
+    res.render('about');
+});
 
+router.get('/404', (req, res) => {
+    res.render('404');
+});
 
+// export controller
 module.exports = router;
