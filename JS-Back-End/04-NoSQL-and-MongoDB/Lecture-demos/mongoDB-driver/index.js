c7841e469d80731d3de53f61f601c0f61937b339
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-27 15:34:26 +0300

    Initialize mongodb driver and client

---
+++ JS-Back-End/04-NoSQL-and-MongoDB/Lecture-demos/mongoDB-driver/index.js
@@ -0,0 +1,7 @@
+const { MongoClient } = require('mongodb');
+
+// const MongoClient = mongodb.MongoClient();
+
+// use connection string to connect to a given DB
+const client = new MongoClient('mongodb://localhost:27017/');
+
