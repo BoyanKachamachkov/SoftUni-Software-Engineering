2c0ab1fb4ea35fad9faee52c0f45ddc0b7b842d1
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-27 15:41:47 +0300

    Finish mongoDB driver demo

--- JS-Back-End/04-NoSQL-and-MongoDB/Lecture-demos/mongoDB-driver/index.js
+++ JS-Back-End/04-NoSQL-and-MongoDB/Lecture-demos/mongoDB-driver/index.js
@@ -1,3 +1,4 @@
+// driver is only used to connect to mongoDB
 const { MongoClient } = require('mongodb');
 
 // const MongoClient = mongodb.MongoClient();
@@ -5,3 +6,16 @@ const { MongoClient } = require('mongodb');
 // use connection string to connect to a given DB
 const client = new MongoClient('mongodb://localhost:27017/');
 
+async function run() {
+
+    const db = client.db('Students');
+    const collection = db.collection('students');
+
+    // mongoDB queries
+    const allStudentsCursor = await collection.find()
+    const students = await allStudentsCursor.toArray();
+
+    console.log(students);
+}
+
+run();
