f013ad73d527c24f3ab72e623301210a7a4d14ce
kachamachkov <kachamachkovboyan@gmail.com>
2024-05-28 10:32:11 +0300

    Add model methods, virtual prop examples

--- JS-Back-End/04-NoSQL-and-MongoDB/Lecture-demos/mongoose/index.js
+++ JS-Back-End/04-NoSQL-and-MongoDB/Lecture-demos/mongoose/index.js
@@ -17,6 +17,17 @@ const studentSchema = new mongoose.Schema({
     age: Number,
 });
 
+// SCHEMA MODELS METHODs (custom)
+studentSchema.methods.logInfo = function () {
+    console.log(`Hello, I'm ${this.name}, and I'm ${this.age} years old.`);
+
+};
+
+// Virtual properties, do not exsist in DB, combination/calculation of other exsisting props.
+studentSchema.virtual('description').get(function () {
+    return `Name: ${this.name}. Age: ${this.age}.`;
+});
+
 // 2. Create model based on Schema? Use upper letter (like class) for naming
 // pass name (usually like const) and then the SCHEMA
 const Student = mongoose.model('Student', studentSchema);
@@ -27,11 +38,32 @@ const student = new Student({
     age: 17
 });
 
-console.log(student)
+// console.log(student);
 
+// CREATE (2 options)
 // 4. save in DB
-student.save()
-    .then((createdStudent) => {
-        console.log(createdStudent);
-        console.log('Student saved')
-    }); //returns promise
+// student.save()
+//     .then((createdStudent) => {
+//         console.log(createdStudent);
+//         console.log('Student saved');
+//     }); //returns promise
+
+
+// // Alternative CREATE
+// Student.create({
+//     name: 'Mariyka',
+//     age: 19
+// }).then(data => console.log(data));
+
+// (READ , GET data)
+Student.find()
+    .then(students => {
+        students.forEach(s => s.logInfo());
+
+    });
+
+// MONGODB query with gte operator
+Student.find({ age: { $gte: 19 } })
+.then(res =>{
+    res.forEach(s => console.log(s.description))
+});
