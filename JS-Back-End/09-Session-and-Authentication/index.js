75117b62a35ae1aef305c2c3dd9d26d1b31a98a9
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-01 11:28:40 +0300

    Add register example

--- JS-Back-End/09-Session-and-Authentication/index.js
+++ JS-Back-End/09-Session-and-Authentication/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
+const bcrpyt = require('bcrypt');
 
 const app = express();
+
+const db = {};
 
 
 app.use(express.urlencoded({ extended: false }));
@@ -36,18 +38,49 @@ app.get('/login', (req, res) => {
 `);
 });
 
+// REGISTER example
+app.get('/register', (req, res) => {
+    res.send(`
+    <form action="/register" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" name="username">
+
+    <label for="password">Password:</label>
+    <input type="password" name="password">
+
+    <input type="submit" value="Register">
+</form>
+`);
+});
+
+app.post('/register', async (req, res) => {
+    const hash = await bcrpyt.hash(req.body.password, 12);
+
+    db[req.body.username] = hash; //connect given username with given hash and save into DB (obj)
+
+    res.redirect('/login');
+
+});
 app.post('/login', async (req, res) => {
-    // 1. hash pw
-    const salt = await bcrypt.genSalt(10);
-
-    // 2. salted hash
-    const saltedHash = await bcrypt.hash(req.body.password, salt);
-
-    console.log(salt);
-    console.log(saltedHash);
-
     res.cookie('user', req.body.username);
-    res.end();
+
+    // check if hash  against given username exsists in db?
+    const hash = db[req.body.username];
+    console.log(hash)
+
+    if (!hash) {
+        return res.status(401).end();
+    }
+
+    // check if pw is valid?
+    const isValid = await bcrpyt.compare(req.body.password, hash);
+    console.log(isValid)
+
+    if (!isValid) {
+        return res.status(401).send('Unauthorized!');
+    }
+
+    res.send('Logged in successfully');
 
 });
 
