97aec5c2ff979d98a726f8f00329a0aea97bbbf2
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-02 11:31:40 +0300

    Hash password on save

--- JS-Back-End/03-ExpressJS-Workshop/src/models/User.js
+++ JS-Back-End/03-ExpressJS-Workshop/src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt')
 
 
 const userSchema = new Schema({
@@ -13,6 +14,13 @@ const userSchema = new Schema({
     }
 });
 
-const User = model('User', userSchema)
+// hash pw before saving to db (GDPR)
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 12);
+
+    this.password = hash;
+});
+
+const User = model('User', userSchema);
 
 module.exports = User;
