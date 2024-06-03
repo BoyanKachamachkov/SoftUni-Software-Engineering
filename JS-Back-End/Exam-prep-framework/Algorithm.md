9daecf4dd3b2b476b6fe92e2fd8c06279fa35ebf
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-03 18:02:22 +0300

    Add routes & handlebars

--- JS-Back-End/Exam-prep-framework/Algorithm.md
+++ JS-Back-End/Exam-prep-framework/Algorithm.md
@@ -3,7 +3,12 @@
 1. Initialize project
 2. Install & config nodemon
 3. Install & config express
-   * install express
-   * start initial express server
-   * add static 
-   * consume static folder
+   1. install express
+   2. start initial express server
+   3. add static 
+   4. consume static folder
+   5. add body parser urlencoded
+   6. add routes module
+4. Install & config handlebars
+   1. install handlebars
+   2. config handlebars with express
--- JS-Back-End/Exam-prep-framework/index.js
+++ JS-Back-End/Exam-prep-framework/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const routes = require('./routes');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(routes);
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false })); //data from req comes to req.body
 
-app.get('/', (req, res) => {
-    res.send('Hey there!');
-});
-
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
 
 app.listen(3000, () => console.log(`App is listening on port 3000...`));
