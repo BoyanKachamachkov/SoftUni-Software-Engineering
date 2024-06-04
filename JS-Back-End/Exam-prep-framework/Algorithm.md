33563723be0dcd07fbddcff0edbdb04df7ea46dd
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-04 10:19:01 +0300

    Add DB and User model

--- JS-Back-End/Exam-prep-framework/Algorithm.md
+++ JS-Back-End/Exam-prep-framework/Algorithm.md
@@ -1,19 +1,23 @@
 # Express App building algorithm
 
 1. Initialize project
-2. Install & config nodemon
-3. Install & config express
+2. Nodemon
+   1. install nodemon
+   2. config start script
+3. Express
    1. install express
    2. start initial express server
    3. add static 
    4. consume static folder
    5. add body parser urlencoded
    6. add routes module
-4. Install & config handlebars
+4. Handlebars
    1. install handlebars
    2. config handlebars with express
    3. add views folder with resources
    4. add main layout
    5. add partials dir
-5. Install & config database
+5. Database
    1. install mongoose
+   2. setup db connection
+   3. add user model
--- JS-Back-End/Exam-prep-framework/index.js
+++ JS-Back-End/Exam-prep-framework/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.use(express.urlencoded({ extended: false })); //data from req comes to req.b
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
+
+// TODO: change DB name to reflect project name
+mongoose.connect('mongodb://localhost:27017/course-book');
+
+mongoose.connection.on('connected', () => console.log('DB is connected'));
+mongoose.connection.on('error', (err) => console.log(err));
+mongoose.connection.on('disconnect', () => console.log('DB is disconnected'));
 
 app.listen(3000, () => console.log(`App is listening on port 3000...`));
