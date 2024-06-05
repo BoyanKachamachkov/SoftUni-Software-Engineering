48dcad465dfc32888872dd00c40e4b3510d6197b
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-05 16:30:02 +0300

    Setup handlebars, home controller, render home

--- JS-Back-End/Exam-prep-framework-3/src/index.js
+++ JS-Back-End/Exam-prep-framework-3/src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
-
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
-app.use(routes)
+app.use(routes);
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
 
 
 
--- JS-Back-End/Exam-prep-framework-3/src/routes.js
+++ JS-Back-End/Exam-prep-framework-3/src/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
+const homeController = require('./controllers/homeController')
 
-router.get('/', (req, res) => {
-    res.send('hi there');
-});
+
+router.use(homeController)
 
 
 module.exports = router;
