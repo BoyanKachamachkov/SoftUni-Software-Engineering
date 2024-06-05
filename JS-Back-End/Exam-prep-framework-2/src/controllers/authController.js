c4b0b6594b48a6ed646df782e242be076c160c77
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-05 11:02:21 +0300

    Add user creation post to authController

--- JS-Back-End/Exam-prep-framework-2/src/controllers/authController.js
+++ JS-Back-End/Exam-prep-framework-2/src/controllers/authController.js
@@ -1,17 +1,27 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 
-router.get('/register', (req,res) =>{
-    res.render('auth/register')
-})
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
 
 
-router.get('/login', (req,res) => {
-    res.render('auth/login')
-})
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
 
 
+router.post('/register', async (req, res) => {
+    // get user data
+    const userData = req.body;
 
+    // utilize service to create user
+    await authService.register(userData);
+
+    // redirect
+    res.redirect('/auth/login')
+});
 
 
 module.exports = router;
