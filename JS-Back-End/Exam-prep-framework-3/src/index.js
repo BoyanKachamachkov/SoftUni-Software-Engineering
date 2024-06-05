1f5f0419cdf5e07e0f9d418ed9b9035fa0de9454
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-05 16:22:46 +0300

    Setup express & static resources

---
+++ JS-Back-End/Exam-prep-framework-3/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+
+
+const app = express();
+
+app.use(express.static('src/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes)
+
+
+
+app.listen(3000, () => console.log('Server is listening on port 3000...'));
