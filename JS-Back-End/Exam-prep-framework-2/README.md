c8d8ed7f0fa6f962b4f57263786264db1799a13c
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-05 10:25:57 +0300

    Install HB, config it, render Home page

--- JS-Back-End/Exam-prep-framework-2/README.md
+++ JS-Back-End/Exam-prep-framework-2/README.md
@@ -12,10 +12,10 @@
    5. [x] add body parser urlencoded
    6. [x] add routes module
 4. Handlebars
-   1. install handlebars
-   2. config handlebars with express
-   3. add views folder with resources
-   4. add home view
+   1. [x] install handlebars
+   2. [x] config handlebars with express
+   3. [x] add views folder with resources
+   4. [x] add home view
    5. add home controller
    6. add home controller to routes
    7. add main layout
--- JS-Back-End/Exam-prep-framework-2/src/index.js
+++ JS-Back-End/Exam-prep-framework-2/src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const handlebars = require('express-handlebars');
+const path = require('path')
 
 const app = express();
+
+// config HB
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
+app.set('views', path.resolve('src/views'))
 
 // MWs
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
-app.use(routes)
-
-
+app.use(routes);
 
 
 app.listen(3000, () => console.log('Backend is listening on port 3000...'));
