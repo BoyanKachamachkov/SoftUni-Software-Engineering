49e520bdab8ba1fa0a06a6f15bdd64833995ef08
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-08 10:17:53 +0300

    Add & config handlebars

--- JS-Back-End/Exam-Course-Book/src/controllers/homeController.js
+++ JS-Back-End/Exam-Course-Book/src/controllers/homeController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.send('Hey');
+    res.render('home');
 });
 
 module.exports = router;
--- JS-Back-End/Exam-Course-Book/src/index.js
+++ JS-Back-End/Exam-Course-Book/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
-
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
 app.listen(3000, () => console.log('Server is listening on port:3000...'));
