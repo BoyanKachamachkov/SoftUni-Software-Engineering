43bf0f73849f41ac0d2d72ac48d42cdc8270e5a1
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-09 17:48:03 +0300

    Fix details template for all cases

--- JS-Back-End/Exam-Course-Book/src/controllers/courseController.js
+++ JS-Back-End/Exam-Course-Book/src/controllers/courseController.js
@@ -22,7 +22,6 @@ router.post('/create', isAuth, async (req, res) => {
 });
 
 router.get('/courses', async (req, res) => {
-
     // масив от документи го изглаждаме до масив от обекти
     const courses = await courseService.getAll().lean();
 
@@ -31,18 +30,15 @@ router.get('/courses', async (req, res) => {
 
 
 router.get('/courses/details/:courseId', async (req, res) => {
-
-    // The req.params property is an object containing properties mapped to the named route “parameters”. For example, if you have the route /student/:id, then the “id” property is available as req.params.id. This object defaults to {}. 
     const course = await courseService.getOneDetailed(req.params.courseId).lean();
-
     const signedUpUsers = course.signUpList.map(user => user.username).join(', ');
+    const isOwner = course.owner._id == req.user?._id;
+    const isSigned = course.signUpList.some(user => user._id == req.user?._id);
 
-    res.render('details', { ...course, signedUpUsers });
+    res.render('details', { ...course, signedUpUsers, isOwner, isSigned });
 });
 
 router.get('/courses/sign-up/:courseId', async (req, res) => {
-
-
     // use params for course ID variable , use req.user for user ID
     await courseService.signUp(req.params.courseId, req.user._id);
 
