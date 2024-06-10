d01738dcdd47f1f5174448f839e56500bd403e9b
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-10 13:28:30 +0300

    Add bonus* - profile functionality

--- JS-Back-End/Exam-Course-Book/src/controllers/homeController.js
+++ JS-Back-End/Exam-Course-Book/src/controllers/homeController.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const courseService = require('../services/courseService');
+const userService = require('../services/userService');
 
 router.get('/', async (req, res) => {
     const latestCourses = await courseService.getLatest().lean();
-    console.log(latestCourses)
+    console.log(latestCourses);
     res.render('home', { latestCourses });
 });
 
+
+router.get('/profile', isAuth, async (req, res) => {
+    const user = await userService.getInfo(req.user._id).lean();
+    const createdCount = user.createdCourses.length;
+    const signedCount = user.signedUpCourses.length;
+
+    res.render('profile', { user, createdCount, signedCount });
+});
 
 
 module.exports = router;
