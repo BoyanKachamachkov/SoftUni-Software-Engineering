127bad7beeca6a0da39e05b6450d4d50df8e58f6
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-17 10:09:09 +0300

    Render register page

--- JS-Back-End/Examp-prep-Magma-Haven/src/routes.js
+++ JS-Back-End/Examp-prep-Magma-Haven/src/routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 const homeController = require('../src/controllers/homeController');
+const authController = require('../src/controllers/authController');
 
 
 router.use(homeController);
-
+router.use('/auth', authController);
 
 
 module.exports = router;
