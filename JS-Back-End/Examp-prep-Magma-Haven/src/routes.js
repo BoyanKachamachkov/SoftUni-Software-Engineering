2cfe4b57dc586c9d751af365466e652ea5472048
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-17 12:24:03 +0300

    Render create Volcano page

--- JS-Back-End/Examp-prep-Magma-Haven/src/routes.js
+++ JS-Back-End/Examp-prep-Magma-Haven/src/routes.js
@@ -2,10 +2,16 @@ const router = require('express').Router();
 
 const homeController = require('../src/controllers/homeController');
 const authController = require('../src/controllers/authController');
+const volcanoesController = require('./controllers/volcanoesController');
 
 
 router.use(homeController);
 router.use('/auth', authController);
+router.use('/volcanoes', volcanoesController);
 
 
+
+router.all('*', (req, res) => {
+    res.render('404');
+});
 module.exports = router;
