cee2005a10902c668c7c3ddf3d5f70cd5fd46f00
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-17 12:02:05 +0300

    Add route guards to login/register/logout

--- JS-Back-End/Examp-prep-Magma-Haven/src/controllers/authController.js
+++ JS-Back-End/Examp-prep-Magma-Haven/src/controllers/authController.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { getErrorMessage } = require('../lib/getErrorMessage');
 const authService = require('../services/authService');
+const { isAuth, isGuest } = require('../middlewares/authMiddleware');
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest, (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', isGuest, async (req, res) => {
     const regData = req.body;
 
     try {
@@ -21,11 +22,11 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest, (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
     const loginData = req.body;
 
     try {
@@ -39,7 +40,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isAuth, (req, res) => {
     res.clearCookie('auth');
     res.redirect('/');
 });
--- JS-Back-End/Examp-prep-Magma-Haven/src/index.js
+++ JS-Back-End/Examp-prep-Magma-Haven/src/index.js
@@ -3,6 +3,7 @@ const routes = require('../src/routes');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { authMiddleware } = require('./middlewares/authMiddleware');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
+app.use(authMiddleware);
 app.use(routes);
 
 mongoose.connect('mongodb://localhost:27017/magma-haven');
