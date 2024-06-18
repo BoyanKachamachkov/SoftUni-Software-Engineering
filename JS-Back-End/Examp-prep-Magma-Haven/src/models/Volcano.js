067358e6f62fa6bac82e70b59f5c29ce4f92ce3d
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-18 12:11:42 +0300

    Add more error validations to models

--- JS-Back-End/Examp-prep-Magma-Haven/src/models/Volcano.js
+++ JS-Back-End/Examp-prep-Magma-Haven/src/models/Volcano.js
@@ -3,31 +3,39 @@ const mongoose = require('mongoose');
 const volcanoSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required.'],
+        minLength: ['2', 'The name should be at least 2 characters long.']
     },
     location: {
         type: String,
-        required: true,
+        required: [true, 'location is required.'],
+        minLength: ['3', 'The location should be at least 3 characters long.']
     },
     elevation: {
         type: String,
-        required: true,
+        required: [true, 'Elevation is required.'],
+        minValue: 0
     },
     lastErruption: {
         type: Number,
-        required: true,
+        required: [true, 'Last erruption year is required.'],
+        minValue: 0,
+        maxValue: 2024,
     },
     image: {
         type: String,
-        required: true,
+        required: [true, 'Image is required.'],
+        match: /^https?:\/\//
     },
     typeVolcano: {
         type: String,
-        required: true,
+        required: [true, 'Type of Volcano is required.'],
+
     },
     description: {
         type: String,
-        required: true,
+        required: [true, 'Description is required.'],
+        minLength: ['10', 'The description should be at least 10 characters long.']
     },
     voteList: [{
         type: mongoose.Types.ObjectId,
