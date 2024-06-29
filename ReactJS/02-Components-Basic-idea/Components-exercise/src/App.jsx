1d0fbb5aaa3f9647e768e98567be9c1c32044ff6
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-29 15:05:45 +0300

    Split components further example

--- ReactJS/02-Components-Basic-idea/Components-exercise/src/App.jsx
+++ ReactJS/02-Components-Basic-idea/Components-exercise/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Gallery from './components/Gallery';
 import Profile from './components/Profile';
 
 function App() {
 
 	return (
 		<>
+			<Gallery />
 			<Profile />
 		</>
 	);
