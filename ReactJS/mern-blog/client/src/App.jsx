b8bae1f8c59d3af1abb8bd7bf2ea87dc780bd6a3
kachamachkov <kachamachkovboyan@gmail.com>
2024-06-30 17:55:33 +0300

    Add styles

--- ReactJS/mern-blog/client/src/App.jsx
+++ ReactJS/mern-blog/client/src/App.jsx
@@ -2,9 +2,26 @@ import './App.css';
 
 function App() {
 
-  return (
-    <div>hi</div>
-  );
+	return (
+		<main>
+			<header>
+				<a href="" className="logo">MyBlog</a>
+
+				<nav>
+					<a href="">Login</a>
+					<a href="">Register</a>
+				</nav>
+			</header>
+
+			<div className="post">
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Dav4Fa-gecwEt4i7wxtprS2l17on_X9VnQ&s" alt="" />
+				<h2>random test</h2>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, in?</p>
+
+			</div>
+		</main>
+
+	);
 }
 
 export default App;
