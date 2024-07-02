f2ddd9c42eab59576399793d1ccabf696853a4f5
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-02 16:00:34 +0300

    Add first component demo

--- ReactJS/01-Intro-to-React-JSX/vite-demo/src/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Test() {
-    // return React.createElement('h2', null, 'Custom Component');
-
-
-    return <h2>Custom component with JSX</h2>;
-}
---
+++ ReactJS/01-Intro-to-React-JSX/vite-demo/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    // return React.createElement('header', {}, 'Header test');
+    return (
+        <header>
+            <h3>Test</h3>
+        </header>
+    );
+}
