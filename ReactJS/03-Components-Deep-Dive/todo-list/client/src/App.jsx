c893ac53732348e8b078af92285ebe204f01201a
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-04 11:27:28 +0300

    Render Todo app staticly

--- ReactJS/03-Components-Deep-Dive/todo-list/client/src/App.jsx
+++ ReactJS/03-Components-Deep-Dive/todo-list/client/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import TodoList from './components/TodoList';
 
 function App() {
 
 	return (
 		<>
 			<Header />
+			<main class="main">
 
+				<TodoList />
+			</main>
 
 			<Footer />
 		</>
--- ReactJS/03-Components-Deep-Dive/todo-list/client/src/components/TodoList.jsx
+++ ReactJS/03-Components-Deep-Dive/todo-list/client/src/components/TodoList.jsx
@@ -12,11 +12,11 @@ export default function TodoList() {
             <div className="table-wrapper">
 
                 {/* <!-- Loading spinner - show the load spinner when fetching the data from the server--> */}
-                <div className="loading-container">
+                {/* <div className="loading-container">
                     <div className="loading-spinner">
                         <span className="loading-spinner-text">Loading</span>
                     </div>
-                </div>
+                </div> */}
 
                 {/* <!-- Todo list table --> */}
                 <table className="table">
