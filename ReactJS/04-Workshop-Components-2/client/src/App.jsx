ccd5aeed0984a2b407e8ca80bc7cc7d817c87930
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-08 13:32:47 +0300

    Render header and footer

--- ReactJS/04-Workshop-Components-2/client/src/App.jsx
+++ ReactJS/04-Workshop-Components-2/client/src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
 
     return (
         <>
+            <Header />
 
-        <h1>Hello</h1>
+
+            <Footer />
         </>
     );
 }
