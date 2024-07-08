df91fe71a941aaa6b861c21778a3ebacd529083f
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-08 14:08:31 +0300

    Fetch users from server with useEffect and IIFE

--- ReactJS/04-Workshop-Components-2/client/src/components/section-list/SectionList.jsx
+++ ReactJS/04-Workshop-Components-2/client/src/components/section-list/SectionList.jsx
@@ -1,19 +1,36 @@
+import { useState, useEffect } from "react";
 import Button from "../button/Button";
 import Pagination from "../pagination/Pagination";
 import Table from "./table/Table";
 
+const baseUrl = 'http://localhost:3030/jsonstore';
+
 export default function SectionList() {
 
+    const [users, setUsers] = useState([]);
 
+
+    useEffect(() => {
+
+        (async function getUsers() {
+            const response = await fetch(`${baseUrl}/users`);
+            const users = await response.json();
+            console.log(Object.values(users));
+
+            setUsers(Object.values(users))
+
+        })();
+
+    }, []);
 
     return (
         <section className="card users-container">
 
-        <Table />
+            <Table />
 
 
-        <Button />
-        <Pagination />
+            <Button />
+            <Pagination />
         </section>
 
     );
