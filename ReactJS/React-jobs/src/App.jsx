c38fb5562a1f7489e7713a3e820f956429a3efe0
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-10 15:35:35 +0300

    Render 404 page for invalid routes

--- ReactJS/React-jobs/src/App.jsx
+++ ReactJS/React-jobs/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
 
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
 
     </Route >)
 );
