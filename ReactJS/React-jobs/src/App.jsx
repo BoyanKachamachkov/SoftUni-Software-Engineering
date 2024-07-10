c25194c7d162b683b48340948d0db27356448f80
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-10 13:56:00 +0300

    Show only first 3 jobs + add View All Jobs component

--- ReactJS/React-jobs/src/App.jsx
+++ ReactJS/React-jobs/src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import HomeCards from './components/HomeCards';
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -10,19 +11,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-
-
-      {/* <!-- Browse Jobs --> */}
-
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
 
   );
--- ReactJS/React-jobs/src/components/JobListings.jsx
+++ ReactJS/React-jobs/src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import JobListing from './JobListing';
 
 function JobListings() {
 
-
+  const recentJobs = jobs.slice(0, 3);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -13,7 +13,7 @@ function JobListings() {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-          {jobs.map((job) => (
+          {recentJobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
 
