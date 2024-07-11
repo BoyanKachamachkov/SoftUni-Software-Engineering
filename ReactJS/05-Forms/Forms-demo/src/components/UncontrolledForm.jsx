88614316ed883ce9d3fc915e6d6d2595d0367406
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-11 10:35:00 +0300

    Form example

--- ReactJS/05-Forms/Forms-demo/src/components/UncontrolledForm.jsx
+++ ReactJS/05-Forms/Forms-demo/src/components/UncontrolledForm.jsx
@@ -1,23 +1,48 @@
+import { useState } from "react";
+
 const UncontrolledForm = () => {
+
+  const [user, setUser] = useState({});
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    console.log(formData.get('username'));
+
+    setUser({
+      username: formData.get('username')
+    });
+
+  };
+
+
+
   return (
     <>
       <h1>Uncontrolled form</h1>
 
-      <form>
+      {user.username
+        ? <p>Hello{user.username}!</p>
+        : (
+          <form onSubmit={formSubmitHandler}>
 
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" name="username" id="username" />
-        </div>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input type="text" name="username" id="username" />
+            </div>
 
-        <div>
-          <label htmlFor="username">Password:</label>
-          <input type="password" name="password" id="password" />
-        </div>
+            <div>
+              <label htmlFor="username">Password:</label>
+              <input type="password" name="password" id="password" />
+            </div>
 
-        <input type="submit" value='Login' />
+            <input type="submit" value='Login' />
 
-      </form>
+          </form>
+        )
+      }
+
 
     </>
   );
