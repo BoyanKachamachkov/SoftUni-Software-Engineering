53c714e665bb7f29033f05b0e3a964f36f1c3afd
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-11 11:00:57 +0300

    Add json server API and fetch it

--- ReactJS/React-jobs/src/components/JobListings.jsx
+++ ReactJS/React-jobs/src/components/JobListings.jsx
@@ -1,11 +1,31 @@
-import jobs from '../jobs.json';
 import JobListing from './JobListing';
+import { useState, useEffect } from 'react';
 
 function JobListings({
   isHome = false
 }) {
 
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -15,7 +35,7 @@ function JobListings({
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-          {jobListings.map((job) => (
+          {jobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
 
