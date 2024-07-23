79d86e46a1a45e10745b510167581b592bc34993
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-23 10:45:17 +0300

    Fetch articles from server, render dynamically in cards

--- ReactJS/08-React-hooks/client/src/App.jsx
+++ ReactJS/08-React-hooks/client/src/App.jsx
@@ -2,22 +2,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ArticleCard from './components/ArticleCard';
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`http://localhost:3030/jsonstore/advanced/articles/list`);
+            const result = await response.json();
+
+            setArticles(result);
+        })();
+    }, []);
+
+    console.log(articles);
 
     return (
         <>
             <NavBar />
 
             <div className={styles['article-list']}>
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
-                <ArticleCard />
+                {articles.map(article => <ArticleCard key={article._id} {...article} />)}
             </div>
         </ >
     );
--- ReactJS/08-React-hooks/client/src/components/ArticleCard.jsx
+++ ReactJS/08-React-hooks/client/src/components/ArticleCard.jsx
@@ -1,16 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ArticleCard() {
+function ArticleCard({
+    _id,
+    title,
+}) {
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2223/2223816.png" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
+                <Card.Title>{title}</Card.Title>
+                {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
-                </Card.Text>
+                </Card.Text> */}
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
         </Card>
