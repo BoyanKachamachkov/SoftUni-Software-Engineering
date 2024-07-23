8e7a65a061889d081f0b269c6d1f58e6bf6eb804
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-23 10:34:30 +0300

    Add nav bar, add styles to article list

--- ReactJS/08-React-hooks/client/src/App.jsx
+++ ReactJS/08-React-hooks/client/src/App.jsx
@@ -1,11 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ArticleCard from './components/ArticleCard';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
 
 function App() {
 
     return (
         <>
-            <ArticleCard />
+            <NavBar />
+
+            <div className={styles['article-list']}>
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+            </div>
         </ >
     );
 }
